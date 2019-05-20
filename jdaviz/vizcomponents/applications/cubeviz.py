d2dc0f7295556aa1290f5b56a8890d1ce9e37140
Sara Ogaz <ogaz@stsci.edu>
2019-05-20 13:43:56 -0400

    trying out ipyvuetify

--- jdaviz/vizcomponents/applications/cubeviz.py
+++ jdaviz/vizcomponents/applications/cubeviz.py
@@ -1,4 +1,5 @@
 from ipywidgets import widgets
+import ipyvuetify as v
 
 from ..viewer.viewernd import ViewerND
 from ..viewer.viewer1d import Viewer1D
@@ -21,6 +22,19 @@ class CubeViz:
             description='',
             layout=widgets.Layout(width='10em'),
         )
+        #
+        # items = [v.ListTile(children=[v.ListTileTitle(children=["Load"])]),
+        #          v.ListTile(children=[v.ListTileTitle(children=["Save"])])]
+        #
+        # self._menu_bar_file = v.Layout(children=[v.Menu(offset_y=True,
+        #               children=[v.Btn(slot='activator',
+        #                                            color='primary',
+        #                                            children=['File',
+        #                                                      v.Icon(right=True,
+        #                                                             children=['arrow_drop_down'])]),
+        #                                      v.List(children=items)])
+        #                                          ])
+
         self._menu_bar_file.observe(self._on_change_menu_bar_file)
 
         #
@@ -35,13 +49,15 @@ class CubeViz:
         self._menu_bar_viewer.observe(self._on_change_menu_bar_viewer)
 
         # Add to menu bar
-        self._menu_bar = widgets.HBox([self._menu_bar_file, self._menu_bar_viewer])
+        self._menu_bar = v.Layout(children=[self._menu_bar_file, self._menu_bar_viewer])
         self._menu_bar.box_style = 'success'
 
         self._v1d = Viewer1D(self._vizapp)
         self._v3d = ViewerND(self._vizapp)
 
-        self._main_box = widgets.Box([widgets.VBox([self._menu_bar, widgets.HBox([self._v3d.show(), self._v1d.show()])])])
+        # this is the line where we would put ipyvutify
+        self._main_box = v.Layout(children=[widgets.VBox([self._menu_bar, widgets.HBox([self._v3d.show(), self._v1d.show()])])])
+        #self._main_box = v.Layout(children=[self._menu_bar, self._v3d.show(), self._v1d.show()])
 
     def _on_change_menu_bar_file(self, change):
         print(change)
