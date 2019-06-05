d246f0aea14387d8d7e8cd4da5caf649c17dadc8
busko <busko@stsci.edu>
2019-06-05 11:34:18 -0400

    Add re-projection ability.

--- jdaviz/vizcomponents/viewer/image_viewer.py
+++ jdaviz/vizcomponents/viewer/image_viewer.py
@@ -1,6 +1,5 @@
-import bqplot
-
 from astropy.io import fits
+from reproject import reproject_interp
 
 from ipywidgets import GridBox, Layout, HTML
 from bqplot.interacts import PanZoom
@@ -20,6 +19,12 @@ class ImageVizWidget(BaseViewer, GridBox):
 
         self._main_data = None
 
+        # these should be somehow accessible by
+        # the user, from the notebook level.
+        self.hdu = 1
+        self.vmin = 0.
+        self.vmax = 10.
+
     def _init_gui(self):
 
         # we use bqplot to exploit its ability to lock together
@@ -35,7 +40,8 @@ class ImageVizWidget(BaseViewer, GridBox):
         self.axis_y = Axis(scale=self.scale_y, grid_lines='solid', label='y',
                            orientation='vertical')
 
-        self.fig_image_1 = Figure(scales={'x': self.scale_x,
+        self.fig_image_1 = Figure(title='Reference',
+                                  scales={'x': self.scale_x,
                                           'y': self.scale_y},
                                   axes=[self.axis_x, self.axis_y],
                                   layout={'width': '500px',
@@ -48,7 +54,8 @@ class ImageVizWidget(BaseViewer, GridBox):
                                             'image': self.scale_image_1})
         self.fig_image_1.marks = [self.image_1_mark]
 
-        self.fig_image_2 = Figure(scales={'x': self.scale_x,
+        self.fig_image_2 = Figure(title='Reprojected',
+                                  scales={'x': self.scale_x,
                                           'y': self.scale_y},
                                   axes=[self.axis_x, self.axis_y],
                                   layout={'width': '500px',
@@ -66,32 +73,47 @@ class ImageVizWidget(BaseViewer, GridBox):
                          self.fig_image_1, HTML(), self.fig_image_2],
                          layout=Layout(width='100%',
                                        grid_template_columns='35% 5% 35%',
-                                       grid_template_rows='10% 30%',
+                                       grid_template_rows='20% 30%',
                                        grid_gap='30px 30px'))
 
         self.layout.justify_content = "center"
         self.layout.align_items = "center"
 
     def add_data(self, data):
+        '''
+        Adds the two images to be displayed, re-projecting the
+        second image on the coordinate system of the first image.
 
-        # in this simple demo version, data is a two-element sequence
-        # with the full path names of the two images to be displayed.
+        :param data: 2-element sequence
+         with file names for reference image, and image to
+         be re-projected.
+        :return: True
+        '''
+        hdu1 = fits.open(data[0])[self.hdu]
+        hdu2 = fits.open(data[1])[self.hdu]
+        array, footprint = reproject_interp(hdu2, hdu1.header)
 
-        image_1_data = fits.getdata(data[0])
+        image_1_data = hdu1.data
         self.image_1_mark.image = image_1_data
         self.image_1_mark.x = (-0.5, 20.)
         self.image_1_mark.y = (-0.5, 20.)
 
         self.scale_image_1.min = float(image_1_data.min())
         self.scale_image_1.max = float(image_1_data.max())
+        if self.vmin is not None and self.vmax is not None:
+            self.scale_image_1.min = self.vmin
+            self.scale_image_1.max = self.vmax
 
-        image_2_data = fits.getdata(data[1])
+        image_2_data = array
         self.image_2_mark.image = image_2_data
         self.image_2_mark.x = (-0.5, 20.)
         self.image_2_mark.y = (-0.5, 20.)
 
         self.scale_image_2.min = float(image_2_data.min())
         self.scale_image_2.max = float(image_2_data.max())
+        if self.vmin is not None and self.vmax is not None:
+            self.scale_image_2.min = self.vmin
+            self.scale_image_2.max = self.vmax
 
         self.scale_x.min = 0
         self.scale_x.max = 20.5
@@ -100,5 +122,6 @@ class ImageVizWidget(BaseViewer, GridBox):
         self.scale_y.max = 20.5
 
         return True
+
 
 
