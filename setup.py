c6934e4513996ee1a46fbebb6bece09989621d00
Erik Tollerud <erik.tollerud@gmail.com>
2019-09-04 14:24:16 -0400

    set up version machinery

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 from setuptools import setup
-setup(use_scm_version=True)
+setup(use_scm_version={'write_to': 'jdaviz/_version.py'})
