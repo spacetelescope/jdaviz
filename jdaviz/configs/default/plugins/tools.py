a13dbf83d0b0128c583e8d1ad992423382c5d31b
nmearl <contact@nicholasearl.me>
2019-10-09 16:10:15 -0400

    Import data functionality and file input uploading

--- jdaviz/configs/default/plugins/tools.py
+++ jdaviz/configs/default/plugins/tools.py
@@ -1,8 +1,9 @@
 from ipyvuetify import VuetifyTemplate
-from traitlets import Unicode, List, Int
+from traitlets import Unicode, List, Int, Bool, Dict, Any
 
 from jdaviz.core.registries import tools
 from jdaviz.core.template_mixin import TemplateMixin
+from jdaviz.core.events import LoadDataMessage
 
 __all__ = ['OpenSessionButton', 'SaveSessionButton', 'ImportDataButton', 'ExportDataButton']
 
@@ -49,15 +50,82 @@ class SaveSessionButton(TemplateMixin):
 
 @tools('g-import-data')
 class ImportDataButton(TemplateMixin):
+    valid = Bool(True).tag(sync=True)
+    dialog = Bool(False).tag(sync=True)
+    file_paths = Any(None).tag(sync=True)
+
     template = Unicode("""
-    <v-btn text class="mx-1">
-        <v-icon left>cloud_download</v-icon>
-        Import Data
-    </v-btn>
+    <div class="text-center">
+        <v-dialog
+          v-model="dialog"
+          width="500"
+          persistent
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn
+              dark
+              v-on="on"
+              text
+              class="mx-1"
+            >
+              <v-icon left>cloud_download</v-icon>
+              Import Data
+            </v-btn>
+          </template>
+    
+            <v-form v-model="valid">
+              <v-card>
+                <v-card-title
+                  class="headline grey lighten-2"
+                  primary-title
+                >
+                  Import Data
+                </v-card-title>
+                
+                <v-card-text>
+                    <v-file-input 
+                        show-size 
+                        counter 
+                        label="File input" 
+                        v-model="file_paths"
+                    ></v-file-input>
+                </v-card-text>
+                <v-divider></v-divider>
+        
+                <v-card-actions>
+                  <div class="flex-grow-1"></div>
+                  <v-btn
+                    color="primary"
+                    text
+                    @click="dialog = false"
+                  >
+                    Cancel
+                  </v-btn>
+                  <v-btn
+                    color="primary"
+                    text
+                    @click="load_data"
+                  >
+                    Import
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-form>
+        </v-dialog>
+      </div>
     """).tag(sync=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def vue_load_data(self, *args, **kwargs):
+        # TODO: hack because of current incompatibility with ipywidget types
+        #  and vuetify templates.
+        for path in ["/Users/nearl/data/single_g235h-f170lp_x1d.fits"]:
+            load_data_message = LoadDataMessage(path, sender=self)
+            self.hub.broadcast(load_data_message)
+
+        self.dialog = False
 
 
 @tools('g-export-data')
