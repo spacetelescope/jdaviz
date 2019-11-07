c023dd3ceb283f81853378ed07a48e561d1671fa
nmearl <contact@nicholasearl.me>
2019-11-07 12:19:39 -0500

    Include custom voila template

---
+++ share/jupyter/voila/templates/vuetify-base/nbconvert_templates/util.js
@@ -0,0 +1,153 @@
+Vue.use(Vuetify);
+
+Vue.component('jupyter-widget-mount-point', {
+    data() {
+        return {
+            renderFn: undefined,
+            elem: undefined,
+        }
+    },
+    props: ['mount-id'],
+    created() {
+        requestWidget(this.mountId);
+    },
+    mounted() {
+        requestWidget(this.mountId)
+            .then(widgetView => {
+                    if (['VuetifyView', 'VuetifyTemplateView'].includes(widgetView.model.get('_view_name'))) {
+                        this.renderFn = createElement => widgetView.vueRender(createElement);
+                    } else {
+                        while (this.$el.firstChild) {
+                            this.$el.removeChild(this.$el.firstChild);
+                        }
+
+                        requirejs(['@jupyter-widgets/base'], widgets =>
+                            widgets.JupyterPhosphorWidget.attach(widgetView.pWidget, this.$el)
+                        );
+                    }
+                }
+            );
+    },
+    render(createElement) {
+        if (this.renderFn) {
+            /* workaround for v-menu click */
+            if (!this.elem) {
+                this.elem = this.renderFn(createElement);
+            }
+            return this.elem;
+        }
+        return createElement('div', this.$slots.default ||
+            [createElement('v-chip' ,`[${this.mountId}]`)]);
+    }
+});
+
+const widgetResolveFns = {};
+const widgetPromises = {};
+
+function provideWidget(mountId, widgetView) {
+    if (widgetResolveFns[mountId]) {
+        widgetResolveFns[mountId](widgetView);
+    } else {
+        widgetPromises[mountId] = Promise.resolve(widgetView);
+    }
+}
+
+function requestWidget(mountId) {
+    if (!widgetPromises[mountId]) {
+        widgetPromises[mountId] = new Promise(resolve => widgetResolveFns[mountId] = resolve);
+    }
+    return widgetPromises[mountId];
+}
+
+function getWidgetManager(voila, kernel) {
+    try {
+        /* voila < 0.1.8 */
+        return new voila.WidgetManager(kernel);
+    } catch (e) {
+        if (e instanceof TypeError) {
+            /* voila >= 0.1.8 */
+            const context = {
+                session: {
+                    kernel,
+                    kernelChanged: {
+                        connect: () => {}
+                    },
+                    statusChanged: {
+                        connect: () => {}
+                    },
+                },
+                saveState: {
+                    connect: () => {}
+                },
+            };
+
+            const settings = {
+                saveState: false
+            };
+
+            const rendermime = new voila.RenderMimeRegistry({
+                initialFactories: voila.standardRendererFactories
+            });
+
+            return new voila.WidgetManager(context, rendermime, settings);
+        } else {
+            throw e;
+        }
+    }
+}
+
+window.init = async (voila) => {
+    define("vue", [], () => Vue);
+
+    const kernel = await voila.connectKernel();
+    window.addEventListener('beforeunload', () => kernel.shutdown());
+
+    const widgetManager = getWidgetManager(voila, kernel);
+
+    if (!window.enable_nbextensions) {
+        const originalLoader = widgetManager.loader;
+        widgetManager.loader = (moduleName, moduleVersion) => {
+            if (moduleName === 'jupyter-vuetify' || moduleName === 'jupyter-vue') {
+                const newModuleName = moduleName + '/nodeps';
+                const promise = originalLoader(newModuleName, moduleVersion);
+                if (moduleName === 'jupyter-vue') {
+                    promise.then(_ => requirejs.config({
+                        map: {
+                            '*': {
+                                [moduleName]: newModuleName
+                            }
+                        }
+                    }));
+                }
+                return promise;
+            }
+            return originalLoader(moduleName, moduleVersion);
+        };
+    }
+
+    await widgetManager.build_widgets();
+
+    Object.values(widgetManager._models)
+        .forEach(async (modelPromise) => {
+            const model = await modelPromise;
+            const meta = model.get('_metadata');
+            const mountId = meta && meta.mount_id;
+            if (mountId && model.get('_view_name')) {
+                const view = await widgetManager.create_view(model);
+                provideWidget(mountId, view);
+            }
+        });
+
+    app.$data.loading = false;
+    removeInterferingStyleTags();
+};
+
+function removeInterferingStyleTags() {
+    document.querySelectorAll("style:not(#vuetify-theme-stylesheet)")
+        .forEach((styleTag) => {
+            if (styleTag.textContent.includes("/* Override Blueprint's _reset.scss styles */")) {
+                document.head.removeChild(styleTag);
+            }
+        });
+}
+
