a43beeb0329447b2325ea85f5bf0f446eb92f4c1
nmearl <contact@nicholasearl.me>
2019-11-18 11:24:01 -0500

    Implement vue goldent layout template

--- jdaviz/widgets/tab_area.vue
+++ jdaviz/widgets/tab_area.vue
@@ -1,5 +1,19 @@
 <template>
   <v-card flat tile class="fill-height" style="height: 100%">
-    <g-goldenlayout />
+    <golden-layout style="height: 200px">
+      <gl-row>
+        <gl-component title="component1">
+          <h1>Component 1</h1>
+        </gl-component>
+        <gl-stack>
+          <gl-component title="component2">
+            <h1>Component 2</h1>
+          </gl-component>
+          <gl-component title="component3">
+            <h1>Component 3</h1>
+          </gl-component>
+        </gl-stack>
+      </gl-row>
+    </golden-layout>
   </v-card>
 </template>
