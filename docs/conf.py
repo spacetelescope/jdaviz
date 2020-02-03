01c88256e3906e107751f33822b995c17d8419e0
nmearl <contact@nicholasearl.me>
2020-02-03 13:12:52 -0500

    Remove unsupported setup config options

--- docs/conf.py
+++ docs/conf.py
@@ -150,6 +150,7 @@ man_pages = [('index', project.lower(), project + u' Documentation',
 
 # -- Options for the edit_on_github extension ---------------------------------
 
+# TODO: this is not supported in the new tox/azure template
 if eval(setup_cfg.get('edit_on_github')):
     extensions += ['sphinx_astropy.ext.edit_on_github']
 
@@ -168,8 +169,8 @@ github_issues_url = 'https://github.com/{0}/issues/'.format(setup_cfg['github_pr
 
 # -- Turn on nitpicky mode for sphinx (to warn about references not found) ----
 #
-# nitpicky = True
-# nitpick_ignore = []
+nitpicky = True
+nitpick_ignore = []
 #
 # Some warnings are impossible to suppress, and you can list specific references
 # that should be ignored in a nitpick-exceptions file which should be inside
