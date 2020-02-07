de0a62ce6e0494cfd3f38c782ec908619af77eaf
nmearl <contact@nicholasearl.me>
2020-02-07 13:34:29 -0500

    Clean up gaussian smoothing behavior

--- jdaviz/configs/default/plugins/gaussian_smoothing/gaussian_smoothing.py
+++ jdaviz/configs/default/plugins/gaussian_smoothing/gaussian_smoothing.py
@@ -4,50 +4,62 @@ from astropy.units import Quantity
 from glue.core import DataCollection
 from glue.core.edit_subset_mode import (AndMode, AndNotMode, OrMode,
                                         ReplaceMode, XorMode)
-from glue.core.message import EditSubsetMessage
+from glue.core.message import (DataCollectionAddMessage,
+                               DataCollectionDeleteMessage, EditSubsetMessage)
 from ipyvuetify import VuetifyTemplate
+from specutils import Spectrum1D
+from specutils.manipulation import gaussian_smooth
 from traitlets import Any, Bool, Dict, Float, Int, List, Unicode
 
 from jdaviz.core.events import DataSelectedMessage, LoadDataMessage
 from jdaviz.core.registries import tools
 from jdaviz.core.template_mixin import TemplateMixin
 from jdaviz.utils import load_template
-from specutils import Spectrum1D
-from specutils.manipulation import gaussian_smooth
 
 __all__ = ['GaussianSmoothingButton']
+
+
+spaxel = u.def_unit('spaxel', 1 * u.Unit(""))
+u.add_enabled_units([spaxel])
 
 
 @tools('g-gaussian-smoothing')
 class GaussianSmoothingButton(TemplateMixin):
     dialog = Bool(False).tag(sync=True)
     template = load_template("gaussian_smoothing.vue", __file__).tag(sync=True)
-    stddev = Float(0).tag(sync=True)
+    stddev = Unicode().tag(sync=True)
+    dc_items = List([]).tag(sync=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.hub.subscribe(self, DataCollectionAddMessage,
+                           handler=self._on_data_updated)
+        self.hub.subscribe(self, DataCollectionDeleteMessage,
+                           handler=self._on_data_updated)
+
+        self._selected_data = None
+
+    def _on_data_updated(self, msg):
+        self.dc_items = [x.label for x in self.data_collection]
+
+    def vue_data_selected(self, event):
+        self._selected_data = next((x for x in self.data_collection
+                                    if x.label == event))
+
     def vue_gaussian_smooth(self, *args, **kwargs):
-        # TODO: hack because of current incompatibility with ipywidget types
-        #  and vuetify templates.
-
-        self.dialog = False
-
-        # Get currently activate data
-        # data = self.session.data_collection
-
         # Testing inputs to make sure putting smoothed spectrum into
         # datacollection works
-        input_flux = Quantity(np.array([0.2, 0.3, 2.2, 0.3]), u.Jy)
-        input_spaxis = Quantity(np.array([1, 2, 3, 4]), u.micron)
-        spec1 = Spectrum1D(input_flux, spectral_axis=input_spaxis)
+        # input_flux = Quantity(np.array([0.2, 0.3, 2.2, 0.3]), u.Jy)
+        # input_spaxis = Quantity(np.array([1, 2, 3, 4]), u.micron)
+        # spec1 = Spectrum1D(input_flux, spectral_axis=input_spaxis)
+        size = float(self.stddev)
+        spec = self._selected_data.get_object(cls=Spectrum1D)
 
         # Takes the user input from the dialog (stddev) and uses it to
         # define a standard deviation for gaussian smoothing
-        spec1_gsmooth = gaussian_smooth(spec1, stddev=self.stddev)
+        spec_smoothed = gaussian_smooth(spec, stddev=size)
 
-        dc = DataCollection()
+        self.data_collection[f"Smoothed {self._selected_data.label}"] = spec_smoothed
 
-        dc['spec1'] = spec1_gsmooth
-
-        self.data_collection.add(dc['spec1'])
+        self.dialog = False
--- jdaviz/configs/default/plugins/gaussian_smoothing/gaussian_smoothing.vue
+++ jdaviz/configs/default/plugins/gaussian_smoothing/gaussian_smoothing.vue
@@ -1,25 +1,41 @@
 <template>
   <v-dialog v-model="dialog" persistent max-width="450">
-    <template v-slot:activator="{ on }">
-      <v-btn v-on="on" icon>
-        <v-icon>adjust</v-icon>
-      </v-btn>
+    <template v-slot:activator="{ on: dialog }">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on: tooltip }">
+          <v-btn v-on="{...tooltip, ...dialog}" icon>
+            <v-icon>adjust</v-icon>
+          </v-btn>
+        </template>
+        <span>Tooltip</span>
+      </v-tooltip>
     </template>
 
     <v-form>
       <v-card>
-        <v-card-title class="headline primary" primary-title>Gaussian Smoothing</v-card-title>
+        <v-card-title class="headline blue lighten-4" primary-title>Gaussian Smoothing</v-card-title>
 
         <v-card-text>
           <v-container>
             <v-row>
               <v-col>
+                <v-select
+                  :items="dc_items"
+                  @change="data_selected"
+                  label="Data"
+                  hint="Select the data set to be smoothed."
+                ></v-select>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
                 <v-text-field
-                  label="Standard Deviation"
-                  @change="stddev = $event"
-                  hint="The stddev of the kernel, in pixels"
+                  ref="stddev"
+                  label="Standard deviation"
+                  v-model="stddev"
+                  hint="The stddev of the kernel, in pixels."
                   persistent-hint
-                  outlined
+                  :rules="[() => !!stddev || 'This field is required']"
                 ></v-text-field>
               </v-col>
             </v-row>
