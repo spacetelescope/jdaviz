672f2683d5b4b3de1e05cce36482a006c625777d
nmearl <contact@nicholasearl.me>
2020-03-19 22:46:42 -0400

    Update dependencies

--- jdaviz/configs/default/plugins/subset_tools/subset_tools.vue
+++ jdaviz/configs/default/plugins/subset_tools/subset_tools.vue
@@ -1,6 +1,6 @@
 <template>
     <v-toolbar-items>
-        <g-subset-select />
-        <g-subset-mode />
+        <g-subset-select></g-subset-select>
+        <g-subset-mode></g-subset-mode>
     </v-toolbar-items>
 </template>
