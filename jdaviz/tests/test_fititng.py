82047a29fee303f9941377862c74f2096d914057
Ivo <busko@stsci.edu>
2020-04-15 12:01:29 -0400

    Basic fitting test

---
+++ jdaviz/tests/test_fititng.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+import astropy.units as u
+import astropy.modeling.models as models
+
+from specutils.spectra import Spectrum1D
+
+from jdaviz.configs.cubeviz.plugins import fitting_backend as fb
+
+
+def test_fitting_backend():
+    np.random.seed(42)
+
+    g1 = models.Gaussian1D(1, 4.6, 0.2)
+    g2 = models.Gaussian1D(2.5, 5.5, 0.1)
+    g3 = models.Gaussian1D(-1.7, 8.2, 0.1)
+    x = np.linspace(0, 10, 200)
+    y = g1(x) + g2(x) + g3(x) + np.random.normal(4., 0.1, x.shape)
+
+    spectrum = Spectrum1D(flux=y*u.Jy, spectral_axis=x*u.um)
+
+    g1f = models.Gaussian1D(0.7*u.Jy, 4.65*u.um, 0.3*u.um, name='g1')
+    g2f = models.Gaussian1D(2.0*u.Jy, 5.55*u.um, 0.3*u.um, name='g2')
+    g3f = models.Gaussian1D(-2.*u.Jy, 8.15*u.um, 0.2*u.um, name='g3')
+    zero_level = models.Const1D(1.*u.Jy, name='const1d')
+
+    model_list = [g1f, g2f, g3f, zero_level]
+    expression = "g1 + g2 + g3 + const1d"
+
+    fm, fitted_spectrum = fb.fit_model_to_spectrum(spectrum, model_list, expression)
+
+    parameters_expected = np.array([1.0104705, 4.58956282, 0.19590464, 2.39892026,
+                                    5.49867754, 0.10834472, -1.66902953, 8.19714439,
+                                    0.09535613, 3.99125545])*u.Jy
+
+    assert np.allclose(fm.parameters, parameters_expected.value, atol=1e-5)
+
+
