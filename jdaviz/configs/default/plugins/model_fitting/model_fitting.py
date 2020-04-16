d31f03d2130dc96e9e9ebf1458cc1dfe89fb1038
Ricky O'Steen <rosteen@stsci.edu>
2020-04-16 11:21:14 -0400

    Starting to flesh out the model fitting dialogue box. Nothing I've added works yet, lots of debugging to do

--- jdaviz/configs/default/plugins/model_fitting/model_fitting.py
+++ jdaviz/configs/default/plugins/model_fitting/model_fitting.py
@@ -1,13 +1,42 @@
+from glue.core.message import (DataCollectionAddMessage,
+                               DataCollectionDeleteMessage)
+from traitlets import Bool, List
+
 from jdaviz.core.registries import tray_registry
 from jdaviz.core.template_mixin import TemplateMixin
 from jdaviz.utils import load_template
+
+from .fitting_backend import fit_model_to_spectrum
 
 __all__ = ['ModelFitting']
 
 
 @tray_registry('g-model-fitting')
 class ModelFitting(TemplateMixin):
+    dialog = Bool(False).tag(sync=True)
     template = load_template("model_fitting.vue", __file__).tag(sync=True)
+    dc_items = List([]).tag(sync=True)
+    # Hard coding this for now, but will want to pull from a config file
+    available_models = List(["Gaussian1D", "Const1D"]).tag(sync=True)
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        self.hub.subscribe(self, DataCollectionAddMessage,
+                           handler=self._on_data_updated)
+        self.hub.subscribe(self, DataCollectionDeleteMessage,
+                           handler=self._on_data_updated)
+
+        self._selected_data = None
+
+    def _on_data_updated(self, msg):
+        self.dc_items = [x.label for x in self.data_collection]
+
+    def vue_data_selected(self, event):
+        self._selected_data = next((x for x in self.data_collection
+                                    if x.label == event))
+
+    def vue_model_fitting(self, *args, **kwargs):
+        # This will be where the model fitting code is run
+        self.dialog = False
--- jdaviz/configs/default/plugins/model_fitting/model_fitting.vue
+++ jdaviz/configs/default/plugins/model_fitting/model_fitting.vue
@@ -1,5 +1,65 @@
 <template>
-    <v-btn>
-        Model Fitting
-    </v-btn>
+  <v-dialog v-model="dialog" persistent max-width="450" @keydown.stop="">
+    <template v-slot:activator="{ on: dialog }">
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on: tooltip }">
+          <v-btn v-on="{...dialog}">
+            Model Fitting
+          </v-btn>
+        </template>
+        <span>Model Fitting</span>
+      </v-tooltip>
+    </template>
+
+    <v-card>
+      <v-card-title class="headline blue lighten-4" primary-title>Model Fitting</v-card-title>
+
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-select
+                :items="dc_items"
+                @change="data_selected"
+                label="Data"
+                hint="Select the data set to be fitted."
+              ></v-select>
+            </v-col>
+        </v-container>
+      </v-card-text>
+      <v-divider></v-divider>
+
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-select
+                :items="available_models"
+                @change="model_selected"
+                label="Model"
+                hint="Select a model to fit"
+              ></v-select>
+            </v-col>
+            <v-col>
+              <v-text-field
+                ref="model_name"
+                label="Name for model"
+                v-model="model_name"
+                hint="A unique name for this model, to use in model equation."
+                persistent-hint
+                :rules="[() => !!model_name || 'This field is required']"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <div class="flex-grow-1"></div>
+        <v-btn color="primary" text @click="dialog = false">Cancel</v-btn>
+        <v-btn color="primary" text @click="model_fitting">Fit</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
