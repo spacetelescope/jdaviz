d20bde7228a16dc09fee6cc414f6a0cffa114816
nmearl <contact@nicholasearl.me>
2020-05-06 09:44:55 -0400

    Use `CCData` as the default returned value in image viewers

--- jdaviz/configs/cubeviz/plugins/viewers.py
+++ jdaviz/configs/cubeviz/plugins/viewers.py
@@ -2,6 +2,7 @@ from glue.core import BaseData
 from glue_jupyter.bqplot.image import BqplotImageView
 from glue_jupyter.bqplot.profile import BqplotProfileView
 from specutils import Spectrum1D
+from astropy.nddata import CCDData
 
 from jdaviz.core.registries import viewer_registry
 
@@ -19,10 +20,10 @@ class CubeVizProfileView(BqplotProfileView):
 
 @viewer_registry("cubeviz-image-viewer", label="Image 2D (CubeViz)")
 class CubeVizImageView(BqplotImageView):
-    default_class = None
+    default_class = CCDData
 
     def data(self, cls=None):
-        return [layer_state.layer
+        return [layer_state.layer.get_object(cls=cls or self.default_class)
                 for layer_state in self.state.layers
                 if hasattr(layer_state, 'layer') and
                 isinstance(layer_state.layer, BaseData)]
