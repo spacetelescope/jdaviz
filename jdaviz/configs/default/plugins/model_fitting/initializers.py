63d135d6631770be7b571f03ef7c8a728a295e0a
Ricky O'Steen <rosteen@stsci.edu>
2020-05-07 11:36:42 -0400

    Fixing name handling in initializers

--- jdaviz/configs/default/plugins/model_fitting/initializers.py
+++ jdaviz/configs/default/plugins/model_fitting/initializers.py
@@ -7,40 +7,44 @@ def get_params(model_dict):
 def get_fixed(model_dict):
     return {x["name"]: x["fixed"] for x in model_dict["parameters"]}
 
-def initialize_gaussian1d(params, fixed):
+def initialize_gaussian1d(params, fixed, name):
     return models.Gaussian1D(amplitude=params["amplitude"],
                              mean=params["mean"],
                              stddev=params["stddev"],
-                             name=model_dict["id"],
+                             name=name,
                              fixed = fixed)
 
-def initialize_const1d(params, fixed):
+def initialize_const1d(params, fixed, name):
     return models.Const1D(amplitude=params["amplitude"],
-                          name=model_dict["id"],
+                          name=name,
                           fixed = fixed)
 
-def initialize_linear1d(params, fixed):
+def initialize_linear1d(params, fixed, name):
     return models.Linear1D(slope=params["slope"],
                            intercept=params["intercept"],
+                           name=name,
                            fixed=fixed)
 
-def initialize_powerlaw1d(params, fixed):
+def initialize_powerlaw1d(params, fixed, name):
     return powerlaws.PowerLaw1D(amplitude=params["amplitude"],
                                 x_0=params["x_0"],
                                 alpha=params["alpha"],
+                                name=name,
                                 fixed=fixed)
 
-def initialize_lorentz1d(params, fixed):
+def initialize_lorentz1d(params, fixed, name):
     return models.Lorentz1D(amplitude=params["amplitude"],
                             x_0=params["x_0"],
                             fwhm=params["fwhm"],
+                            name=name,
                             fixed=fixed)
 
-def initialize_voigt1d(params, fixed):
+def initialize_voigt1d(params, fixed, name):
     return models.Voigt1D(x_0=params["x_0"],
                           amplitude_L=params["amplitude_L"],
                           fwhm_L=params["fwhm_L"],
                           fwhm_G=params["fwhm_G"],
+                          name=name
                           fixed=fixed)
 
 model_initializers = {"Gaussian1D": initialize_gaussian1d,
@@ -62,4 +66,4 @@ model_parameters = {"Gaussian1D": ["amplitude", "stddev", "mean"],
 def initialize_model(model_dict):
     params = get_params(model_dict)
     fixed = get_fixed(model_dict)
-    return model_initializers[model_dict["model_type"]](params, fixed)
+    return model_initializers[model_dict["model_type"]](params, fixed, model_dict["id"])
