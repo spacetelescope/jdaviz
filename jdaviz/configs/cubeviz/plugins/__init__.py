09411cd838becaaacfd9bdfe762adc50bd834b67
nmearl <contact@nicholasearl.me>
2020-06-03 11:57:15 -0400

    Use filename in data label creation

--- jdaviz/configs/cubeviz/plugins/__init__.py
+++ jdaviz/configs/cubeviz/plugins/__init__.py
@@ -1,3 +1,3 @@
 from .viewers import *
 from .unified_slider.unified_slider import *
-from .utils import *
+from .parsers import *
--- jdaviz/configs/cubeviz/plugins/parsers.py
+++ jdaviz/configs/cubeviz/plugins/parsers.py
@@ -3,6 +3,7 @@ from spectral_cube import SpectralCube
 from spectral_cube.io.fits import FITSReadError
 import logging
 import numpy as np
+import os
 from jdaviz.core.registries import data_parser_registry
 
 EXT_TYPES = dict(flux=['flux', 'sci'],
@@ -20,24 +21,17 @@ def parse_data(file_path, app):
     ----------
     file_path : str
         The path to a cube-like data file.
-    data_collection : `~glue.core.DataCollection`
-        The application-level data collection into which the parsed data will
-        be placed.
-
-    Returns
-    -------
-    ext_mapping : dict
-        A mapping of the three necessary components of the cubeviz viewers to
-        the data component labels in glue.
+    app : `~jdaviz.app.Application`
+        The application-level object used to reference the viewers.
     """
-    ext_mapping = {'flux': None,
-                   'uncert': None,
-                   'mask': None}
+    file_name = os.path.basename(file_path)
 
     with fits.open(file_path) as hdulist:
         wcs = None
 
         for hdu in hdulist:
+            data_label = f"{file_name}[{hdu.name}]"
+
             if hdu.data is None:
                 continue
 
@@ -59,22 +53,17 @@ def parse_data(file_path, app):
                 logging.error(e)
                 continue
 
-            app.data_collection[hdu.name] = sc
+            app.data_collection[data_label] = sc
 
             # If the data type is some kind of integer, assume it's the mask/dq
             if hdu.data.dtype in (np.int, np.uint, np.uint32) or \
                     any(x in hdu.name.lower() for x in EXT_TYPES['mask']):
-                ext_mapping['mask'] = hdu.name
-                app.add_data_to_viewer('mask-viewer', hdu.name)
+                app.add_data_to_viewer('mask-viewer', data_label)
 
             if 'errtype' in [x.lower() for x in hdu.header.keys()] or \
                     any(x in hdu.name.lower() for x in EXT_TYPES['uncert']):
-                ext_mapping['uncert'] = hdu.name
-                app.add_data_to_viewer('uncert-viewer', hdu.name)
+                app.add_data_to_viewer('uncert-viewer', data_label)
 
             if any(x in hdu.name.lower() for x in EXT_TYPES['flux']):
-                ext_mapping['flux'] = hdu.name
-                app.add_data_to_viewer('flux-viewer', hdu.name)
-                app.add_data_to_viewer('spectrum-viewer', hdu.name)
-
-    return ext_mapping
+                app.add_data_to_viewer('flux-viewer', data_label)
+                app.add_data_to_viewer('spectrum-viewer', data_label)
