2f9ec35a7fe7be669af12f652b45dd4d5b5f8f8d
nmearl <contact@nicholasearl.me>
2020-06-15 11:21:41 -0400

    Create specific parser functions

--- jdaviz/configs/cubeviz/plugins/parsers.py
+++ jdaviz/configs/cubeviz/plugins/parsers.py
@@ -5,6 +5,7 @@ import logging
 import numpy as np
 import os
 from jdaviz.core.registries import data_parser_registry
+from specutils import Spectrum1D
 
 EXT_TYPES = dict(flux=['flux', 'sci'],
                  uncert=['ivar', 'err', 'var', 'uncert'],
@@ -12,58 +13,104 @@ EXT_TYPES = dict(flux=['flux', 'sci'],
 
 
 @data_parser_registry("cubeviz-data-parser")
-def parse_data(file_path, app):
+def parse_data(app, file_obj, **kwargs):
     """
     Attempts to parse a data file and auto-populate available viewers in
     cubeviz.
 
     Parameters
     ----------
-    file_path : str
-        The path to a cube-like data file.
     app : `~jdaviz.app.Application`
         The application-level object used to reference the viewers.
+    file_path : str
+        The path to a cube-like data file.
     """
-    file_name = os.path.basename(file_path)
+    if isinstance(file_obj, fits.hdu.hdulist.HDUList):
+        _parse_hdu(app, file_obj)
+    elif isinstance(file_obj, str) and os.path.exists(file_obj):
+        file_name = os.path.basename(file_obj)
+        with fits.open(file_obj) as hdulist:
+            hdulist = fits.open(file_obj)
+            _parse_hdu(app, hdulist, file_name, **kwargs)
+    elif isinstance(file_obj, SpectralCube):
+        _parse_spectral_cube(app, file_obj, **kwargs)
+    elif isinstance(file_obj, Spectrum1D):
+        _parse_spectrum1d(app, file_obj, **kwargs)
 
-    with fits.open(file_path) as hdulist:
-        wcs = None
 
-        for hdu in hdulist:
-            data_label = f"{file_name}[{hdu.name}]"
+def _parse_hdu(app, hdulist, file_name=None):
+    if hasattr(hdulist, 'file_name'):
+        file_name = hdulist.file_name
 
-            if hdu.data is None:
-                continue
+    file_name = file_name or "Unknown HDU object"
 
-            # This will fail on attempting to load anything that
-            # isn't cube-shaped
+    wcs = None
+
+    for hdu in hdulist:
+        data_label = f"{file_name}[{hdu.name}]"
+
+        if hdu.data is None:
+            continue
+
+        # This will fail on attempting to load anything that
+        # isn't cube-shaped
+        try:
+            sc = SpectralCube.read(hdu)
+            wcs = sc.wcs
+        except ValueError:
+            # This will fail if the parsing of the wcs does not provide
+            # proper celestial axes
             try:
+                hdu.header.update(wcs.to_header())
                 sc = SpectralCube.read(hdu)
-                wcs = sc.wcs
-            except ValueError:
-                # This will fail if the parsing of the wcs does not provide
-                # proper celestial axes
-                try:
-                    hdu.header.update(wcs.to_header())
-                    sc = SpectralCube.read(hdu)
-                except ValueError as e:
-                    logging.error(e)
-                    continue
-            except FITSReadError as e:
+            except ValueError as e:
                 logging.error(e)
                 continue
+        except FITSReadError as e:
+            logging.error(e)
+            continue
 
-            app.data_collection[data_label] = sc
+        app.data_collection[data_label] = sc
 
-            # If the data type is some kind of integer, assume it's the mask/dq
-            if hdu.data.dtype in (np.int, np.uint, np.uint32) or \
-                    any(x in hdu.name.lower() for x in EXT_TYPES['mask']):
-                app.add_data_to_viewer('mask-viewer', data_label)
+        # If the data type is some kind of integer, assume it's the mask/dq
+        if hdu.data.dtype in (np.int, np.uint, np.uint32) or \
+                any(x in hdu.name.lower() for x in EXT_TYPES['mask']):
+            app.add_data_to_viewer('mask-viewer', data_label)
 
-            if 'errtype' in [x.lower() for x in hdu.header.keys()] or \
-                    any(x in hdu.name.lower() for x in EXT_TYPES['uncert']):
-                app.add_data_to_viewer('uncert-viewer', data_label)
+        if 'errtype' in [x.lower() for x in hdu.header.keys()] or \
+                any(x in hdu.name.lower() for x in EXT_TYPES['uncert']):
+            app.add_data_to_viewer('uncert-viewer', data_label)
 
-            if any(x in hdu.name.lower() for x in EXT_TYPES['flux']):
-                app.add_data_to_viewer('flux-viewer', data_label)
-                app.add_data_to_viewer('spectrum-viewer', data_label)
+        if any(x in hdu.name.lower() for x in EXT_TYPES['flux']):
+            app.add_data_to_viewer('flux-viewer', data_label)
+            app.add_data_to_viewer('spectrum-viewer', data_label)
+
+
+def _parse_spectral_cube(app, file_obj, data_type='flux', data_label=None):
+    data_label = data_label or f"Unknown spectral cube[{data_type.upper()}]"
+
+    app.data_collection[f"{data_label}"] = file_obj.unmasked_data[:, :, :]
+
+    if data_type == 'flux':
+        app.add_data_to_viewer('flux-viewer', f"{data_label}")
+        app.add_data_to_viewer('spectrum-viewer', f"{data_label}")
+    elif data_type == 'mask':
+        app.add_data_to_viewer('mask-viewer', f"{data_label}")
+    elif data_type == 'uncertainty':
+        app.add_data_to_viewer('uncert-viewer', f"{data_label}")
+
+    # TODO: SpectralCube does not store mask information
+    # TODO: SpectralCube does not store data quality information
+
+
+def _parse_spectrum1d(app, file_obj):
+    data_label = "Unknown spectrum object"
+
+    app.data_collection[f"{data_label}[FLUX]"] = file_obj.flux
+    app.add_data_to_viewer('flux-viewer', f"{data_label}[FLUX]")
+    app.add_data_to_viewer('spectrum-viewer', f"{data_label}[FLUX]")
+
+    app.data_collection[f"{data_label}[MASK]"] = file_obj.mask
+    app.add_data_to_viewer('mask-viewer', f"{data_label}[MASK]")
+
+    # TODO: Spectrum1D does not currently store data quality information
