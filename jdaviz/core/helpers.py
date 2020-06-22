0d30921b99af8924366530142a3ce39d7f0bd8b6
Erik Tollerud <erik.tollerud@gmail.com>
2020-06-22 17:24:43 -0400

    add context about the helpers to the docstring

--- jdaviz/core/helpers.py
+++ jdaviz/core/helpers.py
@@ -1,8 +1,26 @@
+"""
+Helper classes are meant to provide a convenient user API for specific
+configurations. They allow a separation of "viztool-specific" API and the glue
+application objects.
+
+See also https://github.com/spacetelescope/jdaviz/issues/104 for more details
+on the motivation behind this concept.
+"""
+
 class ConfigHelper():
     """The Base Helper Class
-    Provides shared abstracted helper methods to the user
+    Provides shared abstracted helper methods to the user.
+
+    Subclasses should set `_default_configuration` if they are meant to be
+    used with a specific configuration.
+
+    Parameters
+    ----------
+    app : jdaviz.app.Application or None
+        The application object, or if None, creates a new one based on the
+        default configuration for this helper.
     """
-    _default_configuration = None
+    _default_configuration = 'default'
 
     def __init__(self, app=None):
         from ..app import Application  # inside to prevent possible circular imports
