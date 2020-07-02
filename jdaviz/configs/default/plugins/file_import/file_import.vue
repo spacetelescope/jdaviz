fd158836f8fc71671af689df6daf2dc1183ae47d
nmearl <contact@nicholasearl.me>
2020-07-02 11:52:17 -0400

    Fix on click method handling

--- jdaviz/configs/default/plugins/file_import/file_import.vue
+++ jdaviz/configs/default/plugins/file_import/file_import.vue
@@ -1,25 +1,23 @@
 <template>
   <v-toolbar-items>
-    <v-btn tile depressed color="primary" @click="onClicked">
+    <v-btn tile depressed color="accent" @click="onClicked">
       Import
-      <div v-show=false id="file-uploader">
-        <g-file-import></g-file-import>
-      </div>
       <v-icon right>mdi-plus</v-icon>
     </v-btn>
+    <g-file-import v-show=false id="file-uploader"></g-file-import>
   </v-toolbar-items>
 </template>
 
 <script>
-  module.exports = {
-    name: "g-file-import",
-    props: [],
+    module.exports = {
+        name: "g-file-import",
+        props: [],
 
-    methods: {
-      onClicked(event) {
-        let uploadButton = document.getElementById("file-uploader").firstElementChild.children[0];
-        uploadButton.click();
-      },
-    },
-  };
+        methods: {
+            onClicked(event) {
+                let uploadButton = document.getElementById("file-uploader").firstElementChild;
+                uploadButton.click();
+            },
+        },
+    };
 </script>
