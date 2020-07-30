f88711cf09dd38fee121f908abffcd2079b7333a
Nicholas Earl <contact@nicholasearl.me>
2020-07-30 12:08:11 -0400

    Expand helper load method docstrings

--- jdaviz/configs/mosviz/helper.py
+++ jdaviz/configs/mosviz/helper.py
@@ -6,13 +6,56 @@ class MosViz(ConfigHelper):
     _default_configuration = 'mosviz'
 
     def load_1d_spectra(self, data_obj, data_labels=None):
+        """
+        Load and parse a set of 1D spectra objects.
+
+        Parameters
+        ----------
+        data_obj : list or str
+            A list of spectra as translatable container objects (e.g.
+            ``Spectrum1D``) that can be read by glue-jupyter. Alternatively,
+            can be a string file path.
+        data_labels : str or list
+            String representing the label for the data item loaded via
+            ``data_obj``. Can be a list of strings representing data labels
+            for each item in ``data_obj`` if  ``data_obj`` is a list.
+        """
         self.app.load_data(data_obj, parser_reference="mosviz-spec1d-parser",
                            data_labels=data_labels)
 
     def load_2d_spectra(self, data_obj, data_labels=None):
+        """
+        Load and parse a set of 2D spectra objects.
+
+        Parameters
+        ----------
+        data_obj : list or str
+            A list of 2D spectra as translatable container objects (e.g.
+            ``Spectrum1D``) that can be read by glue-jupyter. Alternatively,
+            can be a string file path.
+        data_labels : str or list
+            String representing the label for the data item loaded via
+            ``data_obj``. Can be a list of strings representing data labels
+            for each item in ``data_obj`` if  ``data_obj`` is a list.
+        """
         self.app.load_data(data_obj, parser_reference="mosviz-spec2d-parser",
                            data_labels=data_labels)
 
     def load_images(self, data_obj, data_labels=None):
+        """
+        Load and parse a set of image objects. If providing a file path, it
+        must be readable by ``CCDData`` io registries.
+
+        Parameters
+        ----------
+        data_obj : list or str
+            A list of spectra as translatable container objects (e.g.
+            ``CCDData``) that can be read by glue-jupyter. Alternatively,
+            can be a string file path.
+        data_labels : str or list
+            String representing the label for the data item loaded via
+            ``data_obj``. Can be a list of strings representing data labels
+            for each item in ``data_obj`` if  ``data_obj`` is a list.
+        """
         self.app.load_data(data_obj, parser_reference="mosviz-image-parser",
                            data_labels=data_labels)
