1306042d335d550a689e346479ee8fa843c7ddb0
Ricky O'Steen <rosteen@stsci.edu>
2020-08-03 16:25:13 -0400

    Commit initial files

---
+++ jdaviz/configs/cubeviz/plugins/moment_maps/moment_maps.py
@@ -0,0 +1,41 @@
+from astropy import units as u
+from glue.core.message import (DataCollectionAddMessage,
+                               DataCollectionDeleteMessage)
+from traitlets import List, Unicode, Int, Any, observe
+
+from jdaviz.core.events import SnackbarMessage
+from jdaviz.core.registries import tray_registry
+from jdaviz.core.template_mixin import TemplateMixin
+from jdaviz.utils import load_template
+
+__all__ = ['MomentMaps']
+
+
+spaxel = u.def_unit('spaxel', 1 * u.Unit(""))
+u.add_enabled_units([spaxel])
+
+
+@tray_registry('cubeviz-moment-maps', label="Moment Maps")
+class GaussianSmooth(TemplateMixin):
+    template = load_template("moment_maps.vue", __file__).tag(sync=True)
+    stddev = Any().tag(sync=True)
+    dc_items = List([]).tag(sync=True)
+    selected_data = Unicode().tag(sync=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.hub.subscribe(self, DataCollectionAddMessage,
+                           handler=self._on_data_updated)
+        self.hub.subscribe(self, DataCollectionDeleteMessage,
+                           handler=self._on_data_updated)
+
+        self._selected_data = None
+
+    def _on_data_updated(self, msg):
+        self.dc_items = [x.label for x in self.data_collection]
+
+    @observe("selected_data")
+    def _on_data_selected(self, event):
+        self._selected_data = next((x for x in self.data_collection
+                                    if x.label == event['new']))
---
+++ jdaviz/configs/cubeviz/plugins/moment_maps/moment_maps.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card flat tile>
+    <v-container>
+      <v-row>
+        <v-col class="py-0">
+          <v-select
+            :items="dc_items"
+            v-model="selected_data"
+            label="Data"
+            hint="Select the data set."
+            persistent-hint
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+      </v-row>
+    </v-container>
+    <!-- <v-divider></v-divider> -->
+
+    <v-card-actions>
+    </v-card-actions>
+  </v-card>
+</template>
