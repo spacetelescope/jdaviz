d9a441e1e392786d4a0aceb48aa322cee0e58763
Ricky O'Steen <rosteen@stsci.edu>
2020-08-20 09:28:07 -0400

    Add a basic test to make sure spatial smoothing runs

--- jdaviz/configs/default/plugins/gaussian_smooth/tests/test_gaussian_smooth.py
+++ jdaviz/configs/default/plugins/gaussian_smooth/tests/test_gaussian_smooth.py
@@ -2,13 +2,14 @@ import numpy as np
 
 from glue.core import Data
 from jdaviz import Application
+from spectral_cube import SpectralCube
 
 from ..gaussian_smooth import GaussianSmooth
 
 
 def test_linking_after_spectral_smooth(spectral_cube_wcs):
 
-    app = Application()
+    app = Application(configuration="cubeviz")
     dc = app.data_collection
     dc.append(Data(x=np.ones((3, 4, 5)), label='test', coords=spectral_cube_wcs))
 
@@ -24,3 +25,18 @@ def test_linking_after_spectral_smooth(spectral_cube_wcs):
 
     assert dc.external_links[0].cids1[0] is dc[0].world_component_ids[0]
     assert dc.external_links[0].cids2[0] is dc[1].world_component_ids[0]
+
+def test_spatial_convolution(spectral_cube_wcs):
+
+    app = Application()
+    dc = app.data_collection
+    dc.append(Data(x=np.ones((3, 4, 5)), label='test', coords=spectral_cube_wcs))
+
+    gs = GaussianSmooth(app=app)
+    gs._on_data_selected({'new': 'test'})
+    gs.stddev = '3'
+    gs.vue_spatial_convolution()
+
+    assert len(dc) == 2
+    assert dc[1].label == "Smoothed test"
+    assert dc["Smoothed test"].get_object(cls=SpectralCube).shape == (3,4,5)
