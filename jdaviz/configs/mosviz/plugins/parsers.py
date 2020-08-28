cc3f5a2293f987512c7a6ed432af1e04002a2b73
Duy Nguyen <dtn5ah@virginia.edu>
2020-08-28 14:58:18 -0400

    Undo autoformatting

--- jdaviz/configs/mosviz/plugins/parsers.py
+++ jdaviz/configs/mosviz/plugins/parsers.py
@@ -29,17 +29,17 @@ def _add_to_table(app, data, comp_label):
         The label used to describe the data. Also used as the column header.
     """
     # Add data to the mos viz table object
-    if "MOS Table" not in app.data_collection:
-        table_data = Data(label="MOS Table")
+    if 'MOS Table' not in app.data_collection:
+        table_data = Data(label='MOS Table')
         app.data_collection.append(table_data)
 
-        mos_table = app.data_collection["MOS Table"]
+        mos_table = app.data_collection['MOS Table']
         mos_table.add_component(data, comp_label)
 
         viewer = app.get_viewer("table-viewer")
         viewer.add_data(table_data)
     else:
-        mos_table = app.data_collection["MOS Table"]
+        mos_table = app.data_collection['MOS Table']
         mos_table.add_component(data, comp_label)
 
 
@@ -71,7 +71,7 @@ def mos_spec1d_parser(app, data_obj, data_labels=None):
 
     # Coerce into list-like object. This works because `Spectrum1D` objects
     #  don't have a length dunder method.
-    if not hasattr(data_obj, "__len__"):
+    if not hasattr(data_obj, '__len__'):
         data_obj = [data_obj]
     else:
         data_obj = [Spectrum1D.read(x)
@@ -87,7 +87,7 @@ def mos_spec1d_parser(app, data_obj, data_labels=None):
     for i in range(len(data_obj)):
         app.data_collection[data_labels[i]] = data_obj[i]
 
-    _add_to_table(app, data_labels, "1D Spectra")
+    _add_to_table(app, data_labels, '1D Spectra')
 
 
 @data_parser_registry("mosviz-spec2d-parser")
@@ -118,13 +118,13 @@ def mos_spec2d_parser(app, data_obj, data_labels=None):
         with fits.open(path) as hdulist:
             data = hdulist[1].data
 
-            if hdulist[1].header["NAXIS"] == 2:
+            if hdulist[1].header['NAXIS'] == 2:
                 new_data = np.expand_dims(data, axis=1)
-                hdulist[1].header["NAXIS"] = 3
+                hdulist[1].header['NAXIS'] = 3
 
-            hdulist[1].header["NAXIS3"] = 1
-            hdulist[1].header["BUNIT"] = "dN/s"
-            hdulist[1].header["CUNIT3"] = "um"
+            hdulist[1].header['NAXIS3'] = 1
+            hdulist[1].header['BUNIT'] = "dN/s"
+            hdulist[1].header['CUNIT3'] = "um"
             wcs = WCS(hdulist[1].header)
 
         return SpectralCube(new_data, wcs=wcs)
@@ -151,7 +151,7 @@ def mos_spec2d_parser(app, data_obj, data_labels=None):
     for i in range(len(data_obj)):
         app.data_collection[data_labels[i]] = data_obj[i]
 
-    _add_to_table(app, data_labels, "2D Spectra")
+    _add_to_table(app, data_labels, '2D Spectra')
 
 
 @data_parser_registry("mosviz-image-parser")
@@ -174,11 +174,10 @@ def mos_image_parser(app, data_obj, data_labels=None):
     #  raise an error.
     def _parse_as_image(path):
         with fits.open(path) as hdulist:
-            if "BUNIT" not in hdulist[0].header:
-                logging.warning(
-                    "No 'BUNIT' defined in the header, using 'Jy'.")
+            if 'BUNIT' not in hdulist[0].header:
+                logging.warning("No 'BUNIT' defined in the header, using 'Jy'.")
 
-            unit = hdulist[0].header.get("BUNIT", "Jy")
+            unit = hdulist[0].header.get('BUNIT', 'Jy')
 
         return CCDData.read(path, unit=unit)
 
@@ -186,7 +185,7 @@ def mos_image_parser(app, data_obj, data_labels=None):
         data_obj = [_parse_as_image(data_obj)]
 
     # Coerce into list-like object
-    if not hasattr(data_obj, "__len__"):
+    if not hasattr(data_obj, '__len__'):
         data_obj = [data_obj]
     else:
         data_obj = [_parse_as_image(x)
@@ -218,7 +217,7 @@ def mos_meta_parser(app, data_obj):
     """
 
     # Coerce into list-like object
-    if not hasattr(data_obj, "__len__"):
+    if not hasattr(data_obj, '__len__'):
         data_obj = [data_obj]
     else:
         data_obj = [fits.open(x) if _check_is_file(x)
