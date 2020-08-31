491b6d209b9ebf0ccbceeabfb12136b359441706
Jesse Averbukh <javerbukh@stsci.edu>
2020-08-31 13:58:54 -0400

    Minor change to button alignment

--- jdaviz/configs/mosviz/plugins/slit_overlay/slit_overlay.vue
+++ jdaviz/configs/mosviz/plugins/slit_overlay/slit_overlay.vue
@@ -3,15 +3,14 @@
     <!-- <v-divider></v-divider> -->
 
     <v-card-actions>
-      <div class="flex-grow-1"></div>
-      <v-btn
-      color="primary" text @click="slit_overlay">Apply</v-btn>
-    </v-card-actions>
-
-    <v-card-actions>
-      <div class="flex-grow-1"></div>
-      <v-btn
-      color="primary" text @click="slit_overlay_remove">Remove</v-btn>
+      <div class="flex-grow-1">
+        <v-row align="center" justify="center">
+          <v-btn
+            color="primary" text @click="slit_overlay">Apply</v-btn>
+          <v-btn
+            color="primary" text @click="slit_overlay_remove">Remove</v-btn>
+        </v-row>
+      </div>
     </v-card-actions>
 
   </v-card>
