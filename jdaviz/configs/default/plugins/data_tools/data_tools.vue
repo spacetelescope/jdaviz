d584cf06b5a29e586d6fabd19f64d29709dd2628
Ivo <busko@stsci.edu>
2020-11-19 10:47:57 -0500

    Remove commented-out code

--- jdaviz/configs/default/plugins/data_tools/data_tools.vue
+++ jdaviz/configs/default/plugins/data_tools/data_tools.vue
@@ -10,7 +10,6 @@
 
       <v-card>
         <v-card-title class="headline" color="primary" primary-title>Import Data</v-card-title>
-
         <v-card-text>
           <v-container>
             <v-row>
@@ -27,42 +26,14 @@
           </v-container>
         </v-card-text>
         <!-- <v-divider></v-divider> -->
-
         <v-card-actions>
           <div class="flex-grow-1"></div>
           <v-btn color="primary" text @click="dialog = false">Cancel</v-btn>
           <v-btn color="primary" text @click="load_data" :disabled="!valid_path">Import</v-btn>
         </v-card-actions>
       </v-card>
+
     </v-dialog>
-
-<!--
-    <v-tooltip bottom>
-      <template v-slot:activator="{ on }">
-        <v-btn disabled v-on="on" icon tile>
-          <v-icon>mdi-format-superscript</v-icon>
-        </v-btn>
-      </template>
-      <span>Arithmetic Editor</span>
-    </v-tooltip>
-    <v-tooltip bottom>
-      <template v-slot:activator="{ on }">
-        <v-btn disabled v-on="on" icon tile>
-          <v-icon>mdi-export</v-icon>
-        </v-btn>
-      </template>
-      <span>Export Data</span>
-    </v-tooltip>
-    <v-tooltip bottom>
-      <template v-slot:activator="{ on }">
-        <v-btn disabled v-on="on" icon tile>
-          <v-icon>mdi-delete</v-icon>
-        </v-btn>
-      </template>
-      <span>Delete Data</span>
-    </v-tooltip>
--->
-
     <!-- <v-divider vertical></v-divider> -->
   </v-toolbar-items>
 </template>
