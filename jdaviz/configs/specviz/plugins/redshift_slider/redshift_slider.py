8a66f16ebc7634bd715b46d87fc43f58d763bc9a
Ricky O'Steen <rosteen@stsci.edu>
2020-12-14 17:21:43 -0500

    Adding backend for RV/z switching
    Update slider value when a new spectrum is added
    
    Trying to get switching between redshift/RV working
    
    Add missing import
    
    Change default slider min to 0, split out bounds updating, trying to separate text field update handling

--- jdaviz/configs/specviz/plugins/redshift_slider/redshift_slider.py
+++ jdaviz/configs/specviz/plugins/redshift_slider/redshift_slider.py
@@ -1,7 +1,9 @@
 from traitlets import Bool, Float, observe, Any, Int
 import astropy.units as u
+from astropy.constants import c
 from specutils import SpectralAxis
 from glue_astronomy.spectral_coordinates import SpectralCoordinates
+import numpy as np
 
 from jdaviz.core.events import AddDataMessage
 from jdaviz.core.registries import tool_registry
@@ -17,10 +19,11 @@ __all__ = ['RedshiftSlider']
 class RedshiftSlider(TemplateMixin):
     template = load_template("redshift_slider.vue", __file__).tag(sync=True)
     slider = Any(0).tag(sync=True)
+    slider_textbox = Any(0).tag(sync=True)
     slider_type = Any("Redshift").tag(sync=True)
-    min_value = Float(-1).tag(sync=True)
+    min_value = Float(0).tag(sync=True)
     max_value = Float(1).tag(sync=True)
-    slider_step = Float(0.01).tag(sync=True)
+    slider_step = Float(0.001).tag(sync=True)
     linked = Bool(True).tag(sync=True)
     wait = Int(100).tag(sync=True)
 
@@ -36,7 +39,25 @@ class RedshiftSlider(TemplateMixin):
         if isinstance(msg.viewer, BqplotProfileView):
             print(msg.data.get_object())
             label = msg.data.label
-            temp_data = msg.viewer_id
+            temp_data = self.app.get_data_from_viewer("spectrum-viewer")[label]
+            if self.slider_type == "Redshift":\
+                self.slider = temp_data.redshift.value
+            else:
+                self.slider = temp_data.radial_velocity.to("km/s").value
+
+    def _velocity_to_redshift(self, velocity):
+        """
+        Convert a velocity to a relativistic redshift.
+        """
+        beta = velocity / c
+        return np.sqrt((1 + beta) / (1 - beta)) - 1
+
+    def _redshift_to_velocity(self, redshift):
+        """
+        Convert a relativistic redshift to a velocity.
+        """
+        zponesq = (1 + redshift) ** 2
+        return c * (zponesq - 1) / (zponesq + 1)
 
     def _propagate_redshift(self):
         """
@@ -51,16 +72,39 @@ class RedshiftSlider(TemplateMixin):
 
         for data_item in self.app.data_collection:
             if type(data_item.coords.spectral_axis) == SpectralAxis:
-                new_axis = SpectralAxis(data_item.coords.spectral_axis,
-                                                 redshift = self.slider)
+                if self.slider_type == "Redshift":
+                    new_axis = SpectralAxis(data_item.coords.spectral_axis,
+                                            redshift = self.slider)
+                else:
+                    new_axis = SpectralAxis(data_item.coords.spectral_axis,
+                                radial_velocity = u.Quantity(self.slider, "km/s"))
                 data_item.coords = SpectralCoordinates(new_axis)
-            #if "redshift" in data_item.meta:
-            #    data_item.meta["redshift"] = self.slider
 
+    #def _slider_value_updated(self, value):
+    #    if len(value) > 0:
+    #        self.slider = float(value[0])
 
-    def _slider_value_updated(self, value):
-        if len(value) > 0:
-            self.slider = float(value[0])
+    def _update_bounds_redshift(self, new_val):
+        if new_val > 0 and new_val - 0.5 < 0:
+            self.min_value = 0
+        else:
+            self.min_value = new_val - 0.5
+        self.max_value = new_val + 0.5
+        self.slider_step = 0.001
+
+    def _update_bounds_rv(self, new_val):
+        self.min_value = new_val - (new_val / 100.0)
+        self.max_value = new_val + (new_val / 100.0)
+        self.slider_step = int(new_val / 10000.0)
+
+    def vue_textbox_change(self, event):
+        val = float(event)
+        if self.slider_type == "Redshift":
+            self._update_bounds_redshift(val)
+        else:
+            self._update_bounds_rv(val)
+        self.slider = val
+        print(self.slider)
 
     @observe('slider')
     def _on_slider_updated(self, event):
@@ -68,4 +112,16 @@ class RedshiftSlider(TemplateMixin):
             value = 0
         else:
             value = event['new']
+        self.slider_textbox = value
         self._propagate_redshift()
+
+    @observe('slider_type')
+    def _on_type_updated(self, event):
+        if event['new'] == "Redshift":
+            new_val = self._velocity_to_redshift(u.Quantity(self.slider, "km/s")).value
+            self._update_bounds_redshift(new_val)
+            self.slider = new_val
+        else:
+            new_val = self._redshift_to_velocity(self.slider).to('km/s').value
+            self._update_bounds_rv(new_val)
+            self.slider = new_val
