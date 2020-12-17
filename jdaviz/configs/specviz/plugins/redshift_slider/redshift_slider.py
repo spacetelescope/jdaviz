098865313823fccee16bbf206d38f67be16f5daa
Ricky O'Steen <rosteen@stsci.edu>
2020-12-17 13:19:32 -0500

    Update bounds if needed when loading new data, comment out Data update while debugging other things
    Now updates bounds and slider value such that the slider value is never out of bounds
    
    Improve conversion to radial velocity

--- jdaviz/configs/specviz/plugins/redshift_slider/redshift_slider.py
+++ jdaviz/configs/specviz/plugins/redshift_slider/redshift_slider.py
@@ -40,10 +40,16 @@ class RedshiftSlider(TemplateMixin):
             print(msg.data.get_object())
             label = msg.data.label
             temp_data = self.app.get_data_from_viewer("spectrum-viewer")[label]
-            if self.slider_type == "Redshift":\
-                self.slider = temp_data.redshift.value
+            if self.slider_type == "Redshift":
+                new_z = temp_data.redshift.value
+                if new_z < self.min_value or new_z > self.max_value:
+                    self._update_bounds_redshift(new_z)
+                self.slider = new_z
             else:
-                self.slider = temp_data.radial_velocity.to("km/s").value
+                new_rv = temp_data.radial_velocity.to("km/s").value
+                if new_rv < self.min_value or new_rv > self.max_value:
+                    self._update_bounds_rv(new_rv)
+                self.slider = new_rv
 
     def _velocity_to_redshift(self, velocity):
         """
@@ -66,10 +72,16 @@ class RedshiftSlider(TemplateMixin):
         """
         line_list = self.app.get_viewer('spectrum-viewer').spectral_lines
         if line_list is not None:
-            line_list["redshift"]= u.Quantity(self.slider)
+            if self.slider_type == "Redshift":
+                line_list["redshift"] = u.Quantity(self.slider)
+            else:
+                z = self._velocity_to_redshift(u.Quantity(self.slider, "km/s"))
+                line_list["redshift"] = z
+                print(z)
             # Replot with the new redshift
             line_list = self.app.get_viewer('spectrum-viewer').plot_spectral_lines()
 
+        '''
         for data_item in self.app.data_collection:
             if type(data_item.coords.spectral_axis) == SpectralAxis:
                 if self.slider_type == "Redshift":
@@ -79,23 +91,56 @@ class RedshiftSlider(TemplateMixin):
                     new_axis = SpectralAxis(data_item.coords.spectral_axis,
                                 radial_velocity = u.Quantity(self.slider, "km/s"))
                 data_item.coords = SpectralCoordinates(new_axis)
+        '''
 
     #def _slider_value_updated(self, value):
     #    if len(value) > 0:
     #        self.slider = float(value[0])
 
-    def _update_bounds_redshift(self, new_val):
-        if new_val > 0 and new_val - 0.5 < 0:
-            self.min_value = 0
+    def _set_bounds_orderly(self, new_min, new_max, new_val):
+        '''Have to do this in the right order so our slider value is never out of bounds'''
+        if new_val > self.max_value:
+            self.max_value = new_max
+            self.slider = new_val
+            self.min_value = new_min
+        elif new_val < self.min_value:
+            self.min_value = new_min
+            self.slider = new_val
+            self.max_value = new_max
         else:
-            self.min_value = new_val - 0.5
-        self.max_value = new_val + 0.5
+            self.min_value = new_min
+            self.max_value = new_max
+
+    def _update_bounds_redshift(self, new_val):
+        '''Set reasonable slider parameters based on manually set redshift'''
+        if new_val > 0 and new_val - 0.5 < 0:
+            new_min = 0
+        else:
+            new_min = new_val - 0.5
+        new_max = new_val + 0.5
+
+        self._set_bounds_orderly(new_min, new_max, new_val)
+
         self.slider_step = 0.001
 
     def _update_bounds_rv(self, new_val):
-        self.min_value = new_val - (new_val / 100.0)
-        self.max_value = new_val + (new_val / 100.0)
-        self.slider_step = int(new_val / 10000.0)
+        '''Set reasonable slider parameters based on manually set radial velocity'''
+        if new_val >= 0 and new_val < 100000:
+            new_min = 0
+            new_max = new_val + 100000
+            step = 500
+        elif new_val < 0 and new_val > -100000:
+            new_min = new_val-100000
+            new_max = 0
+            step = 500
+        else:
+            new_min = new_val - (new_val / 100.0)
+            new_max = new_val + (new_val / 100.0)
+            step = new_cal / 10000.0
+
+        self._set_bounds_orderly(new_min, new_max, new_val)
+
+        self.slider_step = step
 
     def vue_textbox_change(self, event):
         val = float(event)
