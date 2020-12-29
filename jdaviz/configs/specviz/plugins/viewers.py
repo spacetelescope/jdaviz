766cd28ef0ece56dfd3fd39ec578c7a14b8bab9f
Ricky O'Steen <rosteen@stsci.edu>
2020-12-29 11:30:41 -0500

    Fix setting show column to False when hiding lists

--- jdaviz/configs/specviz/plugins/viewers.py
+++ jdaviz/configs/specviz/plugins/viewers.py
@@ -163,9 +163,14 @@ class SpecvizProfileView(BqplotProfileView):
                 self.spectral_lines["show"] = False
         else:
             temp_marks = []
-            # Toggle "show" value in main astropy table
+            # Toggle "show" value in main astropy table. The astropy table
+            # machinery only allows updating a single row at a time.
             if name_rest is not None:
-                self.spectral_lines.loc[name_rest]["show"] = False
+                if type(name_rest) == str:
+                    self.spectral_lines.loc[name_rest]["show"] = False
+                elif type(name_rest) == list:
+                    for nr in name_rest:
+                        self.spectral_lines.loc[nr]["show"] = False
             # Get rid of the marks we no longer want
             for x in fig.marks:
                 if type(x) == SpectralLine:
@@ -174,7 +179,6 @@ class SpecvizProfileView(BqplotProfileView):
                         if x.name == name:
                             continue
                     else:
-                        self.spectral_lines.loc[name_rest]["show"] = False
                         if type(name_rest) == str:
                             if x.table_index == name_rest:
                                 continue
