5253516716dd0987acf054ec4c0f987bf6da50a0
Ricky O'Steen <rosteen@stsci.edu>
2021-01-25 13:40:56 -0500

    Adding parser to create 1d spec from 2d input

--- jdaviz/configs/specviz2d/plugins/parsers.py
+++ jdaviz/configs/specviz2d/plugins/parsers.py
@@ -72,7 +72,37 @@ def spec2d_parser(app, data_obj, data_label=None):
         data_obj = _parse_as_cube(data_obj)
 
     if data_label is None:
-        data_labels = f"2D Spectrum"
+        data_label = f"Spectrum 2D"
+    else:
+        data_label = "{} 2D".format(data_label)
 
     app.data_collection[data_label] = data_obj
+
+    print(data_label)
+    return data_label
+
+@data_parser_registry("spec2d-1d-parser")
+def spec2d_1d_parser(app, data_obj, data_label=None):
+    """
+    Attempts to parse a 2D spectrum object.
+
+    Notes
+    -----
+    This currently only works with JWST-type data in which the data is in the
+    second hdu of the fits file.
+
+    Parameters
+    ----------
+    app : `~jdaviz.app.Application`
+        The application-level object used to reference the viewers.
+    data_obj : str or list or spectrum-like
+        File path, list, or spectrum-like object to be read as a new row in
+        the mosviz table.
+    data_labels : str, optional
+        The label applied to the glue data component.
+    """
+    if _check_is_file(data_obj):
+        with fits.open(path) as hdulist:
+            data = hdulist[1].data
+            header = hdulist[1].header
 
