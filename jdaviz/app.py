95f3e8e201ee105cf436a3a1f69543b6e2003d41
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2021-02-09 12:46:48 +0100

    fix: prevent update loop when (de)selecting data checkboxes quickly
    Fixes the toggeling checkboxes part of #185

--- jdaviz/app.py
+++ jdaviz/app.py
@@ -873,12 +873,10 @@ class Application(VuetifyTemplate, HubListener):
 
     def _update_selected_data_items(self, viewer_id, selected_items):
         # Find the active viewer
-        viewer_item = self._viewer_item_by_id(viewer_id)
         viewer = self._viewer_by_id(viewer_id)
 
         # Update the stored selected data items
-        viewer_item['selected_data_items'] = selected_items
-        data_ids = viewer_item['selected_data_items']
+        data_ids = selected_items
 
         active_data_labels = []
 
