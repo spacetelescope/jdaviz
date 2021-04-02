1a11a8e6c8b9c05c2ace8dcc32b6fa4235371516
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-04-02 21:40:55 +0100

    Simplify imviz tools, make coordinate overlay and blinking be 'always-on'

--- jdaviz/configs/imviz/plugins/tools.py
+++ jdaviz/configs/imviz/plugins/tools.py
@@ -1,5 +1,4 @@
-from bqplot import Label
-from bqplot_image_gl.interacts import MouseInteraction
+from echo import delay_callback
 from glue.config import viewer_tool
 from glue_jupyter.bqplot.common.tools import InteractCheckableTool
 
@@ -7,63 +6,33 @@ __all__ = []
 
 
 @viewer_tool
-class BqplotCoordsOverlay(InteractCheckableTool):
+class BqplotMatchWCS(InteractCheckableTool):
 
-    # TODO: This class should be moved 'up' into glue-jupyter - however at the
-    # moment it is specific to celestial WCSes, so this would need to be
-    # generalized, potentially providing hooks to register custom formatting
-    # functions. In addition at the moment it will show the data value from the
-    # first layer, so we should figure out how we want this to behave when
-    # multiple layers are present, sometimes with some degree of transparency
-    # between layers.
-
-    # TODO: design a custom icon for this tool
-    icon = 'glue_crosshair'
-    tool_id = 'bqplot:coords'
-    action_text = 'Show cursor overlay'
-    tool_tip = 'Move cursor on image to see coordinates and data overlay'
+    icon = 'glue_link'
+    tool_id = 'bqplot:matchwcs'
+    action_text = 'Match WCS between images'
+    tool_tip = 'Click on image to have the other image viewer show the same coordinates'
 
     def __init__(self, viewer, **kwargs):
 
         super().__init__(viewer, **kwargs)
 
-        self.label = Label(x=[0.05], y=[0.05], text=[''], default_size=12, colors=['orange'])
-        viewer.figure.marks = viewer.figure.marks + [self.label]
+    def activate(self):
+        self.viewer.add_event_callback(self.on_mouse_or_key_event)
 
-        self.interact = MouseInteraction(x_scale=viewer.figure.axes[0].scale,
-                                         y_scale=viewer.figure.axes[1].scale, move_throttle=70)
-        self.interact.on_msg(self.on_event)
+    def deactivate(self):
+        self.viewer.remove_event_callback(self.on_mouse_or_key_event)
 
-    def on_event(self, interaction, data, buffers):
-
-        # For now use the first dataset in the viewer
-        # FIXME: decide how to generalize this
-        image = self.viewer.state.layers[0].layer
-
-        # Extract data coordinates - these are pixels in the image
-        x = data['domain']['x']
-        y = data['domain']['y']
-
-        overlay = f'x={x:.1f} y={y:.1f}'
-
-        # Convert these to a SkyCoord via WCS - note that for other datasets
-        # we aren't actually guaranteed to get a SkyCoord out, just for images
-        # with valid celestial WCS
-        celestial_coordinates = image.coords.pixel_to_world(x, y).icrs.to_string('hmsdms')
-        overlay += f' ICRS={celestial_coordinates}'
-
-        # Extract data values at this position
-        if x > -0.5 and y > -0.5 and x < image.shape[1] and y < image.shape[0]:
-            value = image.get_data(image.main_components[0])[int(round(y)), int(round(x))]
-            overlay += f' data={value:.2g}'
-
-        # For now we just show the coordinates but it would be easy to show the data
-        # values for one or more of the images
-
-        if data['event'] == 'mousemove':
-            self.label.text = [overlay]
-        elif data['event'] == 'mouseleave':
-            self.label.text = ""
-        elif data['event'] == 'mouseenter':
-            self.label.text = ""
+    def on_mouse_or_key_event(self, data):
+        if data['event'] == 'click':
+            x = data['domain']['x']
+            y = data['domain']['y']
+            dx = self.viewer.state.x_max - self.viewer.state.x_min
+            dy = self.viewer.state.y_max - self.viewer.state.y_min
+            for viewer in self.viewer.session.application.viewers:
+                with delay_callback(viewer.state, 'x_min', 'x_max', 'y_min', 'y_max'):
+                    viewer.state.x_min = x - dx / 2
+                    viewer.state.x_max = x + dx / 2
+                    viewer.state.y_min = y - dy / 2
+                    viewer.state.y_max = y + dy / 2
 
--- jdaviz/configs/imviz/plugins/viewers.py
+++ jdaviz/configs/imviz/plugins/viewers.py
@@ -1,6 +1,8 @@
 from glue.core import BaseData
 from glue_jupyter.bqplot.image import BqplotImageView
 
+from bqplot import Label
+
 from jdaviz.core.registries import viewer_registry
 
 __all__ = ['ImVizImageView']
@@ -9,9 +11,70 @@ __all__ = ['ImVizImageView']
 @viewer_registry("imviz-image-viewer", label="Image 2D (ImViz)")
 class ImVizImageView(BqplotImageView):
 
-    tools = ['bqplot:panzoom', 'bqplot:rectangle', 'bqplot:circle', 'bqplot:coords']
+    tools = ['bqplot:panzoom', 'bqplot:rectangle', 'bqplot:circle', 'bqplot:matchwcs']
 
     default_class = None
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.label_mouseover = Label(x=[0.05], y=[0.05], text=[''], default_size=12, colors=['orange'])
+        self.figure.marks = self.figure.marks + [self.label_mouseover]
+
+        self.add_event_callback(self.on_mouse_or_key_event)
+
+    def on_mouse_or_key_event(self, data):
+
+        if data['event'] == 'mousemove':
+
+            # Display the current cursor coordinates (both pixel and world) as
+            # well as data values. For now we use the first dataset in the
+            # viewer for the data values.
+
+            # Extract data coordinates - these are pixels in the image
+            x = data['domain']['x']
+            y = data['domain']['y']
+
+            overlay = f'x={x:.1f} y={y:.1f}'
+
+            image = self.state.layers[0].layer
+
+            # Convert these to a SkyCoord via WCS - note that for other datasets
+            # we aren't actually guaranteed to get a SkyCoord out, just for images
+            # with valid celestial WCS
+            celestial_coordinates = image.coords.pixel_to_world(x, y).icrs.to_string('hmsdms')
+            overlay += f' ICRS={celestial_coordinates}'
+
+            # Extract data values at this position
+            if x > -0.5 and y > -0.5 and x < image.shape[1] and y < image.shape[0]:
+                value = image.get_data(image.main_components[0])[int(round(y)), int(round(x))]
+                overlay += f' data={value:.2g}'
+            
+            self.label_mouseover.text = [overlay]
+
+        elif data['event'] == 'mouseleave' or data['event'] == 'mouseenter':
+
+            self.label_mouseover.text = ""
+
+        if data['event'] == 'keydown' and data['key'] == 'b':
+
+            # Simple blinking of images - this will make it so that only one
+            # layer is visible at a time and cycles through the layers.
+
+            if len(self.state.layers) > 1:
+
+                # If only one layer is visible, pick the next one to be visible,
+                # otherwise start from the last visible one.
+
+                visible = [ilayer for ilayer, layer in enumerate(self.state.layers) if layer.visible]
+
+                if len(visible) > 0:
+                    next_layer = (visible[-1] + 1) % len(self.state.layers)
+                    self.state.layers[next_layer].visible = True
+                    for ilayer in visible:
+                        if ilayer != next_layer:
+                            self.state.layers[ilayer].visible = False
 
     def set_plot_axes(self):
         self.figure.axes[1].tick_format = None
