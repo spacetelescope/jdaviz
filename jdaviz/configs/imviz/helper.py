d32fa9ee4c8518e1d86c3d9129bda45c4a12470b
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2021-04-23 11:50:15 -0400

    Refactor Imviz parser as astrofrog suggested.

--- jdaviz/configs/imviz/helper.py
+++ jdaviz/configs/imviz/helper.py
@@ -1,4 +1,9 @@
+import os
+import re
+
 from jdaviz.core.helpers import ConfigHelper
+
+__all__ = ['Imviz']
 
 
 class Imviz(ConfigHelper):
@@ -6,5 +11,55 @@ class Imviz(ConfigHelper):
     _default_configuration = 'imviz'
 
     def load_data(self, data, parser_reference=None, **kwargs):
-        self.app.load_data(data, parser_reference=parser_reference,
-                           skip_checks=True, **kwargs)
+        if isinstance(data, str):
+            filepath, ext, data_label = split_filename_with_fits_ext(data)
+
+            # These will overwrite inputs, if any.
+            kwargs['ext'] = ext
+            kwargs['data_label'] = data_label
+
+        self.app.load_data(filepath, parser_reference=parser_reference, **kwargs)
+
+
+def split_filename_with_fits_ext(filename):
+    """Split a ``filename[ext]`` input into filename and FITS extension.
+
+    Parameters
+    ----------
+    filename : str
+        Can be a plain filename or ``filename[ext]``. The latter is a form
+        of input that is commonly used by DS9. Example values:
+
+        * ``'myimage.fits'``
+        * ``'myimage.fits[SCI]'`` (assumes ``EXTVER=1``)
+        * ``'myimage.fits[SCI,1]'``
+
+    Returns
+    -------
+    filepath : str
+        Path to the file, without extension.
+
+    ext : str, tuple, or `None`
+        FITS extension, if given. Examples: ``'SCI'`` or ``('SCI', 1)``
+
+    data_label : str
+        Human-readable data label for Glue.
+
+    """
+    s = os.path.splitext(filename)
+    ext_match = re.match(r'(.+)\[(.+)\]', s[1])
+    if ext_match is None:
+        sfx = s[1]
+        ext = None
+    else:
+        sfx = ext_match.group(1)
+        ext = ext_match.group(2)
+        if ',' in ext:
+            ext = ext.split(',')
+            ext[1] = int(ext[1])
+            ext = tuple(ext)
+
+    filepath = f'{s[0]}{sfx}'
+    data_label = f'{os.path.basename(s[0])}[{ext.upper()}]'
+
+    return filepath, ext, data_label
--- jdaviz/configs/imviz/plugins/parsers.py
+++ jdaviz/configs/imviz/plugins/parsers.py
@@ -1,6 +1,5 @@
 import base64
 import os
-import re
 import uuid
 
 from astropy.io import fits
@@ -18,7 +17,7 @@ __all__ = ['parse_data']
 
 
 @data_parser_registry("imviz-data-parser")
-def parse_data(app, file_obj, data_label=None, show_in_viewer=True):
+def parse_data(app, file_obj, ext=None, data_label=None, show_in_viewer=True):
     """Parse a data file into Imviz.
 
     Parameters
@@ -26,84 +25,52 @@ def parse_data(app, file_obj, data_label=None, show_in_viewer=True):
     app : `~jdaviz.app.Application`
         The application-level object used to reference the viewers.
 
-    file_path : str
-        The path to an image data file.
+    file_obj : str or obj
+        The path to an image data file or FITS HDUList or image object.
+
+    ext : str, tuple, or `None`, optional
+        FITS extension, if given. Examples: ``'SCI'`` or ``('SCI', 1)``
 
     data_label : str, optional
         The label to be applied to the Glue data component.
 
-    show_in_viewer : bool
+    show_in_viewer : bool, optional
         Show data in viewer.
 
     """
     # TODO: How much support needed for URL/URI?
     if isinstance(file_obj, str):
-        s = os.path.splitext(file_obj)
-        ext_match = re.match(r'(.+)\[(.+)\]', s[1])
-        if ext_match is None:
-            sfx = s[1]
-            ext = None
-        else:
-            sfx = ext_match.group(1)
-            ext = ext_match.group(2)
-            if ',' in ext:
-                ext = ext.split(',')
-                ext[1] = int(ext[1])
-                ext = tuple(ext)
         if data_label is None:
-            data_label = os.path.basename(s[0])
-        with fits.open(f'{s[0]}{sfx}') as pf:
+            data_label = os.path.splitext(os.path.basename(file_obj))[0]
+        with fits.open(file_obj) as pf:
             _parse_image(app, pf, data_label, show_in_viewer, ext=ext)
     else:
         if data_label is None:
             data_label = f'imviz_data|{str(base64.b85encode(uuid.uuid4().bytes), "utf-8")}'
-        _parse_image(app, file_obj, data_label, show_in_viewer)
+        _parse_image(app, file_obj, data_label, show_in_viewer, ext=ext)
 
 
 def _parse_image(app, file_obj, data_label, show_in_viewer, ext=None):
+    if data_label is None:
+        raise NotImplementedError('data_label should be set by now')
+
     if isinstance(file_obj, fits.HDUList):
         if 'ASDF' in file_obj:  # JWST ASDF-in-FITS
             if HAS_JWST_ASDF:
-                if ext is None:
-                    ext = 'data'
-                else:
-                    if isinstance(ext, tuple):
-                        ext = ext[0]  # EXTVER means nothing in ASDF
-                    ext = ext.lower()
-                    if ext in ('sci', 'asdf'):
-                        ext = 'data'
-
-                data_label += f'[{ext.upper()}]'
-                with datamodels.open(file_obj) as dm:
-                    if 'bunit' in dm.meta:
-                        bunit = dm.meta.bunit
-                    else:
-                        bunit = 'count'
-                    image_ccd = Data(label=data_label)
-
-                    # This is instance of gwcs.WCS, not astropy.wcs.WCS
-                    image_ccd.coords = dm.meta.wcs
-
-                    imdata = getattr(dm, ext)
-                    component = Component.autotyped(imdata, units=bunit)
-                    image_ccd.add_component(component=component, label=ext)
+                data = _jwst_to_glue_data(file_obj, ext, data_label)
             else:
                 raise ImportError('jwst package is missing')
 
         elif ext is not None:  # Load just the EXT user wants
             hdu = file_obj[ext]
-            if hdu.data is not None and hdu.is_image:
-                data_label += f'[{hdu.name},{hdu.ver}]'
-                image_ccd = _hdu_to_ccddata(hdu, data_label)
-            else:
-                raise ValueError(f'{file_obj}[{ext}] is not FITS image')
+            _validate_image2d(hdu)
+            data = _hdu_to_glue_data(hdu, data_label)
 
         else:  # Load first image extension found
             found = False
             for hdu in file_obj:
-                if hdu.data is not None and hdu.is_image:
-                    data_label += f'[{hdu.name},{hdu.ver}]'
-                    image_ccd = _hdu_to_ccddata(hdu, data_label)
+                if _validate_image2d(hdu, raise_error=False):
+                    data = _hdu_to_glue_data(hdu, data_label)
                     found = True
                     break
             if not found:
@@ -111,22 +78,58 @@ def _parse_image(app, file_obj, data_label, show_in_viewer, ext=None):
 
     elif isinstance(file_obj, (fits.ImageHDU, fits.CompImageHDU, fits.PrimaryHDU)):
         # NOTE: ext is not used here. It only means something if HDUList is given.
-        if hdu.data is not None and hdu.is_image:
-            data_label += f'[{hdu.name},{hdu.ver}]'
-            image_ccd = _hdu_to_ccddata(file_obj, data_label)
-        else:
-            raise ValueError(f'{file_obj} is not FITS image')
+        _validate_image2d(file_obj)
+        data = _hdu_to_glue_data(file_obj, data_label)
+
     else:
         raise NotImplementedError(f'Imviz does not support {file_obj}')
 
-    app.add_data(image_ccd, data_label)
+    app.add_data(data, data_label)
     if show_in_viewer:
         app.add_data_to_viewer("viewer-1", data_label)
 
 
-def _hdu_to_ccddata(hdu, data_label):
-    image_ccd = Data(label=data_label)
-    image_ccd.coords = WCS(hdu.header)
+def _validate_image2d(hdu, raise_error=True):
+    valid = hdu.data is not None and hdu.is_image and hdu.data.ndim == 2
+    if not valid and raise_error:
+        raise ValueError(f'Imviz cannot load this HDU ({hdu}): '
+                         f'is_image={hdu.is_image}, data={hdu.data}')
+    return valid
+
+
+def _jwst_to_glue_data(file_obj, ext, data_label):
+    # Translate FITS extension into JWST ASDF convention.
+    if ext is None:
+        ext = 'data'
+    else:
+        if isinstance(ext, tuple):
+            ext = ext[0]  # EXTVER means nothing in ASDF
+        ext = ext.lower()
+        if ext in ('sci', 'asdf'):
+            ext = 'data'
+
+    # This is very specific to JWST pipeline image output.
+    with datamodels.open(file_obj) as dm:
+        if 'bunit' in dm.meta:
+            bunit = dm.meta.bunit
+        else:
+            bunit = 'count'
+
+        data = Data(label=data_label)
+
+        # This is instance of gwcs.WCS, not astropy.wcs.WCS
+        data.coords = dm.meta.wcs
+
+        imdata = getattr(dm, ext)
+        component = Component.autotyped(imdata, units=bunit)
+        data.add_component(component=component, label=ext)
+
+    return data
+
+
+def _hdu_to_glue_data(hdu, data_label):
+    data = Data(label=data_label)
+    data.coords = WCS(hdu.header)
     component = Component.autotyped(hdu.data, units=hdu.header.get('BUNIT', 'count'))
-    image_ccd.add_component(component=component, label=f'{hdu.name},{hdu.ver}')
-    return image_ccd
+    data.add_component(component=component, label=f'{hdu.name},{hdu.ver}')
+    return data
