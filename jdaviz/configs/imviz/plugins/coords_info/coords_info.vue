507d96c2d670f92e42962fc66a46ff51bc0af544
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-05-04 15:55:28 +0100

    Hide labels if values are empty

--- jdaviz/configs/imviz/plugins/coords_info/coords_info.vue
+++ jdaviz/configs/imviz/plugins/coords_info/coords_info.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="pixel" style="white-space: nowrap;">
-        <b>Pixel </b>{{ pixel }}&nbsp;&nbsp;<b>Value </b>{{ value }}<br>
-        <b>World </b>{{ world }}<br>
+        <b v-if="pixel">Pixel </b>{{ pixel }}&nbsp;&nbsp;<b v-if="value">Value </b>{{ value }}<br>
+        <b v-if="world">World </b>{{ world }}<br>
     </div>
 </template>
