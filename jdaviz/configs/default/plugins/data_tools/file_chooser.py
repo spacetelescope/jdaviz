82eb1fd375e6a667e39a510ec126cd9fec67f390
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-05-11 16:46:46 +0100

    Move license

--- jdaviz/configs/default/plugins/data_tools/file_chooser.py
+++ jdaviz/configs/default/plugins/data_tools/file_chooser.py
@@ -1,27 +1,5 @@
 # The code in this file has been forked from the ipyfilechooser package which
-# was originally released under the following license:
-#
-# MIT License
-#
-# Copyright (c) 2019 Thomas Bouve
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+# was originally released under the license in licenses/IPYFILECHOOSER_LICENSE.rst
 #
 # This was forked to enable us to use our own buttons for the dialog and use
 # this just for the file browser view, but we could switch back to using the
