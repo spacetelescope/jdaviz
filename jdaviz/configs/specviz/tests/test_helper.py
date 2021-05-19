bcd1b1bc0eefc466ba4ea0b410b05d23d70d4e61
Jesse Averbukh <javerbukh@gmail.com>
2021-05-19 11:11:41 -0400

    Remove comments

--- jdaviz/configs/specviz/tests/test_helper.py
+++ jdaviz/configs/specviz/tests/test_helper.py
@@ -40,7 +40,6 @@ def test_load_spectrum1d(specviz_app, spectrum1d):
     assert list(data.keys())[0] == label
 
 
-# 1 3 5 6 7 8 9 11
 def test_get_spectra(specviz_app, spectrum1d):
     label = "Test 1D Spectrum"
     specviz_app.load_spectrum(spectrum1d, data_label=label)
@@ -51,7 +50,6 @@ def test_get_spectra(specviz_app, spectrum1d):
     assert spectra[label].flux.unit == spectrum1d.flux.unit
 
 
-# 1 2
 def test_get_spectra_no_redshift(specviz_app, spectrum1d):
     label = "Test 1D Spectrum"
     specviz_app.load_spectrum(spectrum1d, data_label=label)
@@ -62,7 +60,6 @@ def test_get_spectra_no_redshift(specviz_app, spectrum1d):
     assert spectra[label].flux.unit == spectrum1d.flux.unit
 
 
-# 1 3 5 6 7 9 11
 def test_get_spectra_no_data_label(specviz_app, spectrum1d):
     label = "Test 1D Spectrum"
     specviz_app.load_spectrum(spectrum1d, data_label=label)
@@ -73,18 +70,6 @@ def test_get_spectra_no_data_label(specviz_app, spectrum1d):
     assert spectra[label].flux.unit == spectrum1d.flux.unit
 
 
-# 1 3 5 6 7 8 9 11 DUPLICATE
-def test_get_spectra_no_label_redshift_warn(specviz_app, spectrum1d):
-    label = "Test 1D Spectrum"
-    specviz_app.load_spectrum(spectrum1d, data_label=label)
-
-    spectra = specviz_app.get_spectra(data_label=None, apply_slider_redshift="Warn")
-
-    assert np.allclose(spectra[label].flux, spectrum1d.flux)
-    assert spectra[label].flux.unit == spectrum1d.flux.unit
-
-
-# 1 3 4 5 6 7 9 10 11
 def test_get_spectra_label_redshift(specviz_app, spectrum1d):
     label = "Test 1D Spectrum"
     specviz_app.load_spectrum(spectrum1d, data_label=label)
@@ -95,7 +80,6 @@ def test_get_spectra_label_redshift(specviz_app, spectrum1d):
     assert spectra.flux.unit == spectrum1d.flux.unit
 
 
-# 1 3 4 5 6 7 8 9 10 11
 def test_get_spectra_label_redshift_warn(specviz_app, spectrum1d):
     label = "Test 1D Spectrum"
     specviz_app.load_spectrum(spectrum1d, data_label=label)
@@ -106,28 +90,24 @@ def test_get_spectra_label_redshift_warn(specviz_app, spectrum1d):
     assert spectra.flux.unit == spectrum1d.flux.unit
 
 
-# 1 3 5 7 8 9 11
 def test_get_spectra_no_spectra(specviz_app, spectrum1d):
     spectra = specviz_app.get_spectra()
 
     assert spectra == {}
 
 
-# 1 3 5 7 9 11
 def test_get_spectra_no_spectra_redshift_error(specviz_app, spectrum1d):
     spectra = specviz_app.get_spectra(apply_slider_redshift="Error")
 
     assert spectra == {}
 
 
-# 1 3 4 5 7 8 9 10 11
 def test_get_spectra_no_spectra_label(specviz_app, spectrum1d):
     label = "label"
     with pytest.raises(AttributeError):
         spectra = specviz_app.get_spectra(data_label=label)
 
 
-# 1 3 4 5 7 9 10 11
 def test_get_spectra_no_spectra_label_redshift_error(specviz_app, spectrum1d):
     label = "label"
     with pytest.raises(AttributeError):
