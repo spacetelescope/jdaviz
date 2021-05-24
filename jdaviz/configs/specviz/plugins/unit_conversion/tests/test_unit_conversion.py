df134e7192af0c9ea6faf962f224ea89fba15ebd
Jesse Averbukh <javerbukh@gmail.com>
2021-05-24 10:38:02 -0400

    Add assert statements for uncertainty

--- jdaviz/configs/specviz/plugins/unit_conversion/tests/test_unit_conversion.py
+++ jdaviz/configs/specviz/plugins/unit_conversion/tests/test_unit_conversion.py
@@ -19,6 +19,11 @@ RESULT_FLUX = [1.04067240e-07, 9.52912307e-08, 9.77144651e-08,
                9.05651431e-08, 8.33870526e-08, 7.47628902e-08,
                7.74896053e-08]
 
+RESULT_UNCERTAINTY = [3.85914248e-09, 3.60631495e-09, 1.74661581e-09,
+                      1.29057072e-08, 1.08965936e-08, 3.33352891e-09,
+                      5.64618219e-09, 1.65028707e-09, 4.49994292e-09,
+                      6.61559372e-09]
+
 
 def test_value_error_spec_axis_exception(specviz_app, spectrum1d):
     label = "Test 1D Spectrum"
@@ -76,6 +81,7 @@ def test_no_spec_no_flux_no_uncert(specviz_app, spectrum1d):
 
     assert converted_spectrum.flux.unit == spectrum1d.flux.unit
     assert converted_spectrum.spectral_axis.unit == spectrum1d.spectral_axis.unit
+    assert converted_spectrum.uncertainty is None
 
 
 def test_spec_no_flux_no_uncert(specviz_app, spectrum1d):
@@ -94,6 +100,7 @@ def test_spec_no_flux_no_uncert(specviz_app, spectrum1d):
                        RESULT_SPECTRAL_AXIS, atol=1e-5)
     assert converted_spectrum.flux.unit == spectrum1d.flux.unit
     assert converted_spectrum.spectral_axis.unit == new_spectral_axis
+    assert converted_spectrum.uncertainty is None
 
 
 def test_no_spec_no_flux_uncert(specviz_app, spectrum1d):
@@ -104,6 +111,8 @@ def test_no_spec_no_flux_uncert(specviz_app, spectrum1d):
     converted_spectrum = conv_func(specviz_app.app, spectrum=spectrum1d)
 
     assert converted_spectrum.flux.unit == spectrum1d.flux.unit
+    assert np.allclose(converted_spectrum.uncertainty.quantity,
+                       spectrum1d.uncertainty.quantity, atol=1e-5)
 
 
 def test_no_spec_no_flux_uncert_unit_exp_none(specviz_app, spectrum1d):
@@ -118,6 +127,7 @@ def test_no_spec_no_flux_uncert_unit_exp_none(specviz_app, spectrum1d):
 
     assert converted_spectrum.flux.unit == spectrum1d.flux.unit
     assert converted_spectrum.spectral_axis.unit == spectrum1d.spectral_axis.unit
+    assert converted_spectrum.uncertainty is None
 
 
 def test_no_spec_flux_no_uncert(specviz_app, spectrum1d):
@@ -135,6 +145,7 @@ def test_no_spec_flux_no_uncert(specviz_app, spectrum1d):
     assert np.allclose(converted_spectrum.flux.value,
                        RESULT_FLUX, atol=1e-5)
     assert converted_spectrum.spectral_axis.unit == spectrum1d.spectral_axis.unit
+    assert converted_spectrum.uncertainty is None
 
 
 def test_no_spec_flux_unit_exp_not_none(specviz_app, spectrum1d):
@@ -149,6 +160,8 @@ def test_no_spec_flux_unit_exp_not_none(specviz_app, spectrum1d):
 
     assert np.allclose(converted_spectrum.flux.value,
                        RESULT_FLUX, atol=1e-5)
+    assert np.allclose(converted_spectrum.uncertainty.quantity.value,
+                       RESULT_UNCERTAINTY, atol=1e-5)
 
 
 def test_spec_flux_no_uncert(specviz_app, spectrum1d):
@@ -169,6 +182,7 @@ def test_spec_flux_no_uncert(specviz_app, spectrum1d):
                        RESULT_FLUX, atol=1e-5)
     assert np.allclose(converted_spectrum.spectral_axis.value,
                        RESULT_SPECTRAL_AXIS, atol=1e-5)
+    assert converted_spectrum.uncertainty is None
 
 
 def test_spec_no_flux_uncert_no_unit_exp(specviz_app, spectrum1d):
@@ -187,6 +201,7 @@ def test_spec_no_flux_uncert_no_unit_exp(specviz_app, spectrum1d):
     assert converted_spectrum.flux.unit == spectrum1d.flux.unit
     assert np.allclose(converted_spectrum.spectral_axis.value,
                        RESULT_SPECTRAL_AXIS, atol=1e-5)
+    assert converted_spectrum.uncertainty is None
 
 
 def test_no_spec_flux_uncert_no_unit_exp(specviz_app, spectrum1d):
@@ -205,6 +220,7 @@ def test_no_spec_flux_uncert_no_unit_exp(specviz_app, spectrum1d):
     assert converted_spectrum.spectral_axis.unit == spectrum1d.spectral_axis.unit
     assert np.allclose(converted_spectrum.flux.value,
                        RESULT_FLUX, atol=1e-5)
+    assert converted_spectrum.uncertainty is None
 
 
 def test_spec_no_flux_uncert_unit_exp(specviz_app, spectrum1d):
@@ -219,6 +235,8 @@ def test_spec_no_flux_uncert_unit_exp(specviz_app, spectrum1d):
 
     assert np.allclose(converted_spectrum.spectral_axis.value,
                        RESULT_SPECTRAL_AXIS, atol=1e-5)
+    assert np.allclose(converted_spectrum.uncertainty.quantity,
+                       spectrum1d.uncertainty.quantity, atol=1e-5)
 
 
 def test_spec_flux_uncert_no_unit_exp(specviz_app, spectrum1d):
@@ -240,6 +258,7 @@ def test_spec_flux_uncert_no_unit_exp(specviz_app, spectrum1d):
                        RESULT_SPECTRAL_AXIS, atol=1e-5)
     assert np.allclose(converted_spectrum.flux.value,
                        RESULT_FLUX, atol=1e-5)
+    assert converted_spectrum.uncertainty is None
 
 
 def test_spec_flux_uncert_unit_exp(specviz_app, spectrum1d):
@@ -258,3 +277,5 @@ def test_spec_flux_uncert_unit_exp(specviz_app, spectrum1d):
                        RESULT_SPECTRAL_AXIS, atol=1e-5)
     assert np.allclose(converted_spectrum.flux.value,
                        RESULT_FLUX, atol=1e-5)
+    assert np.allclose(converted_spectrum.uncertainty.quantity.value,
+                       RESULT_UNCERTAINTY, atol=1e-5)
