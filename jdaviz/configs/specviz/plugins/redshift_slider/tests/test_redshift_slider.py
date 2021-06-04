3b67ed7e6c8bd35b491bbd16dde500492e952dd2
Jesse Averbukh <javerbukh@gmail.com>
2021-06-04 12:27:26 -0400

    Implement conftest fixtures

--- jdaviz/configs/specviz/plugins/redshift_slider/tests/test_redshift_slider.py
+++ jdaviz/configs/specviz/plugins/redshift_slider/tests/test_redshift_slider.py
@@ -1,29 +1,5 @@
 from .. import redshift_slider as rs
 
-import astropy.units as u
-import numpy as np
-import pytest
-from jdaviz.configs.specviz.helper import SpecViz
-from specutils import Spectrum1D
-
-
-SPECTRUM_SIZE = 10  # length of spectrum
-
-
-@pytest.fixture
-def specviz_app():
-    return SpecViz()
-
-
-@pytest.fixture
-def spectrum1d():
-    spec_axis = np.linspace(6000, 8000, SPECTRUM_SIZE) * u.AA
-    flux = (np.random.randn(len(spec_axis.value)) +
-            10*np.exp(-0.001*(spec_axis.value-6563)**2) +
-            spec_axis.value/500) * u.Jy
-
-    return Spectrum1D(spectral_axis=spec_axis, flux=flux)
-
 
 def test_bounds_orderly_new_val_greater_than(specviz_app, spectrum1d):
     label = "Test 1D Spectrum"
