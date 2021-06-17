11f0d6a0f1164799aea1b64d6c01add5b461434d
Ricky O'Steen <rosteen@stsci.edu>
2021-06-17 13:44:23 -0400

    Remove units from all models, pin to dev specutils for now

--- jdaviz/configs/default/plugins/model_fitting/model_fitting.py
+++ jdaviz/configs/default/plugins/model_fitting/model_fitting.py
@@ -412,27 +412,23 @@ class ModelFitting(TemplateMixin):
         # models_to_fit = self._reinitialize_with_fixed()
         models_to_fit = list(self._initialized_models.values())
 
-        # Remove units if necessary to work around current specutils limitations
+        # Remove units to work around current specutils/astropy modeling limitations
+        # TODO: Keep units once astropy/specutils can handle them for all models
         removed_units = {}
-        remove_units = False
+        unitless_models = []
+
         for m in models_to_fit:
-            if type(m) in (models.Polynomial1D, models.Linear1D):
-                remove_units = True
-                break
-        if remove_units:
-            unitless_models = []
-            for m in models_to_fit:
-                removed_units[m.name] = {}
-                if type(m) == models.Polynomial1D:
-                    temp_model = m.__class__(name=m.name, degree=m.degree)
-                else:
-                    temp_model = m.__class__(name=m.name)
-                for pname in m.param_names:
-                    p = getattr(m, pname)
-                    removed_units[m.name][pname] = p.unit
-                    setattr(temp_model, pname, p.value)
-                unitless_models.append(temp_model)
-            models_to_fit = unitless_models
+            removed_units[m.name] = {}
+            if type(m) == models.Polynomial1D:
+                temp_model = m.__class__(name=m.name, degree=m.degree)
+            else:
+                temp_model = m.__class__(name=m.name)
+            for pname in m.param_names:
+                p = getattr(m, pname)
+                removed_units[m.name][pname] = p.unit
+                setattr(temp_model, pname, p.value)
+            unitless_models.append(temp_model)
+        models_to_fit = unitless_models
 
         try:
             fitted_model, fitted_spectrum = fit_model_to_spectrum(
