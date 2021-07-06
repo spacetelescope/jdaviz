c26a04275bc021cd775640d5a5185d0ed2e94805
Jesse Averbukh <javerbukh@gmail.com>
2021-07-06 09:20:20 -0400

    Remove extra blank lines

--- jdaviz/configs/specviz/tests/test_helper.py
+++ jdaviz/configs/specviz/tests/test_helper.py
@@ -14,7 +14,6 @@ class TestSpecvizHelper:
         self.label = "Test 1D Spectrum"
         self.spec_app.load_spectrum(spectrum1d, data_label=self.label)
 
-
     def test_load_spectrum1d(self):
         assert len(self.spec_app.app.data_collection) == 1
         assert self.spec_app.app.data_collection[0].label == self.label
@@ -24,13 +23,11 @@ class TestSpecvizHelper:
         assert isinstance(list(data.values())[0], Spectrum1D)
         assert list(data.keys())[0] == self.label
 
-
     def test_get_spectra(self):
         spectra = self.spec_app.get_spectra()
 
         assert_quantity_allclose(spectra[self.label].flux,
                                  self.spec.flux, atol=1e-5*u.Unit(self.spec.flux.unit))
-
 
     def test_get_spectra_no_redshift(self):
         spectra = self.spec_app.get_spectra(apply_slider_redshift=None)
@@ -38,20 +35,17 @@ class TestSpecvizHelper:
         assert_quantity_allclose(spectra[self.label].flux,
                                  self.spec.flux, atol=1e-5*u.Unit(self.spec.flux.unit))
 
-
     def test_get_spectra_no_data_label(self):
         spectra = self.spec_app.get_spectra(data_label=None, apply_slider_redshift="Error")
 
         assert_quantity_allclose(spectra[self.label].flux,
                                  self.spec.flux, atol=1e-5*u.Unit(self.spec.flux.unit))
 
-
     def test_get_spectra_label_redshift(self):
         spectra = self.spec_app.get_spectra(data_label=self.label, apply_slider_redshift="Error")
 
         assert_quantity_allclose(spectra.flux,
                                  self.spec.flux, atol=1e-5*u.Unit(self.spec.flux.unit))
-
 
     def test_get_spectra_label_redshift_warn(self):
         spectra = self.spec_app.get_spectra(data_label=self.label, apply_slider_redshift="Warn")
