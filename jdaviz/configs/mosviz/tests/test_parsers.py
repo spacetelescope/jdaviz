5e01b001f8ba57ef82822de775bea99ec318a052
Ricky O'Steen <rosteen@stsci.edu>
2021-08-16 15:27:38 -0400

    Address review comments from @pllim

--- jdaviz/configs/mosviz/tests/test_parsers.py
+++ jdaviz/configs/mosviz/tests/test_parsers.py
@@ -7,25 +7,21 @@ from astropy.utils.data import download_file
 
 from jdaviz.configs.mosviz.helper import MosViz
 
-@pytest.fixture
-def mosviz_helper():
-    return MosViz()
 
 @pytest.mark.remote_data
-def test_niriss_parser(mosviz_helper):
-    data_dir = tempfile.gettempdir()
+def test_niriss_parser(mosviz_app, tmpdir):
 
     test_data = 'https://stsci.box.com/shared/static/9lkf5zha6zkf8ujnairy6krobbh038wt.zip'
     fn = download_file(test_data, cache=True)
     with ZipFile(fn, 'r') as sample_data_zip:
-        sample_data_zip.extractall(data_dir)
+        sample_data_zip.extractall(tmpdir)
 
-    level3_path = (pathlib.Path(data_dir) / 'NIRISS_for_parser_p0171')
+    level3_path = (pathlib.Path(tmpdir) / 'NIRISS_for_parser_p0171')
 
     data_dir = level3_path
 
-    mosviz_helper.load_niriss_data(data_dir)
+    mosviz_app.load_niriss_data(data_dir)
 
-    assert len(mosviz_helper.app.data_collection) == 80
-    assert mosviz_helper.app.data_collection[0].label == "Image canucs F150W"
-    assert mosviz_helper.app.data_collection[-1].label == "MOS Table"
+    assert len(mosviz_app.app.data_collection) == 80
+    assert mosviz_app.app.data_collection[0].label == "Image canucs F150W"
+    assert mosviz_app.app.data_collection[-1].label == "MOS Table"
