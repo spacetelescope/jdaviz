f52d31cd751359faa2262b4dca54993595d188b8
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-08-17 13:57:35 +0100

    Set timeout for download

--- jdaviz/configs/mosviz/tests/test_parsers.py
+++ jdaviz/configs/mosviz/tests/test_parsers.py
@@ -9,7 +9,7 @@ from astropy.utils.data import download_file
 def test_niriss_parser(mosviz_app, tmpdir):
 
     test_data = 'https://stsci.box.com/shared/static/l2azhcqd3tvzhybdlpx2j2qlutkaro3z.zip'
-    fn = download_file(test_data, cache=True)
+    fn = download_file(test_data, cache=True, timeout=30)
     with ZipFile(fn, 'r') as sample_data_zip:
         sample_data_zip.extractall(tmpdir)
 
