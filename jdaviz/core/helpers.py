8486e8230bd8e2cd311e22e5746385fb7b287088
Jesse Averbukh <javerbukh@gmail.com>
2021-08-31 14:36:17 -0400

    Remove reference to auto_link from app calls

--- jdaviz/core/helpers.py
+++ jdaviz/core/helpers.py
@@ -31,9 +31,9 @@ class ConfigHelper(HubListener):
     """
     _default_configuration = 'default'
 
-    def __init__(self, app=None, auto_link=True):
+    def __init__(self, app=None):
         if app is None:
-            self.app = Application(configuration=self._default_configuration, auto_link=auto_link)
+            self.app = Application(configuration=self._default_configuration)
         else:
             self.app = app
 
