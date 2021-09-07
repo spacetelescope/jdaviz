53979fc97cb3a68e61265d2c69469e329fb2f0ba
Ricky O'Steen <rosteen@stsci.edu>
2021-09-07 14:39:35 -0400

    Update specviz parser to handle SpectrumList input

--- jdaviz/configs/specviz/plugins/parsers.py
+++ jdaviz/configs/specviz/plugins/parsers.py
@@ -2,8 +2,10 @@ import base64
 import pathlib
 import uuid
 
+from astropy.io.registry import IORegistryError
+from specutils import Spectrum1D, SpectrumList, SpectrumCollection
+
 from jdaviz.core.registries import data_parser_registry
-from specutils import Spectrum1D, SpectrumCollection
 
 __all__ = ["specviz_spectrum1d_parser"]
 
@@ -15,8 +17,8 @@ def specviz_spectrum1d_parser(app, data, data_label=None, format=None, show_in_v
 
     Parameters
     ----------
-    data : str or `~specutils.Spectrum1D`
-        Spectrum1D spectra, or path to compatible data file.
+    data : str, `~specutils.Spectrum1D`, or `~specutils.SpectrumList`
+        Spectrum1D, SpectrumList, or path to compatible data file.
     data_label : str
         The Glue data label found in the ``DataCollection``.
     format : str
@@ -30,32 +32,51 @@ def specviz_spectrum1d_parser(app, data, data_label=None, format=None, show_in_v
             base64.b85encode(uuid.uuid4().bytes), "utf-8"
         )
 
-    # If data provided is a path, try opening into a Spectrum1D object
-    try:
+    if isinstance(data, SpectrumCollection):
+        raise TypeError("SpectrumCollection detected."
+                        " Please provide a Spectrum1D or SpectrumList")
+    elif isinstance(data, Spectrum1D):
+        data = [data]
+        data_label = [data_label]
+    elif isinstance(data, SpectrumList):
+        pass
+    else:
         path = pathlib.Path(data)
 
         if path.is_file():
-            data = Spectrum1D.read(str(path), format=format)
+            try:
+                data = [Spectrum1D.read(str(path), format=format)]
+                data_label = [data_label]
+            except IORegistryError:
+                # Multi-extension files may throw a registry error
+                data = SpectrumList.read(str(path), format=format)
         else:
             raise FileNotFoundError("No such file: " + str(path))
-    # If not, it must be a Spectrum1D object. Otherwise, it's unsupported
-    except TypeError:
-        if type(data) is SpectrumCollection:
-            raise TypeError(
-                "SpectrumCollection detected. Please provide a Spectrum1D"
-            )
-        elif type(data) is not Spectrum1D:
-            raise TypeError("Data is not a Spectrum1D object or compatible file")
+
+    if isinstance(data, SpectrumList):
+        if not isinstance(data_label, (list, tuple)):
+            temp_labels = []
+            for i in range(len(data)):
+                temp_labels.append(f"{data_label} {i}")
+            data_label = temp_labels
+        elif len(data_label) != len(data):
+            raise ValueError(f"Length of data labels list ({len(data_label)}) is different"
+                             f" than length of list of data ({len(data)})")
 
     # If there's already data in the viewer, convert units if needed
     current_spec = app.get_data_from_viewer("spectrum-viewer")
-    if current_spec != {} and current_spec is not None:
-        spec_key = list(current_spec.keys())[0]
-        current_unit = current_spec[spec_key].spectral_axis.unit
-        if data.spectral_axis.unit != current_unit:
-            data = Spectrum1D(flux=data.flux,
-                              spectral_axis=data.spectral_axis.to(current_unit))
+    for i in range(len(data)):
+        spec = data[i]
+        if current_spec != {} and current_spec is not None:
+            spec_key = list(current_spec.keys())[0]
+            current_unit = current_spec[spec_key].spectral_axis.unit
+            if spec.spectral_axis.unit != current_unit:
+                spec = Spectrum1D(flux=spec.flux,
+                                  spectral_axis=spec.spectral_axis.to(current_unit))
 
-    app.add_data(data, data_label)
-    if show_in_viewer:
-        app.add_data_to_viewer("spectrum-viewer", data_label)
+        app.add_data(spec, data_label[i])
+        if show_in_viewer:
+            app.add_data_to_viewer("spectrum-viewer", data_label[i])
+
+        # Only auto-show the first spectrum in a list
+        show_in_viewer = False
