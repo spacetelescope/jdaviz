c40a2f8ffbef528c3bf30a0c2e3d5123c75e925f
Kyle Conroy <kyleconroy@gmail.com>
2021-11-26 17:56:55 -0500

    mosviz redshift slider
    * row stores values, and assigned to spectrum when changing row (could be optimized to avoid caching and 0 flashing)
    * mosviz.get_spectrum_1d/2d helper methods to access underlying spectra in ANY row with or without applied redshift
    * minimal support for RV switch in slider (values do not match exactly when changing switch, no RV column in table)

--- jdaviz/configs/mosviz/plugins/viewers.py
+++ jdaviz/configs/mosviz/plugins/viewers.py
@@ -144,6 +144,10 @@ class MosvizTableViewer(TableViewer):
     def nrows(self):
         return self.widget_table.get_state()['total_length']
 
+    @property
+    def current_row(self):
+        return self.widget_table.highlighted
+
     def select_row(self, n):
         if n < 0 or n >= self.nrows:
             raise ValueError("n must be between 0 and {}".format(self.nrows-1))
@@ -159,18 +163,18 @@ class MosvizTableViewer(TableViewer):
         self.widget_table.highlighted = n
 
     def next_row(self):
-        current_row = self.widget_table.highlighted
+        current_row = self.current_row
         new_row = 0 if current_row == self.nrows - 1 else current_row + 1
         self.select_row(new_row)
 
     def prev_row(self):
-        current_row = self.widget_table.highlighted
+        current_row = self.current_row
         new_row = self.nrows - 1 if current_row == 0 else current_row - 1
         self.select_row(new_row)
 
     def _on_row_selected(self, event):
         if self._on_row_selected_begin:
-            self._on_row_selected_begin()
+            self._on_row_selected_begin(event)
 
         self.row_selection_in_progress = True
         # Grab the index of the latest selected row
@@ -238,7 +242,7 @@ class MosvizTableViewer(TableViewer):
         self.row_selection_in_progress = False
 
         if self._on_row_selected_end:
-            self._on_row_selected_end()
+            self._on_row_selected_end(event)
 
     def set_plot_axes(self, *args, **kwargs):
         return
