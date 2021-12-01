90b2780699dc8c4604a2915b563f07b52ea5daee
Pey Lian Lim (Github) <2090236+pllim@users.noreply.github.com>
2021-12-01 15:06:07 -0500

    Have Pixel not jump around

--- jdaviz/configs/imviz/plugins/coords_info/coords_info.vue
+++ jdaviz/configs/imviz/plugins/coords_info/coords_info.vue
@@ -7,16 +7,16 @@
         </td>
       </tr>
       <tr>
-        <td width="42"><b v-if="world_ra">World</b></td>
+        <td width="42"><b>{{ world_label_prefix }}</b></td>
         <td width="115">{{ world_ra }}</td>
         <td width="120">{{ world_dec }}</td>
-        <td><div v-if="world_ra">(ICRS)</div></td>
+        <td>{{ world_label_icrs }}</td>
       </tr>
       <tr>
         <td width="42"></td>
         <td width="115">{{ world_ra_deg }}</td>
         <td width="120">{{ world_dec_deg }}</td>
-        <td><div v-if="world_ra">(deg)</div></td>
+        <td>{{ world_label_deg }}</td>
       </tr>
     </table>
   </div>
