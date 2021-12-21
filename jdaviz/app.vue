ca109851af254c214642fd9d85b533f671f93bcb
Kyle Conroy <kyleconroy@gmail.com>
2021-12-21 13:46:27 -0500

    fix plugin tray overflow-x

--- jdaviz/app.vue
+++ jdaviz/app.vue
@@ -44,7 +44,7 @@
             </golden-layout>
           </pane>
           <pane size="25" min-size="25" v-if="state.drawer" style="background-color: #fafbfc;">
-            <v-card flat tile class="overflow-y-auto fill-height" color="#f8f8f8">
+            <v-card flat tile class="overflow-y-auto fill-height" style="overflow-x: hidden" color="#f8f8f8">
               <v-expansion-panels accordion multiple focusable flat tile v-model="state.tray_items_open">
                 <v-expansion-panel v-for="(tray, index) in state.tray_items" :key="index">
                   <v-expansion-panel-header>
