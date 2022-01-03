fefe4514eadf8602c93eb6645571d2dfc2465b3c
Pey Lian Lim (Github) <2090236+pllim@users.noreply.github.com>
2022-01-03 17:02:53 -0500

    WIP: Re-enable test_moment_maps

--- jdaviz/configs/cubeviz/plugins/moment_maps/tests/test_moment_maps.py
+++ jdaviz/configs/cubeviz/plugins/moment_maps/tests/test_moment_maps.py
@@ -1,19 +1,15 @@
 from specutils import Spectrum1D
 
-from jdaviz import Application
 from jdaviz.configs.cubeviz.plugins.moment_maps.moment_maps import MomentMap
 
 
-def todo_fix_test_moment_calculation(spectrum1d_cube):
-
-    app = Application()
+def test_moment_calculation(cubeviz_app, spectrum1d_cube):
+    app = cubeviz_app.app
     dc = app.data_collection
     app.add_data(spectrum1d_cube, 'test')
 
     mm = MomentMap(app=app)
     mm._subset_selected = 'None'
-    # mm.spectral_min = 1.0 * u.m
-    # mm.spectral_max = 2.0 * u.m
     mm._on_data_updated(None)
 
     mm._on_data_selected({'new': 'test'})
