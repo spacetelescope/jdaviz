642ccbb7b526daac5478906e1324553cc5a14bb3
Kyle Conroy <kyleconroy@gmail.com>
2022-02-01 14:19:54 -0500

    line analysis plugin: basic linear continuum support
    * option to provide width (in number of data points surrounding the line) or a custom subset.  Either way, a line is fitted to these points and divided/subtracted before passing to specutils
    * data dropdown now defaults to first data entry instead of blank
    * added spinner while results are computing/updating instead of clearing and re-populating
    * form validation logic to make sure not using the same subset as the line and to make sure a reasonable value of width is passed

--- jdaviz/configs/specviz/plugins/line_analysis/line_analysis.py
+++ jdaviz/configs/specviz/plugins/line_analysis/line_analysis.py
@@ -2,8 +2,10 @@ import numpy as np
 from glue.core.message import (SubsetDeleteMessage,
                                SubsetUpdateMessage)
 from traitlets import Bool, List, Unicode, observe
-from specutils import analysis, SpectralRegion
+from specutils import analysis
+from specutils.manipulation import extract_region
 
+from jdaviz.core.custom_traitlets import IntHandleEmpty
 from jdaviz.core.events import AddDataMessage, RemoveDataMessage
 from jdaviz.core.registries import tray_registry
 from jdaviz.core.template_mixin import TemplateMixin
@@ -25,7 +27,10 @@ class LineAnalysis(TemplateMixin):
     spectral_subset_items = List(["Entire Spectrum"]).tag(sync=True)
     selected_spectrum = Unicode("").tag(sync=True)
     selected_subset = Unicode("Entire Spectrum").tag(sync=True)
-    result_available = Bool(False).tag(sync=True)
+    continuum_subset_items = List(["Surrounding"]).tag(sync=True)
+    selected_continuum = Unicode("Surrounding").tag(sync=True)
+    width_points = IntHandleEmpty(5).tag(sync=True)
+    results_computing = Bool(False).tag(sync=True)
     results = List().tag(sync=True)
 
     def __init__(self, *args, **kwargs):
@@ -35,7 +40,7 @@ class LineAnalysis(TemplateMixin):
         self._spectrum1d = None
         self._viewer_id = self.app._viewer_item_by_reference('spectrum-viewer').get('id')
         self._units = {}
-        self.result_available = False
+        self.update_results(None)
 
         self.hub.subscribe(self, AddDataMessage,
                            handler=self._on_viewer_data_changed)
@@ -76,49 +81,109 @@ class LineAnalysis(TemplateMixin):
             pass
 
         self.spectral_subset_items = ["Entire Spectrum"] + sorted(self._spectral_subsets.keys())
+        self.continuum_subset_items = ["Surrounding"] + sorted(self._spectral_subsets.keys())
 
         self.dc_items = [layer_state.layer.label for layer_state in viewer.state.layers
                          if layer_state.layer.label not in self.spectral_subset_items]
 
+        if self.selected_spectrum == "":
+            # default to first entry in list instead of leaving empty
+            self.selected_spectrum = self.dc_items[0]
+
         if len(self.dc_items) == 0:
             self.selected_spectrum = ""
             self.selected_subset = "Entire Spectrum"
-            self.result_available = False
+            self.selected_continuum = "Surrounding"
+            self.update_results(None)
 
-    @observe("selected_subset", "selected_spectrum")
+    def update_results(self, results=None):
+        if results is None:
+            self.results = [{'function': function, 'result': ''} for function in FUNCTIONS]
+        else:
+            self.results = results
+        self.results_computing = False
+
+    @observe("selected_subset", "selected_spectrum", "selected_continuum", "width_points")
     def _calculate_statistics(self, *args, **kwargs):
         """
         Run the line analysis functions on the selected data/subset and
         display the results.
         """
-        if self.selected_spectrum == "":
-            self.result_available = False
+        # show spinner, disable, whatever
+        self.results_computing = True
+
+        if self.selected_spectrum == "" or self.width_points == "":
+            self.update_results(None)
             return
 
         self._spectrum1d = self.app.get_data_from_viewer("spectrum-viewer",
                                                          data_label=self.selected_spectrum)
+        spectral_axis = self._spectrum1d.spectral_axis
 
-        if self.selected_subset != "Entire Spectrum":
-            mask = self.app.get_data_from_viewer("spectrum-viewer",
-                                                 data_label=self.selected_subset).mask
-            self._spectrum1d.mask = mask
+        if self.selected_continuum == self.selected_subset:
+            # already raised a validation error in the UI
+            self.update_results(None)
+            return
+        elif self.selected_subset == "Entire Spectrum":
+            inds_bounds = [0, len(spectral_axis)]
+            spectrum = self._spectrum1d
+        else:
+
+            sr = self.app.get_subsets_from_viewer("spectrum-viewer",
+                                                  subset_type="spectral").get(self.selected_subset) # noqa
+            spectrum = extract_region(self._spectrum1d, sr, return_single_spectrum=True)
+
+            inds_within_region, = np.where((sr.lower <= spectral_axis) & (spectral_axis <= sr.upper)) # noqa
+            inds_bounds = inds_within_region[0], inds_within_region[-1]
+
+        # compute continuum
+        if self.selected_continuum == "Surrounding":
+            if self.width_points > 50 or self.width_points <= 0:
+                # if changing this max-value, also change the value in the
+                # rules in line_analysis.vue.  Could make the rule to be
+                # dynamic based on the length of the spectrum, but that is
+                # likely overkill
+                self.update_results(None)
+                return
+
+            if inds_bounds[0] < self.width_points:
+                # then we need to extend into the line region itself
+                left = np.arange(0, self.width_points)
+            else:
+                left = np.arange(inds_bounds[0]-self.width_points, inds_bounds[0])
+
+            if inds_bounds[1] > len(spectral_axis)-self.width_points:
+                # then we need to extend into the line region itself
+                right = np.arange(len(spectral_axis)-self.width_points, len(spectral_axis))
+            else:
+                right = np.arange(inds_bounds[1]+1, inds_bounds[1]+1+self.width_points)
+
+            continuum_mask = np.concatenate((left, right))
+        else:
+            continuum_mask = self.app.get_data_from_viewer("spectrum-viewer",
+                                                           data_label=self.selected_continuum).mask # noqa
+
+        continuum_x = spectral_axis[continuum_mask].value
+        continuum_y = self._spectrum1d.flux[continuum_mask].value
+        # DEV NOTE: could replace this with internal calls to the model fitting infrastructure
+        # to enable other model-types and to give visual feedback (by labeling the model
+        # as line_analysis:continuum, for example)
+        slope, intercept = np.polyfit(continuum_x, continuum_y, deg=1)
+        continuum = slope * spectrum.spectral_axis.value + intercept
 
         temp_results = []
         for function in FUNCTIONS:
-            # Centroid function requires a region argument, create one to pass
-            if function == "Centroid":
-                spectral_axis = self._spectrum1d.spectral_axis
-                if self._spectrum1d.mask is None:
-                    spec_region = SpectralRegion(spectral_axis[0], spectral_axis[-1])
-                else:
-                    spec_region = self._spectrum1d.spectral_axis[np.where(
-                                                                 self._spectrum1d.mask == 0)]
-                    spec_region = SpectralRegion(spec_region[0], spec_region[-1])
-                temp_result = FUNCTIONS[function](self._spectrum1d, spec_region)
+            # TODO: update specutils to allow ALL analysis to take regions and continuum so we
+            # don't need these if statements
+            if function == "Equivalent Width":
+                temp_result = FUNCTIONS[function](spectrum/continuum)
+            elif function == "Centroid":
+                # TODO: update specutils to be consistent with region vs regions and default to
+                # regions=None so this elif can be removed
+                temp_result = FUNCTIONS[function](spectrum-continuum, region=None)
             else:
-                temp_result = FUNCTIONS[function](self._spectrum1d)
+                temp_result = FUNCTIONS[function](spectrum-continuum)
 
             temp_results.append({'function': function, 'result': str(temp_result)})
 
-        self.results = temp_results
-        self.result_available = True
+        self.update_results(temp_results)
--- jdaviz/configs/specviz/plugins/line_analysis/line_analysis.vue
+++ jdaviz/configs/specviz/plugins/line_analysis/line_analysis.vue
@@ -2,8 +2,13 @@
   <j-tray-plugin>
     <v-row>
       <j-docs-link :link="'https://jdaviz.readthedocs.io/en/'+vdocs+'/'+config+'/plugins.html#line-analysis'">
-        Return statistics for a single spectral line.  The results are meaningful only when the continuum is properly normalized or subtracted, depending on the function you are interested in. See the <j-external-link link="https://specutils.readthedocs.io/en/stable/analysis.html" linktext="specutils docs"/> for more details.
+        Return statistics for a single spectral line.  See the <j-external-link link="https://specutils.readthedocs.io/en/stable/analysis.html" linktext="specutils docs"/> for more details.
       </j-docs-link>
+    </v-row>
+
+    <j-plugin-section-header>Line</j-plugin-section-header>
+    <v-row>
+      <j-docs-link>Choose a region that defines the spectral line.</j-docs-link>
     </v-row>
 
     <v-row>
@@ -11,7 +16,7 @@
         :items="dc_items"
         v-model="selected_spectrum"
         label="Data"
-        hint="Select the data set to be fitted."
+        hint="Select the data set to be analysed."
         persistent-hint
       ></v-select>
     </v-row>
@@ -21,32 +26,80 @@
         :items="spectral_subset_items"
         v-model="selected_subset"
         label="Spectral Region"
-        hint="Select spectral region to apply the collapse."
+        hint="Select spectral region that defines the line."
         persistent-hint
       ></v-select>
     </v-row>
 
-    <div v-if="result_available">
-      <j-plugin-section-header>Results</j-plugin-section-header>
-      <v-row>
-        <v-col cols=6><U>Function</U></v-col>
-        <v-col cols=6><U>Result</U></v-col>
-      </v-row>
-      <v-row 
-        v-for="item in results"
-        :key="item.function">
-        <v-col cols=6>
-          {{  item.function  }}
-          <j-tooltip :tooltipcontent="'specutils '+item.function+' documentation'">
-            <a v-bind:href="'https://specutils.readthedocs.io/en/stable/api/specutils.analysis.'+item.function.toLowerCase().replaceAll(' ', '_')+'.html'" 
-              target="__blank" 
-              style="color: #A75000">
-              <v-icon x-small color="#A75000">mdi-open-in-new</v-icon>
-            </a> 
-          </j-tooltip>
-        </v-col>
-        <v-col cols=6>{{ item.result }}</v-col>
-      </v-row>
+    <j-plugin-section-header>Continuum</j-plugin-section-header>
+    <v-row>
+      <j-docs-link>Choose a region to fit a linear line as the underlying continuum.  If "surrounding", choose a width in number of data points to consider on each side of the line region defined above.</j-docs-link>
+    </v-row>
+
+    <v-row>
+      <v-select
+        :items="continuum_subset_items"
+        v-model="selected_continuum"
+        label="Spectral Region"
+        :rules="[() => selected_continuum!==selected_subset || 'Must not match line selection']"
+        hint="Select spectral region to fit linear line to represent the continuum."
+        persistent-hint
+      ></v-select>
+    </v-row>
+
+    <v-row v-if="selected_continuum=='Surrounding'">
+      <!-- NOTE: the rules below should also be handled within line_analysis.py to clear the existing results -->
+      <v-text-field
+        label="Width"
+        type="number"
+        v-model.number="width_points"
+        :rules="[() => width_points!=='' || 'This field is required',
+                 () => width_points<=50 || 'Width must be <= 50',
+                 () => width_points>0 || 'Width must be >0']"
+        hint="Number of data points surrounding the spectral region to fit a linear continuum."
+        persistent-hint
+      >
+      </v-text-field>
+    </v-row>
+
+    <j-plugin-section-header>Results</j-plugin-section-header>
+    <div style="display: grid"> <!-- overlay container -->
+      <div style="grid-area: 1/1">
+        <v-row>
+          <v-col cols=6><U>Function</U></v-col>
+          <v-col cols=6><U>Result</U></v-col>
+        </v-row>
+        <v-row 
+          v-for="item in results"
+          :key="item.function">
+          <v-col cols=6>
+            {{  item.function  }}
+            <j-tooltip :tooltipcontent="'specutils '+item.function+' documentation'">
+              <a v-bind:href="'https://specutils.readthedocs.io/en/stable/api/specutils.analysis.'+item.function.toLowerCase().replaceAll(' ', '_')+'.html'" 
+                target="__blank" 
+                style="color: #A75000">
+                <v-icon x-small color="#A75000">mdi-open-in-new</v-icon>
+              </a> 
+            </j-tooltip>
+          </v-col>
+          <v-col cols=6>{{ item.result }}</v-col>
+        </v-row>
+      </div>
+      <div v-if="results_computing"
+           class="text-center"
+           style="grid-area: 1/1; 
+                  z-index:2;
+                  margin-left: -24px;
+                  margin-right: -24px;
+                  padding-top: 60px;
+                  background-color: rgb(0 0 0 / 20%)">
+        <v-progress-circular
+          indeterminate
+          color="default"
+          size="50"
+          width="6"
+        ></v-progress-circular>
+      </div>
     </div>
   </j-tray-plugin>
 </template>
