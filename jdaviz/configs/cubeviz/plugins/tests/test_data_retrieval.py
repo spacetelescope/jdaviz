1917dd66d4d93cb98112e7dec05f5aa62cc0a904
Pey Lian Lim (Github) <2090236+pllim@users.noreply.github.com>
2022-02-03 18:03:56 -0500

    Use new pytest fixture

--- jdaviz/configs/cubeviz/plugins/tests/test_data_retrieval.py
+++ jdaviz/configs/cubeviz/plugins/tests/test_data_retrieval.py
@@ -12,18 +12,18 @@ from astropy.utils.data import download_file
 
 @pytest.mark.filterwarnings('ignore')
 @pytest.mark.remote_data
-def test_data_retrieval(cubeviz_app):
+def test_data_retrieval(cubeviz_helper):
     # This file is originally from
     # https://data.sdss.org/sas/dr14/manga/spectro/redux/v2_1_2/7495/stack/manga-7495-12704-LOGCUBE.fits.gz
     URL = 'https://stsci.box.com/shared/static/28a88k1qfipo4yxc4p4d40v4axtlal8y.fits'
 
     fn = download_file(URL, cache=True)
-    cubeviz_app.load_data(fn)
+    cubeviz_helper.load_data(fn)
 
     # two ways of retrieving data from the viewer.
     # They should return the same spectral values
-    a1 = cubeviz_app.app.get_viewer('spectrum-viewer').data()
-    a2 = cubeviz_app.app.get_data_from_viewer("spectrum-viewer")
+    a1 = cubeviz_helper.app.get_viewer('spectrum-viewer').data()
+    a2 = cubeviz_helper.app.get_data_from_viewer("spectrum-viewer")
 
     test_value_1 = a1[0].data
     test_value_2 = list(a2.values())[0].data
