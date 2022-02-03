7c3874922b335cfcda3cc109d55903d7605738cc
duytnguyendtn <duytnguyendtn.open@gmail.com>
2022-02-03 17:38:31 -0500

    Add expected failure display mode tests

--- jdaviz/tests/test_show.py
+++ jdaviz/tests/test_show.py
@@ -1,4 +1,6 @@
+from multiprocessing.sharedctypes import Value
 import sidecar
+from pytest import raises
 
 
 def test_show_inline(specviz_helper):
@@ -11,6 +13,21 @@ def test_show_sidecar(specviz_helper):
     assert isinstance(res, sidecar.Sidecar)
 
 
-def test_show_new_tab(specviz_helper):
+def test_show_new_jupyter_tab(specviz_helper):
     res = specviz_helper.show(mode="new jupyter tab")
     assert isinstance(res, sidecar.Sidecar)
+
+
+def test_show_new_browser_tab(specviz_helper):
+    with raises(NotImplementedError):
+        specviz_helper.show(mode="new browser tab")
+
+
+def test_show_popout(specviz_helper):
+    with raises(NotImplementedError):
+        specviz_helper.show(mode="popout")
+
+
+def test_show_invalid_mode(specviz_helper):
+    with raises(ValueError):
+        specviz_helper.show(mode="thisisnotavaliddisplaymode")
