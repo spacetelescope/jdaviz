4316c007938944dd1d311dc4c04e15b145328e0c
duytnguyendtn <duytnguyendtn.open@gmail.com>
2022-04-06 17:38:26 -0400

    Add directory loading tests for mosviz

--- jdaviz/configs/mosviz/tests/test_data_loading.py
+++ jdaviz/configs/mosviz/tests/test_data_loading.py
@@ -54,10 +54,33 @@ def test_load_single_image_multi_spec(mosviz_helper, image, spectrum1d, spectrum
         assert np.all(qtable["Images"] == "Test Label")
     assert len(qtable) == 3
 
+
 @pytest.mark.remote_data
-@pytest.mark.filterwarnings('ignore')
-@pytest.mark.parametrize('label', [None, "Test Label"])
-def test_niriss_directory(mosviz_helper, tmpdir):
+def test_nirpsec_loader(mosviz_helper, tmpdir):
+    '''
+    Tests loading our default MosvizExample notebook data
+    '''
+
+    test_data = 'https://stsci.box.com/shared/static/ovyxi5eund92yoadvv01mynwt8t5n7jv.zip'
+    fn = download_file(test_data, cache=True, timeout=30)
+    with ZipFile(fn, 'r') as sample_data_zip:
+        sample_data_zip.extractall(tmpdir)
+
+    level3_path = (pathlib.Path(tmpdir) / 'mosviz_nirspec_data_0.3' / 'level3')
+
+    data_dir = level3_path
+
+    mosviz_helper.load_data(directory=data_dir, instrument='nirspec')
+
+    assert len(mosviz_helper.app.data_collection) == 16
+    assert "MOS Table" in mosviz_helper.app.data_collection
+    assert "Image 4" in mosviz_helper.app.data_collection
+    assert "1D Spectrum 4" in mosviz_helper.app.data_collection
+    assert "2D Spectrum 4" in mosviz_helper.app.data_collection
+
+
+@pytest.mark.remote_data
+def test_niriss_loader(mosviz_helper, tmpdir):
 
     test_data = 'https://stsci.box.com/shared/static/l2azhcqd3tvzhybdlpx2j2qlutkaro3z.zip'
     fn = download_file(test_data, cache=True, timeout=30)
@@ -68,8 +91,33 @@ def test_niriss_directory(mosviz_helper, tmpdir):
 
     data_dir = level3_path
 
-    mosviz_helper.load_niriss_data(data_dir)
+    mosviz_helper.load_data(directory=data_dir, instrument='niriss')
 
     assert len(mosviz_helper.app.data_collection) == 80
     assert mosviz_helper.app.data_collection[0].label == "Image canucs F150W"
     assert mosviz_helper.app.data_collection[-1].label == "MOS Table"
+
+
+@pytest.mark.remote_data
+def test_nirpsec_fallback(mosviz_helper, tmpdir):
+    '''
+    When no instrument is provided, mosviz.load_data is expected to fallback to the nirspec loader.
+    Naturally, the nirspec dataset should then work without any instrument keyword
+    '''
+
+    test_data = 'https://stsci.box.com/shared/static/ovyxi5eund92yoadvv01mynwt8t5n7jv.zip'
+    fn = download_file(test_data, cache=True, timeout=30)
+    with ZipFile(fn, 'r') as sample_data_zip:
+        sample_data_zip.extractall(tmpdir)
+
+    level3_path = (pathlib.Path(tmpdir) / 'mosviz_nirspec_data_0.3' / 'level3')
+
+    data_dir = level3_path
+
+    mosviz_helper.load_data(directory=data_dir)
+
+    assert len(mosviz_helper.app.data_collection) == 16
+    assert "MOS Table" in mosviz_helper.app.data_collection
+    assert "Image 4" in mosviz_helper.app.data_collection
+    assert "1D Spectrum 4" in mosviz_helper.app.data_collection
+    assert "2D Spectrum 4" in mosviz_helper.app.data_collection
