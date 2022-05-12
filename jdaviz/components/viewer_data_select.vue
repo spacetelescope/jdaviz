c75f322a06873f15da8e1133a088c0b96066fbc6
Kyle Conroy <kyleconroy@gmail.com>
2022-05-12 11:02:54 -0400

    fix indentation

--- jdaviz/components/viewer_data_select.vue
+++ jdaviz/components/viewer_data_select.vue
@@ -17,38 +17,38 @@
         </v-btn>
       </template>
   
-        <v-list style="max-height: 500px; width: 350px;" class="overflow-y-auto">
-          <v-row v-for="item in filteredDataItems" :key="item.id" style="padding-left: 25px">
-            <j-tooltip tipid='viewer-data-select-toggle'>
-              <v-btn 
+      <v-list style="max-height: 500px; width: 350px;" class="overflow-y-auto">
+        <v-row v-for="item in filteredDataItems" :key="item.id" style="padding-left: 25px">
+          <j-tooltip tipid='viewer-data-select-toggle'>
+            <v-btn 
+              icon
+              :color="viewer.selected_data_items.includes(item.id) ? 'accent' : 'default'"
+              @click="$emit('data-item-selected', {
+                id: viewer.id,
+                item_id: item.id,
+                checked: !viewer.selected_data_items.includes(item.id)
+              })">
+                <v-icon>{{viewer.selected_data_items.includes(item.id) ? "mdi-eye" : "mdi-eye-off"}}</v-icon>
+            </v-btn>
+          </j-tooltip>
+
+          <span class='text--primary' style="overflow-wrap: anywhere; font-size: 12pt; padding-top: 6px; padding-left: 6px">
+            {{item.name}}
+          </span>
+
+          <div style="position: absolute; right: 10px">
+            <j-tooltip tipid='viewer-data-select-delete'>
+              <v-btn
                 icon
-                :color="viewer.selected_data_items.includes(item.id) ? 'accent' : 'default'"
-                @click="$emit('data-item-selected', {
-                  id: viewer.id,
-                  item_id: item.id,
-                  checked: !viewer.selected_data_items.includes(item.id)
-                })">
-                  <v-icon>{{viewer.selected_data_items.includes(item.id) ? "mdi-eye" : "mdi-eye-off"}}</v-icon>
-              </v-btn>
+                @click="$emit('data-item-remove', {item_name: item.name})"
+              ><v-icon>mdi-delete</v-icon></v-btn>
             </j-tooltip>
+          </div>
+        </v-row>
+      </v-list>
 
-            <span class='text--primary' style="overflow-wrap: anywhere; font-size: 12pt; padding-top: 6px; padding-left: 6px">
-              {{item.name}}
-            </span>
-
-            <div style="position: absolute; right: 10px">
-              <j-tooltip tipid='viewer-data-select-delete'>
-                <v-btn
-                  icon
-                  @click="$emit('data-item-remove', {item_name: item.name})"
-                ><v-icon>mdi-delete</v-icon></v-btn>
-              </j-tooltip>
-            </div>
-          </v-row>
-        </v-list>
-
-      </v-menu>
-    </j-tooltip>
+    </v-menu>
+  </j-tooltip>
 </template>
 <script>
 
@@ -74,13 +74,14 @@ module.exports = {
             if (item.ndims !== 1) {
               // filters out table, spectrum 2d, images
               return false
+            } else if (item.meta.Plugin === undefined) {
+              console.log("***", item.name, item.meta.mosviz_row)
             }
-            // TODO: filter by row
             return true
           }
         } else if (this.$props.viewer.config === 'cubeviz') {
           if (this.$props.viewer.reference === 'spectrum-viewer') {
-            if (item.plugin === null) {
+            if (item.meta.Plugin === undefined) {
               // then the data can be a cube (auto-collapsed) as long as its the flux data
               return item.name.indexOf('[FLUX]') !== -1
             } else {
