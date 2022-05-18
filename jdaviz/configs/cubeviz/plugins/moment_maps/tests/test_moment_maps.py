6d3c35fc410db83a8857dd1957c8ca0c6e0c08cb
Pey Lian Lim (Github) <2090236+pllim@users.noreply.github.com>
2022-05-18 12:29:57 -0400

    TST: Reproducible traceback

--- jdaviz/configs/cubeviz/plugins/moment_maps/tests/test_moment_maps.py
+++ jdaviz/configs/cubeviz/plugins/moment_maps/tests/test_moment_maps.py
@@ -10,6 +10,11 @@ from jdaviz.configs.cubeviz.plugins.moment_maps.moment_maps import MomentMap
 def test_moment_calculation(cubeviz_helper, spectrum1d_cube, tmpdir):
     dc = cubeviz_helper.app.data_collection
     cubeviz_helper.load_data(spectrum1d_cube, data_label='test')
+    flux_viewer = cubeviz_helper.app.get_viewer('flux-viewer')
+
+    # Since we are not really displaying, need this to trigger GUI stuff.
+    flux_viewer.shape = (100, 100)
+    flux_viewer.state._set_axes_aspect_ratio(1)
 
     mm = MomentMap(app=cubeviz_helper.app)
     mm.dataset_selected = 'test[FLUX]'
@@ -33,6 +38,9 @@ def test_moment_calculation(cubeviz_helper, spectrum1d_cube, tmpdir):
     assert mm.results_label == 'moment 0'
     assert mm.results_label_overwrite is True
 
+    # Make sure adding it to viewer does not crash.
+    cubeviz_helper.app.add_data_to_viewer('flux-viewer', 'moment 0')
+
     result = dc[1].get_object(cls=CCDData)
     assert result.shape == (4, 2)  # Cube shape is (2, 2, 4)
 
@@ -40,7 +48,6 @@ def test_moment_calculation(cubeviz_helper, spectrum1d_cube, tmpdir):
     assert dc[1].coords is None
 
     # Make sure coordinate display still works
-    flux_viewer = cubeviz_helper.app.get_viewer('flux-viewer')
     flux_viewer.on_mouse_or_key_event({'event': 'mousemove', 'domain': {'x': 0, 'y': 0}})
     assert flux_viewer.state.slices == (0, 0, 1)
     assert flux_viewer.label_mouseover.pixel == 'x=00.0 y=00.0'
