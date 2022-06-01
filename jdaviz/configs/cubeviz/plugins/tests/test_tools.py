c583822458f5ead558cb9210660f515befba7947
Jesse Averbukh <javerbukh@gmail.com>
2022-06-01 15:44:17 -0400

    Fix tests

--- jdaviz/configs/cubeviz/plugins/tests/test_tools.py
+++ jdaviz/configs/cubeviz/plugins/tests/test_tools.py
@@ -17,13 +17,13 @@ def test_spectrum_at_spaxel(cubeviz_helper, spectrum1d_cube):
     assert len(flux_viewer.figure.marks) == 2
     assert len(spectrum_viewer.data()) == 1
 
-    # Click on pixel location
+    # Click on spaxel location
     flux_viewer.toolbar.active_tool.on_mouse_event({'event': 'click', 'domain': {'x': x, 'y': y}})
     assert len(flux_viewer.figure.marks) == 3
     assert len(spectrum_viewer.data()) == 2
-    assert app.data_collection[-1].label == "test_at_pixel"
-    assert app.data_collection[-1].meta["reference_data"] == "test"
-    assert app.data_collection[-1].meta["created_from_pixel"] == f"({x}, {y})"
+    assert app.data_collection[-1].label == "test[FLUX]_at_spaxel"
+    assert app.data_collection[-1].meta["reference_data"] == "test[FLUX]"
+    assert app.data_collection[-1].meta["created_from_spaxel"] == f"({x}, {y})"
 
     # Deselect tool
     flux_viewer.toolbar.active_tool = None
@@ -48,7 +48,7 @@ def test_spectrum_at_spaxel_with_2d(cubeviz_helper):
     assert len(flux_viewer.figure.marks) == 2
     assert len(spectrum_viewer.data()) == 0
 
-    # Click on pixel location
+    # Click on spaxel location
     flux_viewer.toolbar.active_tool.on_mouse_event({'event': 'click', 'domain': {'x': x, 'y': y}})
     assert len(flux_viewer.figure.marks) == 2
     assert len(spectrum_viewer.data()) == 0
