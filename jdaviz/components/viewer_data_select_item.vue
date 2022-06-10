fd7e0a9e7f49135342e984ba7def5cd7d0484b2b
Kyle Conroy <kyleconroy@gmail.com>
2022-06-10 14:30:37 -0400

    ellipsis logic before extension (when applicable)

--- jdaviz/components/viewer_data_select_item.vue
+++ jdaviz/components/viewer_data_select_item.vue
@@ -15,8 +15,15 @@
       </v-btn>
     </j-tooltip>
 
-    <j-tooltip :tooltipcontent="'data label: '+item.name" span_style="font-size: 12pt; padding-top: 6px; padding-left: 6px; width: calc(100% - 80px); text-overflow: ellipsis; white-space: nowrap; overflow-x: hidden; cursor: default">
-      {{item.name}}
+    <j-tooltip :tooltipcontent="'data label: '+item.name" span_style="font-size: 12pt; padding-top: 6px; padding-left: 6px; width: calc(100% - 80px); cursor: default;">
+      <div class="text-ellipsis-middle">
+        <span>
+          {{itemNamePrefix}}
+        </span>
+        <span>
+          {{itemNameExtension}}
+        </span>
+      </div>
     </j-tooltip>
 
 
@@ -36,6 +43,22 @@
 module.exports = {
   props: ['item', 'multi_select', 'viewer'],
   computed: {
+    itemNamePrefix() {
+      if (this.$props.item.name.indexOf("[") !== -1) {
+        // return everything BEFORE the LAST [
+        return this.$props.item.name.split('[').slice(0, -1).join()
+      } else {
+        return this.$props.item.name
+      }
+    },
+    itemNameExtension() {
+      if (this.$props.item.name.indexOf("[") !== -1) {
+        // return the LAST [ and everything FOLLOWING
+        return '['+this.$props.item.name.split('[').slice(-1)
+      } else {
+        return ''
+      }
+    },
     isSelected() {
       return this.$props.viewer.selected_data_items.includes(this.$props.item.id)
     }, 
@@ -52,3 +75,23 @@ module.exports = {
   }
 };
 </script>
+
+<style>
+  .text-ellipsis-middle {
+    display: inline-flex;
+    flex-wrap: nowrap;
+    max-width: 100%;
+  }
+
+  .text-ellipsis-middle > span:first-child {
+    flex: 0 1 auto;
+    text-overflow: ellipsis;
+    overflow:hidden;
+    white-space:nowrap;
+  }
+
+  .text-ellipsis-middle > span + span {
+    flex: 1 0 auto;
+    white-space: nowrap;
+  }
+</style>
