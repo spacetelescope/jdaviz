a8e27868ca6348f88463eef30bb3cb9ffa915125
Sabrina Volfman <svolfman@stsci.edu>
2022-06-27 13:03:49 -0400

    Center point of image in viewer can be found

--- jdaviz/configs/imviz/plugins/conesearch/conesearch.py
+++ jdaviz/configs/imviz/plugins/conesearch/conesearch.py
@@ -4,6 +4,7 @@ from astroquery.sdss import SDSS
 
 from jdaviz.core.registries import tray_registry
 from jdaviz.core.template_mixin import PluginTemplateMixin, ViewerSelectMixin
+from jdaviz.configs.imviz.helper import get_top_layer_index
 
 __all__ = ['ConeSearch']
 
@@ -16,16 +17,30 @@ class ConeSearch(PluginTemplateMixin, ViewerSelectMixin):
         super().__init__(*args, **kwargs)
 
     def vue_do_conesearch(self, *args, **kwargs):
+        # current viewer object
+        curr_viewer = self.viewer.selected_obj
+
+        # used to obtain the current image visible in the viewer
+        viewer = self.app.get_viewer_by_id(self.viewer_selected)
+        i = get_top_layer_index(viewer)
+        data = viewer.state.layers[i].layer
+
+        # obtains the center point of the current image and converts the point into sky coordinates
+        center_point = curr_viewer._get_zoom_center(data)
+        x_center = center_point[0]
+        y_center = center_point[1]
+        skycoord_center = curr_viewer.state.reference_data.coords.pixel_to_world(x_center, y_center)
+
         # queries the region (based on the provided center point and radius) to find all the sources in that region
-        query_region_result = SDSS.query_region(SkyCoord('00h07m13.8036s +14d56m16.8706s', frame='icrs'), radius='0.015 deg', data_release=17)
+        query_region_result = SDSS.query_region(skycoord_center, radius='0.1 deg', data_release=17)
 
         # a table is created storing the 'ra' and 'dec' plottable points of each source found
         skycoord_table = SkyCoord(query_region_result['ra'], query_region_result['dec'], unit='deg')
         conesearch_results = Table({'coord': [skycoord_table]})
 
         # markers are added to the viewer based on the table
-        self.viewer.selected_obj.add_markers(table=conesearch_results, use_skycoord=True, marker_name='conesearch_results')
+        curr_viewer.add_markers(table=conesearch_results, use_skycoord=True, marker_name='conesearch_results')
 
         # get top layer of viewer, zoom limits, work radius from that, convert that to the query
-        # the radius should just be the corner of the image (right now just get some radius and center hardcoded)
+        # the radius should just be the corner of the image
         # get the data outputted?
