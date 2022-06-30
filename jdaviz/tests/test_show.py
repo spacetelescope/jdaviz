c996555cc1c3a3dfa60550c785398c672f6aa892
Duy Nguyen <duytnguyendtn.signin@gmail.com>
2022-06-30 13:52:49 -0400

    Change test args

--- jdaviz/tests/test_show.py
+++ jdaviz/tests/test_show.py
@@ -3,30 +3,30 @@ from pytest import warns
 
 
 def test_show_inline(specviz_helper):
-    res = specviz_helper.show(mode='inline')
+    res = specviz_helper.show(loc='inline')
     assert res is None
 
 
 def test_show_sidecar(specviz_helper):
-    res = specviz_helper.show(mode='sidecar')
+    res = specviz_helper.show(loc='sidecar')
     assert isinstance(res, sidecar.Sidecar)
 
 
 def test_show_new_jupyter_tab(specviz_helper):
-    res = specviz_helper.show(mode="new jupyter tab")
+    res = specviz_helper.show(loc="new jupyter tab")
     assert isinstance(res, sidecar.Sidecar)
 
 
 def test_show_new_browser_tab(specviz_helper):
     with warns(RuntimeWarning, match='Error detected in display'):
-        specviz_helper.show(mode="new browser tab")
+        specviz_helper.show(loc="new browser tab")
 
 
 def test_show_popout(specviz_helper):
     with warns(RuntimeWarning):
-        specviz_helper.show(mode="popout")
+        specviz_helper.show(loc="popout")
 
 
 def test_show_invalid_mode(specviz_helper):
     with warns(RuntimeWarning):
-        specviz_helper.show(mode="thisisnotavaliddisplaymode")
+        specviz_helper.show(loc="thisisnotavaliddisplaymode")
