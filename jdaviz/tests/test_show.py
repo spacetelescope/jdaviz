a4b16fccdf49ebf073ece546cdb0833e2b7ed0af
Duy Nguyen <duytnguyendtn.signin@gmail.com>
2022-07-05 10:37:23 -0400

    Typo

--- jdaviz/tests/test_show.py
+++ jdaviz/tests/test_show.py
@@ -7,6 +7,5 @@ def test_show_popout(specviz_helper):
 
 
 def test_show_invalid_mode(specviz_helper):
-    with pytest.raises(RuntimeError):
-        specviz_helper.show(loc="thisisnotavaliddisplaymode",
-                            match='Error in displaying Jdaviz')
+    with pytest.raises(RuntimeError, match='Error in displaying Jdaviz'):
+        specviz_helper.show(loc="thisisnotavaliddisplaymode")
