5c4f54e5ec1be69555172bb96afb915e7d58c10d
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2022-07-17 00:51:30 -0500

    Edits and fixes from pllim

--- jdaviz/configs/cubeviz/plugins/tests/test_parsers.py
+++ jdaviz/configs/cubeviz/plugins/tests/test_parsers.py
@@ -32,7 +32,7 @@ def image_hdu_obj():
     })
 
     flux_hdu.header.update(wcs.to_header())
-    flux_hdu.header['BUNIT'] = '1E-17 erg*s^-1*cm^-2*Angstrom^-1*pix^-1'
+    flux_hdu.header['BUNIT'] = '1E-17 erg*s^-1*cm^-2*Angstrom^-1'
 
     mask_hdu.header.update(wcs.to_header())
     uncert_hdu.header.update(wcs.to_header())
@@ -42,16 +42,16 @@ def image_hdu_obj():
 
 @pytest.fixture
 def image_hdu_obj_microns():
-    flux_hdu = fits.ImageHDU(np.ones((10, 10, 10)))
+    flux_hdu = fits.ImageHDU(np.ones((8, 9, 10)).astype(np.float32))
     flux_hdu.name = 'FLUX'
 
-    mask_hdu = fits.ImageHDU(np.zeros((10, 10, 10)))
-    mask_hdu.name = 'MASK'
-
-    uncert_hdu = fits.ImageHDU(np.ones((10, 10, 10)))
+    uncert_hdu = fits.ImageHDU(np.zeros((8, 9, 10)).astype(np.float32))
     uncert_hdu.name = 'ERR'
 
-    wcs = WCS(header={
+    mask_hdu = fits.ImageHDU(np.ones((8, 9, 10)).astype(np.int16))
+    mask_hdu.name = 'MASK'
+
+    wcs = WCS({
         'WCSAXES': 3, 'CRPIX1': 38.0, 'CRPIX2': 38.0, 'CRPIX3': 1.0,
         'PC1_1 ': -0.000138889, 'PC2_2 ': 0.000138889,
         'PC3_3 ': 8.33903304339E-11, 'CDELT1': 1.0, 'CDELT2': 1.0,
@@ -60,19 +60,16 @@ def image_hdu_obj_microns():
         'CRVAL1': 205.4384, 'CRVAL2': 27.004754, 'CRVAL3': 3.62159598486E-07,
         'LONPOLE': 180.0, 'LATPOLE': 27.004754, 'MJDREFI': 0.0,
         'MJDREFF': 0.0, 'DATE-OBS': '2014-03-30',
-        'RADESYS': 'FK5', 'EQUINOX': 2000.0
-    })
+        'RADESYS': 'FK5', 'EQUINOX': 2000.0})
 
     fits_primary = fits.PrimaryHDU()
-    fits_primary.header['CUNIT3'] = 'um'
 
     flux_hdu.header.update(wcs.to_header())
-    flux_hdu.header['BUNIT'] = '1E-17 erg*s^-1*cm^-2*Angstrom^-1*pix^-1'
+    flux_hdu.header['BUNIT'] = '1E-17 erg*s^-1*cm^-2*Angstrom^-1'
 
-    mask_hdu.header.update(wcs.to_header())
-    uncert_hdu.header.update(wcs.to_header())
+    uncert_hdu.header['BUNIT'] = '1E-17 erg*s^-1*cm^-2*Angstrom^-1'
 
-    return fits.HDUList([fits_primary, flux_hdu, mask_hdu, uncert_hdu])
+    return fits.HDUList([fits_primary, flux_hdu, uncert_hdu, mask_hdu])
 
 
 @pytest.mark.filterwarnings('ignore')
@@ -114,7 +111,7 @@ def test_fits_image_hdu_parse_from_file_with_microns(tmpdir,
     flux_viewer = cubeviz_helper.app.get_viewer('flux-viewer')
     flux_viewer.on_mouse_or_key_event({'event': 'mousemove', 'domain': {'x': 0, 'y': 0}})
     assert flux_viewer.label_mouseover.pixel == 'x=00.0 y=00.0'
-    assert flux_viewer.label_mouseover.value == '+1.00000e+00 1e-17 erg / (Angstrom cm2 pix s)'
+    assert flux_viewer.label_mouseover.value == '+1.00000e+00 1e-17 erg / (Angstrom cm2 s)'
 
     unc_viewer = cubeviz_helper.app.get_viewer('uncert-viewer')
     unc_viewer.on_mouse_or_key_event({'event': 'mousemove', 'domain': {'x': -1, 'y': 0}})
@@ -124,7 +121,7 @@ def test_fits_image_hdu_parse_from_file_with_microns(tmpdir,
     mask_viewer = cubeviz_helper.app.get_viewer('mask-viewer')
     mask_viewer.on_mouse_or_key_event({'event': 'mousemove', 'domain': {'x': 9, 'y': 0}})
     assert mask_viewer.label_mouseover.pixel == 'x=09.0 y=00.0'
-    assert mask_viewer.label_mouseover.value == '+0.00000e+00 ct'  # No unit define, ct as fallback
+    assert mask_viewer.label_mouseover.value == '+1.00000e+00 '  # Mask should be unitless
 
 
 @pytest.mark.filterwarnings('ignore')
@@ -148,9 +145,9 @@ def test_fits_image_hdu_parse_from_file(tmpdir, image_hdu_obj, cubeviz_helper):
     flux_viewer = cubeviz_helper.app.get_viewer('flux-viewer')
     flux_viewer.on_mouse_or_key_event({'event': 'mousemove', 'domain': {'x': 0, 'y': 0}})
     assert flux_viewer.label_mouseover.pixel == 'x=00.0 y=00.0'
-    assert flux_viewer.label_mouseover.value == '+1.00000e+00 1e-17 erg / (Angstrom cm2 pix s)'
-    assert flux_viewer.label_mouseover.world_ra_deg == '205.4433848390'
-    assert flux_viewer.label_mouseover.world_dec_deg == '26.9996149270'
+    assert flux_viewer.label_mouseover.value == '+1.00000e+00 1e-17 erg / (Angstrom cm2 s)'
+    assert flux_viewer.label_mouseover.world_ra_deg == '205.4441642302'
+    assert flux_viewer.label_mouseover.world_dec_deg == '26.9996148973'
 
     unc_viewer = cubeviz_helper.app.get_viewer('uncert-viewer')
     unc_viewer.on_mouse_or_key_event({'event': 'mousemove', 'domain': {'x': -1, 'y': 0}})
@@ -162,7 +159,7 @@ def test_fits_image_hdu_parse_from_file(tmpdir, image_hdu_obj, cubeviz_helper):
     mask_viewer = cubeviz_helper.app.get_viewer('mask-viewer')
     mask_viewer.on_mouse_or_key_event({'event': 'mousemove', 'domain': {'x': 9, 'y': 0}})
     assert mask_viewer.label_mouseover.pixel == 'x=09.0 y=00.0'
-    assert mask_viewer.label_mouseover.value == '+0.00000e+00 ct'  # No unit define, ct as fallback
+    assert mask_viewer.label_mouseover.value == '+0.00000e+00 '  # Mask should be unitless
     assert mask_viewer.label_mouseover.world_ra_deg == '205.4441642302'
     assert mask_viewer.label_mouseover.world_dec_deg == '26.9996148973'
 
@@ -181,7 +178,7 @@ def test_spectrum3d_parse(image_hdu_obj, cubeviz_helper):
     flux_viewer = cubeviz_helper.app.get_viewer('flux-viewer')
     flux_viewer.on_mouse_or_key_event({'event': 'mousemove', 'domain': {'x': 0, 'y': 0}})
     assert flux_viewer.label_mouseover.pixel == 'x=00.0 y=00.0'
-    assert flux_viewer.label_mouseover.value == '+1.00000e+00 1e-17 erg / (Angstrom cm2 pix s)'
+    assert flux_viewer.label_mouseover.value == '+1.00000e+00 1e-17 erg / (Angstrom cm2 s)'
     assert flux_viewer.label_mouseover.world_ra_deg == '205.4433848390'
     assert flux_viewer.label_mouseover.world_dec_deg == '26.9996149270'
 
