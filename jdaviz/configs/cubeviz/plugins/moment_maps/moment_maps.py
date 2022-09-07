ea8e99c49ac69ed6e762cde553abd01659e52bf8
Kyle Conroy <kyleconroy@gmail.com>
2022-09-07 09:54:03 -0400

    user API for moment maps plugin

--- jdaviz/configs/cubeviz/plugins/moment_maps/moment_maps.py
+++ jdaviz/configs/cubeviz/plugins/moment_maps/moment_maps.py
@@ -13,6 +13,7 @@ from jdaviz.core.template_mixin import (PluginTemplateMixin,
                                         DatasetSelectMixin,
                                         SpectralSubsetSelectMixin,
                                         AddResultsMixin)
+from jdaviz.core.user_api import PluginUserApi
 
 __all__ = ['MomentMap']
 
@@ -24,6 +25,22 @@ u.add_enabled_units([spaxel])
 @tray_registry('cubeviz-moment-maps', label="Moment Maps")
 class MomentMap(PluginTemplateMixin, DatasetSelectMixin, SpectralSubsetSelectMixin,
                 AddResultsMixin):
+    """
+    See the :ref:`Moment Maps Plugin Documentation <moment-maps>` for more details.
+
+    Only the following attributes and methods are available through the
+    :ref:`public plugin API <plugin-apis>`:
+
+    * :meth:`~jdaviz.core.template_mixin.PluginTemplateMixin.show`
+    * :meth:`~jdaviz.core.template_mixin.PluginTemplateMixin.open_in_tray`
+    * ``dataset`` (:class:`~jdaviz.core.template_mixin.DatasetSelect`):
+      Dataset to use for computing line statistics.
+    * ``spectral_subset`` (:class:`~jdaviz.core.template_mixin.SubsetSelect`):
+      Subset to use for the line, or ``Entire Spectrum``.
+    * ``n_moment``
+    * ``add_results`` (:class:`~jdaviz.core.template_mixin.AddResults`)
+    * :meth:`calculate_moment`
+    """
     template_file = __file__, "moment_maps.vue"
 
     n_moment = IntHandleEmpty(0).tag(sync=True)
@@ -38,6 +55,13 @@ class MomentMap(PluginTemplateMixin, DatasetSelectMixin, SpectralSubsetSelectMix
         self.dataset.add_filter('is_image')
         self.add_results.viewer.filters = ['is_image_viewer']
 
+    @property
+    def user_api(self):
+        # NOTE: leaving save_as_fits out for now - we may want a more general API to do that
+        # accross all plugins at some point
+        return PluginUserApi(self, expose=('dataset', 'spectral_subset', 'n_moment',
+                                           'add_results', 'calculate_moment'))
+
     @observe("dataset_selected", "dataset_items", "n_moment")
     def _set_default_results_label(self, event={}):
         label_comps = []
@@ -46,7 +70,15 @@ class MomentMap(PluginTemplateMixin, DatasetSelectMixin, SpectralSubsetSelectMix
         label_comps += [f"moment {self.n_moment}"]
         self.results_label_default = " ".join(label_comps)
 
-    def vue_calculate_moment(self, *args):
+    def calculate_moment(self, add_data=True):
+        """
+        Calculate the moment map
+
+        Parameters
+        ----------
+        add_data : bool
+            Whether to add the resulting data object to the app according to ``add_results``.
+        """
         # Retrieve the data cube and slice out desired region, if specified
         cube = self.dataset.get_object(cls=Spectrum1D, statistic=None)
         spec_min, spec_max = self.spectral_subset.selected_min_max(cube)
@@ -67,13 +99,19 @@ class MomentMap(PluginTemplateMixin, DatasetSelectMixin, SpectralSubsetSelectMix
         fname_label = self.dataset_selected.replace("[", "_").replace("]", "")
         self.filename = f"moment{n_moment}_{fname_label}.fits"
 
-        self.add_results.add_results_from_plugin(self.moment)
+        if add_data:
+            self.add_results.add_results_from_plugin(self.moment)
+
+            msg = SnackbarMessage("{} added to data collection".format(self.results_label),
+                                  sender=self, color="success")
+            self.hub.broadcast(msg)
 
         self.moment_available = True
 
-        msg = SnackbarMessage("{} added to data collection".format(self.results_label),
-                              sender=self, color="success")
-        self.hub.broadcast(msg)
+        return self.moment
+
+    def vue_calculate_moment(self, *args):
+        self.calculate_moment(add_data=True)
 
     def vue_save_as_fits(self, *args):
         if self.moment is None or not self.filename:  # pragma: no cover
