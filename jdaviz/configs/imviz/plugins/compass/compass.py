69517e79211de3632fb884c48e812d8ebe637a37
Kyle Conroy <kyleconroy@gmail.com>
2022-09-07 13:18:56 -0400

    user API for compass plugin

--- jdaviz/configs/imviz/plugins/compass/compass.py
+++ jdaviz/configs/imviz/plugins/compass/compass.py
@@ -3,12 +3,24 @@ from traitlets import Unicode, observe
 from jdaviz.core.events import AddDataMessage, RemoveDataMessage
 from jdaviz.core.registries import tray_registry
 from jdaviz.core.template_mixin import PluginTemplateMixin, ViewerSelectMixin
+from jdaviz.core.user_api import PluginUserApi
 
 __all__ = ['Compass']
 
 
 @tray_registry('imviz-compass', label="Imviz Compass")
 class Compass(PluginTemplateMixin, ViewerSelectMixin):
+    """
+    See the :ref:`Compass Plugin Documentation <imviz-compass>` for more details.
+
+    Only the following attributes and methods are available through the
+    :ref:`public plugin API <plugin-apis>`:
+
+    * :meth:`~jdaviz.core.template_mixin.PluginTemplateMixin.show`
+    * :meth:`~jdaviz.core.template_mixin.PluginTemplateMixin.open_in_tray`
+    * ``viewer`` (:class:`~jdaviz.core.template_mixin.ViewerSelect`):
+      Viewer to show orientation/compass information.
+    """
     template_file = __file__, "compass.vue"
     icon = Unicode("").tag(sync=True)
     data_label = Unicode("").tag(sync=True)
@@ -19,6 +31,14 @@ class Compass(PluginTemplateMixin, ViewerSelectMixin):
 
         self.hub.subscribe(self, AddDataMessage, handler=self._on_viewer_data_changed)
         self.hub.subscribe(self, RemoveDataMessage, handler=self._on_viewer_data_changed)
+
+    @property
+    def user_api(self):
+        return PluginUserApi(self, expose=('viewer',))
+
+    def show(self, *args, **kwargs):
+        super().show(*args, **kwargs)
+        self._compass_with_new_viewer(from_show=True)
 
     def _on_viewer_data_changed(self, msg=None):
         if self.viewer_selected:
@@ -33,7 +53,7 @@ class Compass(PluginTemplateMixin, ViewerSelectMixin):
 
         # There can be only one!
         for vid, viewer in self.app._viewer_store.items():
-            if vid == self.viewer.selected_id and self.plugin_opened:
+            if vid == self.viewer.selected_id and (self.plugin_opened or kwargs.get('from_show')):
                 viewer.compass = self
                 viewer.on_limits_change()  # Force redraw
             else:
