52ed6254692a4c795542a9d7636d4fc369827108
Kyle Conroy <kyleconroy@gmail.com>
2022-10-04 13:53:22 -0400

    regression test

---
+++ jdaviz/configs/default/plugins/subset_plugin/tests/test_subset_plugin.py
@@ -0,0 +1,18 @@
+import pytest
+from glue.core.roi import XRangeROI
+
+
+@pytest.mark.filterwarnings('ignore')
+def test_plugin(specviz_helper, spectrum1d):
+    specviz_helper.load_data(spectrum1d)
+    p = specviz_helper.plugins['Subset Tools']
+
+    # regression test for https://github.com/spacetelescope/jdaviz/issues/1693
+    sv = specviz_helper.app.get_viewer('spectrum-viewer')
+    sv.apply_roi(XRangeROI(6500, 7400))
+
+    p._obj.subset_select.selected = 'Create New'
+
+    po = specviz_helper.plugins['Plot Options']
+    po.layer = 'Subset 1'
+    po.line_color = 'green'
