Correcting masking bugs

jdaviz/configs/default/plugins/model_fitting/model_fitting.py
@@ -826,7 +826,7 @@ class ModelFitting(PluginTemplateMixin, DatasetSelectMixin,
                     subset_mask = np.all(subset_mask, axis=(0, 1))
             spectrum.mask |= subset_mask
         else:
-            if subset_mask.ndim > spectrum.flux.ndim:
+            if subset_mask.ndim < spectrum.flux.ndim:
                 # correct the shape of spectral/spatial axes when they're different:
                 subset_mask = np.broadcast_to(subset_mask, spectrum.flux.shape)
 

jdaviz/conftest.py
@@ -179,9 +179,9 @@ def multi_order_spectrum_list(spectrum1d, spectral_orders=10):
     return SpectrumList(sc)
 
 
-def _create_spectrum1d_cube_with_fluxunit(fluxunit=u.Jy):
+def _create_spectrum1d_cube_with_fluxunit(fluxunit=u.Jy, shape=(2, 2, 4)):
 
-    flux = np.arange(16).reshape((2, 2, 4)) * fluxunit
+    flux = np.arange(np.prod(shape)).reshape(shape) * fluxunit
     wcs_dict = {"CTYPE1": "RA---TAN", "CTYPE2": "DEC--TAN", "CTYPE3": "WAVE-LOG",
                 "CRVAL1": 205, "CRVAL2": 27, "CRVAL3": 4.622e-7,
                 "CDELT1": -0.0001, "CDELT2": 0.0001, "CDELT3": 8e-11,
@@ -193,7 +193,12 @@ def _create_spectrum1d_cube_with_fluxunit(fluxunit=u.Jy):
 
 @pytest.fixture
 def spectrum1d_cube():
-    return _create_spectrum1d_cube_with_fluxunit(u.Jy)
+    return _create_spectrum1d_cube_with_fluxunit(fluxunit=u.Jy)
+
+
+@pytest.fixture
+def spectrum1d_cube_larger():
+    return _create_spectrum1d_cube_with_fluxunit(fluxunit=u.Jy, shape=(SPECTRUM_SIZE, 2, 4))
 
 
 @pytest.fixture

jdaviz/core/tests/test_template_mixin.py
@@ -32,8 +32,8 @@ def test_spectralsubsetselect(specviz_helper, spectrum1d):
 
     # check selected subset mask available via API:
     expected_mask_with_spectral_subset = (
-        (spectrum1d.wavelength.to(u.AA).value > 6500) &
-        (spectrum1d.wavelength.to(u.AA).value < 7400)
+        (spectrum1d.wavelength.to(u.AA).value < 6500) |
+        (spectrum1d.wavelength.to(u.AA).value > 7400)
     )
     assert np.all(
         expected_mask_with_spectral_subset == p.spectral_subset.selected_subset_mask