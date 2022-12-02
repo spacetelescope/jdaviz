c19a821cd0b8b76c180420b82e82cd5a6b2a6f00
Duy Nguyen <duytnguyendtn.open@gmail.com>
2022-12-02 14:10:20 -0500

    Add direct launchers for configs

--- jdaviz/cli.py
+++ jdaviz/cli.py
@@ -85,14 +85,15 @@ def main(filename, layout='default', instrument=None, browser='default',
         os.chdir(start_dir)
 
 
-def _main():
+def _main(force_layout=None):
     import argparse
     import sys
 
     parser = argparse.ArgumentParser(description='Start a Jdaviz application instance with data '
                                      'loaded from FILENAME.')
-    parser.add_argument('layout', choices=['cubeviz', 'specviz', 'specviz2d', 'mosviz', 'imviz'],
-                        help='Configuration to use.')
+    if force_layout == None:
+        parser.add_argument('layout', choices=['cubeviz', 'specviz', 'specviz2d', 'mosviz', 'imviz'],
+                            help='Configuration to use.')
     parser.add_argument('filename', type=str,
                         help='The path to the file to be loaded into the Jdaviz application.')
     parser.add_argument('--instrument', type=str, default='nirspec',
@@ -115,6 +116,30 @@ def _main():
     parser.add_argument('--version', action='version', version=f'%(prog)s {__version__}')
     args = parser.parse_args()
 
-    main(args.filename, layout=args.layout, instrument=args.instrument, browser=args.browser,
+    if force_layout == None:
+        layout = args.layout
+    else:
+        layout = force_layout
+
+    main(args.filename, layout=layout, instrument=args.instrument, browser=args.browser,
          theme=args.theme, verbosity=args.verbosity, history_verbosity=args.history_verbosity,
          hotreload=args.hotreload)
+
+def _specviz():
+    _main(force_layout='specviz')
+
+
+def _specviz2d():
+    _main(force_layout='specviz2d')
+
+
+def _imviz():
+    _main(force_layout='imviz')
+
+
+def _cubeviz():
+    _main(force_layout='cubeviz')
+
+
+def _mosviz():
+    _main(force_layout='mosviz')
