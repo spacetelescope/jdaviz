e7d1237d58bdaf171cd575372ba875d4693f418c
Kyle Conroy <kyleconroy@gmail.com>
2022-12-06 15:09:09 -0500

    override reset_x_limits for profile viewer

--- jdaviz/core/freezable_state.py
+++ jdaviz/core/freezable_state.py
@@ -1,3 +1,6 @@
+from echo import delay_callback
+import numpy as np
+
 from glue.viewers.profile.state import ProfileViewerState
 from glue_jupyter.bqplot.image.state import BqplotImageViewerState
 from glue.viewers.matplotlib.state import DeferredDrawCallbackProperty as DDCProperty
@@ -18,6 +21,29 @@ class FreezableState():
 
 class FreezableProfileViewerState(ProfileViewerState, FreezableState):
     show_uncertainty = DDCProperty(False, docstring='Whether to show data uncertainties')
+
+    def _reset_x_limits(self, *event):
+        # override glue's _reset_x_limits to account for all layers,
+        # not just reference data (_reset_y_limits already does so)
+        # This is essentially copied directly from Glue's
+        # ProfileViewerState._reset_y_limits but modified for x-limits
+
+        x_min, x_max = np.inf, -np.inf
+        for layer in self.layers:
+            try:
+                profile = layer.profile
+            except Exception:  # nosec
+                # e.g. incompatible subset
+                continue
+            if profile is not None:
+                x, y = profile
+                if len(x) > 0:
+                    x_min = min(x_min, np.nanmin(x))
+                    x_max = max(x_max, np.nanmax(x))
+
+        with delay_callback(self, 'x_min', 'x_max'):
+            self.x_min = x_min
+            self.x_max = x_max
 
 
 class FreezableBqplotImageViewerState(BqplotImageViewerState, FreezableState):
