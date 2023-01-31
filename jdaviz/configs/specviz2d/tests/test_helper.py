ad2e6c4cc5314f6c4d563255868abe175486c502
Jesse Averbukh <javerbukh@gmail.com>
2023-01-31 10:27:21 -0500

    Cover specviz2d specific line with test

--- jdaviz/configs/specviz2d/tests/test_helper.py
+++ jdaviz/configs/specviz2d/tests/test_helper.py
@@ -5,6 +5,8 @@ def test_helper(specviz2d_helper, spectrum1d):
     specviz2d_helper.load_data(spectrum_1d=spectrum1d)
     assert isinstance(specviz2d_helper.specviz, Specviz)
 
+    specviz2d_helper.app.data_collection[0].meta['Trace'] = "Test"
+
     returned_data = specviz2d_helper.get_data()
     assert len(returned_data.shape) == 1
 
