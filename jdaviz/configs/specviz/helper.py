f2b88643b2bde9d6a4656a265fbdcb2dde72a669
Ricky O'Steen <rosteen@stsci.edu>
2023-03-20 14:12:39 -0400

    Bug fixes for get_spectra in other configs

--- jdaviz/configs/specviz/helper.py
+++ jdaviz/configs/specviz/helper.py
@@ -76,12 +76,15 @@ class Specviz(ConfigHelper, LineListMixin):
         # Just to save line length
         get_data_method = self.app._jdaviz_helper.get_data
         viewer = self.app.get_viewer(self._default_spectrum_viewer_reference_name)
-        statistic = getattr(viewer, "function", None)
+        statistic = None
+        if self.app.config == "cubeviz":
+            statistic = getattr(viewer.state, "function")
 
         if data_label is not None:
             spectrum = get_data_method(data_label=data_label,
                                        subset_to_apply=subset_to_apply,
-                                       statistic=statistic)
+                                       statistic=statistic,
+                                       cls=Spectrum1D)
             spectra[data_label] = spectrum
         else:
             for layer_state in viewer.state.layers:
@@ -90,19 +93,26 @@ class Specviz(ConfigHelper, LineListMixin):
                     if lyr.label == subset_to_apply:
                         spectrum = get_data_method(data_label=lyr.data.label,
                                                    subset_to_apply=subset_to_apply,
-                                                   statistic=statistic)
-                        spectra[lyr.data.label] = spectrum
+                                                   statistic=statistic,
+                                                   cls=Spectrum1D)
+                        # Work around get_data returning Data even with cls specified
+                        if isinstance(spectrum, Spectrum1D):
+                            spectra[lyr.data.label] = spectrum
                     else:
                         continue
                 else:
                     if isinstance(lyr, GroupedSubset):
                         spectrum = get_data_method(data_label=lyr.data.label,
                                                    subset_to_apply=lyr.label,
-                                                   statistic=statistic)
-                        spectra[f'{lyr.data.label} ({lyr.label})'] = spectrum
+                                                   statistic=statistic,
+                                                   cls=Spectrum1D)
+                        # Work around get_data returning Data even with cls specified
+                        if isinstance(spectrum, Spectrum1D):
+                            spectra[f'{lyr.data.label} ({lyr.label})'] = spectrum
                     else:
                         spectrum = get_data_method(data_label=lyr.label,
-                                                   statistic=statistic)
+                                                   statistic=statistic,
+                                                   cls=Spectrum1D)
                         spectra[lyr.label] = spectrum
 
         if not apply_slider_redshift:
