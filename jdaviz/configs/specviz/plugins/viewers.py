d83ad8dfb7de80e48d0b470427c91dd2ab33f4ea
Kyle Conroy <kyleconroy@gmail.com>
2023-05-15 08:33:38 -0400

    yrangezoom tool
    implements a yrangezoom tool and uses it in specviz, cubeviz, mosviz, specviz2d (for spectrum and spectrum-2d viewers)

--- jdaviz/configs/specviz/plugins/viewers.py
+++ jdaviz/configs/specviz/plugins/viewers.py
@@ -26,7 +26,7 @@ class SpecvizProfileView(JdavizViewerMixin, BqplotProfileView):
     # categories: zoom resets, zoom, pan, subset, select tools, shortcuts
     tools_nested = [
                     ['jdaviz:homezoom', 'jdaviz:prevzoom'],
-                    ['jdaviz:boxzoom', 'jdaviz:xrangezoom'],
+                    ['jdaviz:boxzoom', 'jdaviz:xrangezoom', 'jdaviz:yrangezoom'],
                     ['jdaviz:panzoom', 'jdaviz:panzoom_x', 'jdaviz:panzoom_y'],
                     ['bqplot:xrange'],
                     ['jdaviz:selectline'],
