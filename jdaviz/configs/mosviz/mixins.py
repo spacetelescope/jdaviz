a5053759a4b6a1c5ee80ce4dd4631b1ce042907c
Kyle Conroy <kyleconroy@gmail.com>
2023-06-05 09:00:47 -0400

    implement alt method for preventing feedback loops
    * should handle the case where there is a discontinuity which was causing the viewer with user-set limits to be updated outside those limits

--- jdaviz/configs/mosviz/mixins.py
+++ jdaviz/configs/mosviz/mixins.py
@@ -2,6 +2,8 @@ __all__ = ['Matched2dSpectrumMixin']
 
 
 class Matched2dSpectrumMixin:
+    _during_limit_sync = False
+
     def _setup_xaxes_linking(self):
         spec1d = self.app.get_viewer(self._default_spectrum_viewer_reference_name)
         spec2d = self.app.get_viewer(self._default_spectrum_2d_viewer_reference_name)
@@ -10,6 +12,9 @@ class Matched2dSpectrumMixin:
             spec2d.state.add_callback(k, self._on_spec2d_x_limits_changed)
 
     def _on_spec1d_x_limits_changed(self, *args):
+        if self._during_limit_sync:
+            # prevent feedback-loop
+            return
         spec1d = self.app.get_viewer(self._default_spectrum_viewer_reference_name)
         spec2d = self.app.get_viewer(self._default_spectrum_2d_viewer_reference_name)
         if spec1d.state.reference_data is None or spec2d.state.reference_data is None:
@@ -22,16 +27,16 @@ class Matched2dSpectrumMixin:
             spec2d_limits = spec2d.world_to_pixel_limits(spec1d_limits)
 
         spec2d_x_scales = spec2d.scales['x']
-        rng = abs(spec2d_x_scales.max - spec2d_x_scales.min)
-        if (abs(spec2d_limits[0] - spec2d_x_scales.min)/rng < 0.01 and
-                abs(spec2d_limits[1] - spec2d_x_scales.max)/rng < 0.01):
-            # avoid infinite recursion
-            return
 
+        self._during_limit_sync = True
         with spec2d_x_scales.hold_sync():
             spec2d_x_scales.min, spec2d_x_scales.max = spec2d_limits
+        self._during_limit_sync = False
 
     def _on_spec2d_x_limits_changed(self, *args):
+        if self._during_limit_sync:
+            # prevent feedback-loop
+            return
         spec1d = self.app.get_viewer(self._default_spectrum_viewer_reference_name)
         spec2d = self.app.get_viewer(self._default_spectrum_2d_viewer_reference_name)
         if spec1d.state.reference_data is None or spec2d.state.reference_data is None:
@@ -44,10 +49,8 @@ class Matched2dSpectrumMixin:
             spec1d_limits = spec2d.pixel_to_world_limits(spec2d_limits)
 
         spec1d_x_scales = spec1d.scales['x']
-        rng = abs(spec1d_x_scales.max - spec1d_x_scales.min)
-        if (abs(spec1d_limits[0] - spec1d_x_scales.min)/rng < 0.01 and
-                abs(spec1d_limits[1] - spec1d_x_scales.max)/rng < 0.01):
-            # avoid infinite recursion
-            return
+
+        self._during_limit_sync = True
         with spec1d_x_scales.hold_sync():
             spec1d_x_scales.min, spec1d_x_scales.max = spec1d_limits
+        self._during_limit_sync = False
