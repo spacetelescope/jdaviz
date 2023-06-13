dfe785315de15b29ef1da073b73f0b1e1f9623b8
Brett M. Morris <bmmorris@stsci.edu>
2023-06-13 13:08:35 -0400

    only add refdata tooltip for imviz, remove refdata references in tooltip

--- jdaviz/components/viewer_data_select_item.vue
+++ jdaviz/components/viewer_data_select_item.vue
@@ -12,7 +12,7 @@
       </j-tooltip>
     </div>
     <j-tooltip
-      :tooltipcontent="isRefData() ? 'Reference data' : 'Click to set as reference data'"
+      :tooltipcontent="isRefData() && viewer.config === 'imviz' ? 'Current viewer orientation' : 'Set viewer orientation'"
       span_style="font-size: 12pt; padding-top: 6px; padding-left: 4px; padding-right: 16px; width: calc(100% - 80px); white-space: nowrap; cursor: default;">
     <span
       style="cursor: pointer;"
