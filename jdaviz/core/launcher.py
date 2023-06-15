d3d251374039582b84119e8a7f757241907d574f
Duy Nguyen <duytnguyendtn.open@gmail.com>
2023-06-15 13:30:57 -0400

    Add functional but ugly launcher

---
+++ jdaviz/core/launcher.py
@@ -0,0 +1,53 @@
+import ipyvuetify as v
+from ipywidgets import jslink
+from traitlets import Dict
+
+from jdaviz import configs as jdaviz_configs
+from jdaviz.core.data_formats import open as jdaviz_open
+
+
+def show_launcher(configs=['imviz', 'specviz', 'mosviz', 'cubeviz', 'specviz2d']):
+    main = v.Sheet()
+    main.add_traits(_metadata=Dict(default_value={'mount_id': 'content'}).tag(sync=True))
+    main.children = []
+
+    # Create Intro Row
+    intro_row = v.Row()
+    welcome_text = v.Html(tag='h1', attributes={'title': 'a title'},
+                          children=['Welcome to Jdaviz'])
+    #links = 
+    intro_row.children = [welcome_text]
+
+    # Filepath row
+    filepath_row = v.Row()
+    text_field = v.TextField(label="URI or File Path", v_model=None)
+
+    def load_file(filepath):
+        print(filepath)
+        if filepath:
+            helper = jdaviz_open(filepath, show=False)
+            main.children = [helper.app]
+    
+    open_data_btn = v.Btn(class_="ma-2", outlined=True, color="primary", children=[v.Icon(children=["mdi-upload"])])
+    open_data_btn.on_event('click', lambda btn, event, data: load_file(btn.value))
+    jslink((text_field, 'v_model'), (open_data_btn, 'value'))
+    
+    filepath_row.children = [text_field, open_data_btn]
+
+    # Config buttons
+    def create_config(config):
+        viz_class = getattr(jdaviz_configs, config.capitalize())
+        main.children = [viz_class().app]
+
+    btns = []
+    for config in configs:
+        config_btn = v.Btn(class_="ma-2", outlined=True, color="primary", children=[config.capitalize()])
+        config_btn.on_event('click', lambda btn, event, data: create_config(btn.children[0]))
+        btns.append(config_btn)
+
+    # Create button row
+    btn_row = v.Row()
+    btn_row.children = btns
+    main.children = [intro_row, filepath_row, btn_row]
+
+    return main
