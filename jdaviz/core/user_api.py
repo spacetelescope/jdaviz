24e08c5f7c4017cbb01b3bdbaa934f7627ea12e9
Kyle Conroy <kyleconroy@gmail.com>
2023-08-08 10:17:14 -0400

    inapplicable_attrs for plugin user APIs
    * allows a plugin to control which exposed traitlets/methods are inapplicable and should be "hidden" from the user based on that values of other input parameters

--- jdaviz/core/user_api.py
+++ jdaviz/core/user_api.py
@@ -2,7 +2,7 @@ import astropy.units as u
 
 __all__ = ['UserApiWrapper', 'PluginUserApi', 'ViewerUserApi']
 
-_internal_attrs = ('_obj', '_expose', '_readonly', '__doc__')
+_internal_attrs = ('_obj', '_expose', '_readonly', '_inapplicable_attrs', '__doc__')
 
 
 class UserApiWrapper:
@@ -10,10 +10,11 @@ class UserApiWrapper:
     This is an API wrapper around an internal object.  For a full list of attributes/methods,
     call dir(object).
     """
-    def __init__(self, obj, expose=[], readonly=[]):
+    def __init__(self, obj, expose=[], readonly=[], inapplicable_attrs=None):
         self._obj = obj
         self._expose = list(expose) + list(readonly)
         self._readonly = readonly
+        self._inapplicable_attrs = inapplicable_attrs  # string of a traitlet in obj or None
         if obj.__doc__ is not None:
             self.__doc__ = self.__doc__ + "\n\n\n" + obj.__doc__
 
@@ -30,6 +31,10 @@ class UserApiWrapper:
         if attr in _internal_attrs or attr not in self._expose:
             return super().__getattribute__(attr)
 
+        if self._inapplicable_attrs is not None:
+            if attr in getattr(self._obj, self._inapplicable_attrs):
+                raise AttributeError(f"{attr} is not currently applicable due to values of other parameters")  # noqa
+
         exp_obj = getattr(self._obj, attr)
         return getattr(exp_obj, 'user_api', exp_obj)
 
@@ -38,6 +43,10 @@ class UserApiWrapper:
             return super().__setattr__(attr, value)
         if attr not in self._expose:
             raise ValueError(f"{attr} is not a valid attribute and cannot be set")
+
+        if self._inapplicable_attrs is not None:
+            if attr in getattr(self._obj, self._inapplicable_attrs):
+                raise AttributeError(f"{attr} is not currently applicable due to values of other parameters")  # noqa
 
         if attr in self._readonly:
             raise AttributeError("cannot set read-only item")
@@ -90,11 +99,11 @@ class PluginUserApi(UserApiWrapper):
     For example::
       help(plugin_object.show)
     """
-    def __init__(self, plugin, expose=[], readonly=[]):
+    def __init__(self, plugin, expose=[], readonly=[], inapplicable_attrs=None):
         expose = list(set(list(expose) + ['open_in_tray', 'close_in_tray', 'show']))
         if plugin.uses_active_status:
             expose += ['keep_active', 'as_active']
-        super().__init__(plugin, expose, readonly)
+        super().__init__(plugin, expose, readonly, inapplicable_attrs)
 
     def __repr__(self):
         return f'<{self._obj._registry_label} API>'
