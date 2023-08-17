7075bd36b1dc82729245b59570b02665793d4de2
Ricky O'Steen <rosteen@stsci.edu>
2023-08-17 15:31:07 -0400

    This is working locally now....

--- standalone/hooks/hook-jdaviz.py
+++ standalone/hooks/hook-jdaviz.py
@@ -1,4 +1,5 @@
-from PyInstaller.utils.hooks import collect_data_files, copy_metadata
+from PyInstaller.utils.hooks import collect_data_files, copy_metadata, collect_submodules
 
+hiddenimports = collect_submodules("jdaviz")
 datas = collect_data_files('jdaviz')
 datas += copy_metadata('jdaviz')
