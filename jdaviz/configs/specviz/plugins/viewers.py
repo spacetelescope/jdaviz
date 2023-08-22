eb445151065853941e57b398e8661cc6bd426868
Brett M. Morris <bmmorris@stsci.edu>
2023-08-22 14:02:50 -0400

    profile viewer x-axis label correction

--- jdaviz/configs/specviz/plugins/viewers.py
+++ jdaviz/configs/specviz/plugins/viewers.py
@@ -625,7 +625,7 @@ class SpecvizProfileView(JdavizViewerMixin, BqplotProfileView):
         else:
             flux_unit_type = "Counts"
             spectral_axis_unit_type = "Sample"
-            x_disp_unit = ''
+            x_disp_unit = self.state.x_display_unit = ''
 
         with self.figure.hold_sync():
             self.figure.axes[0].label = f"{spectral_axis_unit_type} {x_disp_unit}"
