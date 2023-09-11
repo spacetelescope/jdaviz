dd97d86646456fa266fc28bc13904de5138ec903
Ricky O'Steen <39831871+rosteen@users.noreply.github.com>
2023-09-11 16:27:01 -0400

    Debug standalone build (#2441)
    * Log jdaviz output for debugging
    
    * Run workflow on debugging branch
    
    * See if collecting data from jupyter-client will fix this, otherwise might need to downgrade
    
    * Remove debugging stuff

--- standalone/hooks/hook-jupyter_client.py
+++ standalone/hooks/hook-jupyter_client.py
@@ -1,3 +1,7 @@
-from PyInstaller.utils.hooks import collect_submodules, collect_data_files
+from PyInstaller.utils.hooks import (collect_submodules,
+                                     collect_data_files,
+                                     copy_metadata)
 
+datas = collect_data_files('jupyter_client')
+datas += copy_metadata('jupyter_client')
 hiddenimports = collect_submodules("jupyter_client")
