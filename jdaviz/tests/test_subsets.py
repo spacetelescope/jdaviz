191fe658828511c16f42c9c3cbbf8bdb723d0d24
Clare Shanahan <cshanahan@stsci.edu>
2023-09-25 09:22:27 -0400

    .

--- jdaviz/tests/test_subsets.py
+++ jdaviz/tests/test_subsets.py
@@ -139,8 +139,6 @@ def test_region_from_subset_3d(cubeviz_helper):
     flux_viewer = cubeviz_helper.app.get_viewer("flux-viewer")
     # We set the active tool here to trigger a reset of the Subset state to "Create New"
     flux_viewer.toolbar.active_tool = flux_viewer.toolbar.tools['bqplot:circannulus']
-
-    xc=region_shape.center.x, yc=region_shape.center.y,
     cubeviz_helper.app.get_viewer('flux-viewer').apply_roi(CircularAnnulusROI(xc=3,
                                                                               yc=4,
                                                                               inner_radius=2,
