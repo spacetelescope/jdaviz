91945a4faf6801c2a0b05f9efe86a4abf9eba011
Jesse Averbukh <javerbukh@gmail.com>
2023-11-03 11:13:49 -0400

    Use return instead of break

--- jdaviz/configs/imviz/plugins/links_control/links_control.py
+++ jdaviz/configs/imviz/plugins/links_control/links_control.py
@@ -157,7 +157,7 @@ class LinksControl(PluginTemplateMixin, ViewerSelectMixin):
         for data in self.app.data_collection:
             if hasattr(data.coords, 'pixel_to_world'):
                 self.wcs_linking_available = True
-                break
+                return
         self.wcs_linking_available = False
 
     def _on_new_app_data(self, msg):
