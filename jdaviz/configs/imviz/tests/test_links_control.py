f1f4e1c68964c37a4ff3376176eb3c078b740b01
Kyle Conroy <kyleconroy@gmail.com>
2023-11-10 13:50:34 -0500

    fix traitlet name change in tests

--- jdaviz/configs/imviz/tests/test_links_control.py
+++ jdaviz/configs/imviz/tests/test_links_control.py
@@ -16,16 +16,16 @@ class TestLinksControl(BaseImviz_WCS_WCS):
         assert lc_plugin.wcs_use_affine is True
 
         # adding markers should disable changing linking from both UI and API
-        assert lc_plugin.need_clear_markers is False
+        assert lc_plugin.need_clear_astrowidget_markers is False
         tbl = Table({'x': (0, 0), 'y': (0, 1)})
         self.viewer.add_markers(tbl, marker_name='xy_markers')
 
-        assert lc_plugin.need_clear_markers is True
+        assert lc_plugin.need_clear_astrowidget_markers is True
         with pytest.raises(ValueError, match="cannot change linking"):
             lc_plugin.link_type.selected = 'WCS'
         assert lc_plugin.link_type.selected == 'Pixels'
 
         lc_plugin.vue_reset_markers()
 
-        assert lc_plugin.need_clear_markers is False
+        assert lc_plugin.need_clear_astrowidget_markers is False
         lc_plugin.link_type.selected = 'WCS'
