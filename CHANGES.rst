a851693922fb8e9cb49354587b0275fc78e9994c
P. L. Lim <2090236+pllim@users.noreply.github.com>
2023-12-07 17:03:22 -0500

    Backport PR #2594: BUG: Fix Line Profile when there is NaN

--- CHANGES.rst
+++ CHANGES.rst
@@ -15,6 +15,8 @@ Cubeviz
 Imviz
 ^^^^^
 
+- Line Profile (XY) plugin no longer malfunctions when image contains NaN values. [#2594]
+
 Mosviz
 ^^^^^^
 
--- jdaviz/configs/imviz/tests/test_line_profile_xy.py
+++ jdaviz/configs/imviz/tests/test_line_profile_xy.py
@@ -1,7 +1,7 @@
 import numpy as np
 from astropy import units as u
 from astropy.nddata import NDData
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 
 from jdaviz.configs.imviz.tests.utils import BaseImviz_WCS_NoWCS
 
@@ -9,7 +9,7 @@ from jdaviz.configs.imviz.tests.utils import BaseImviz_WCS_NoWCS
 class TestLineProfileXY(BaseImviz_WCS_NoWCS):
     def test_plugin_linked_by_pixel(self):
         """Go through plugin logic but does not check plot contents."""
-        lp_plugin = self.imviz.app.get_tray_item_from_name('imviz-line-profile-xy')
+        lp_plugin = self.imviz.plugins['Imviz Line Profiles (XY)']._obj
         lp_plugin.plugin_opened = True
 
         assert lp_plugin.viewer.labels == ['imviz-0']
@@ -72,3 +72,28 @@ class TestLineProfileXY(BaseImviz_WCS_NoWCS):
                                         'domain': {'x': 5.1, 'y': 5}})
         lp_plugin.selected_x = 1.1
         lp_plugin.selected_y = 9
+
+
+def test_line_profile_with_nan(imviz_helper):
+    arr = np.ones((10, 10))
+    arr[5, 5] = np.nan
+    imviz_helper.load_data(arr)
+
+    lp_plugin = imviz_helper.plugins['Imviz Line Profiles (XY)']._obj
+    lp_plugin.plugin_opened = True
+    lp_plugin.selected_x = 5
+    lp_plugin.selected_y = 5
+    lp_plugin.vue_draw_plot()
+    assert lp_plugin.plot_available
+
+    # NaN still in data but rendered properly as gap.
+    # Cannot check the gap stuff in CI but can make sure X-axis is populated properly etc.
+    for lp_plot in (lp_plugin.plot_across_x, lp_plugin.plot_across_y):
+        assert lp_plot.layers['line'].state.line_visible
+        assert not np.all(np.isfinite(lp_plot.layers['line'].layer.data['y']))
+        assert_array_equal(lp_plot.layers['line'].layer.data['x'], range(10))
+        assert_allclose([lp_plot.layers['line'].state.viewer_state.x_min,
+                         lp_plot.layers['line'].state.viewer_state.x_max,
+                         lp_plot.layers['line'].state.viewer_state.y_min,
+                         lp_plot.layers['line'].state.viewer_state.y_max],
+                        [0, 9, 0.95, 1.05])
