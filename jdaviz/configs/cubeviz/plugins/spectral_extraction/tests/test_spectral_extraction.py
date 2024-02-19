7eabb16895190608a42c7a89de36a0da94cb7dbe
Jesse Averbukh <javerbukh@gmail.com>
2024-02-19 13:59:26 -0500

    Fix test

--- jdaviz/configs/cubeviz/plugins/spectral_extraction/tests/test_spectral_extraction.py
+++ jdaviz/configs/cubeviz/plugins/spectral_extraction/tests/test_spectral_extraction.py
@@ -261,4 +261,4 @@ def test_cone_aperture(cubeviz_helper, spectrum1d_cube_largest):
     extract_plg._obj.vue_adopt_slice_as_reference()
     cone_aperture_2 = extract_plg._obj.cone_aperture()
 
-    assert np.testing.assert_array_equal(cone_aperture, cone_aperture_2)
+    assert np.testing.assert_array_equal(cone_aperture, cone_aperture_2) is False
