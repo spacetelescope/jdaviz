Fix reparenting subsets between rotation orientations (#2734)
* Experimenting...

* Radius calculation now correct under rotation, trying to handle angle properly

* Account for rotation to update theta attribute

* Updated labels since we reparent to Orientation now. Numbers probably wrong

* Added correct calculations for rotated rectangle subset

* Codestyle

* Add to changelog

* Update jdaviz/configs/imviz/tests/test_delete_data.py

Co-authored-by: P. L. Lim <2090236+pllim@users.noreply.github.com>

* Remove extra pixel_to_pixel call

* Fixed bug with deleting second non-default orientation, debugging new angle with flip

* Fix flip handling, add modulo for angles

* Address review comments

Co-authored-by: P. L. Lim <2090236+pllim@users.noreply.github.com>

* Modulo should be in radians

* Apply suggestions from code review

Co-authored-by: P. L. Lim <2090236+pllim@users.noreply.github.com>

* Add test

* pllim review as commit

* Update jdaviz/app.py

---------

Co-authored-by: P. L. Lim <2090236+pllim@users.noreply.github.com>

jdaviz/app.py
@@ -30,7 +30,6 @@ from glue.core.units import unit_converter
 from glue_astronomy.spectral_coordinates import SpectralCoordinates
 from glue_astronomy.translators.regions import roi_subset_state_to_region
 from glue_jupyter.app import JupyterApplication
-from glue_jupyter.bqplot.common.tools import TrueCircularROI
 from glue_jupyter.common.toolbar_vuetify import read_icon
 from glue_jupyter.state_traitlets_helpers import GlueState
 from ipypopout import PopoutButton
@@ -1763,6 +1762,7 @@ class Application(VuetifyTemplate, HubListener):
             item in the data collection that doesn't match ``old_parent`` will be chosen.
         '''
         from astropy.wcs.utils import pixel_to_pixel
+        from jdaviz.configs.imviz.wcs_utils import get_compass_info
 
         if isinstance(old_parent, str):
             old_parent = self.data_collection[old_parent]
@@ -1796,11 +1796,22 @@ class Application(VuetifyTemplate, HubListener):
                     # Translate bounds through WCS if needed
                     if (self.config == "imviz" and
                             self._jdaviz_helper.plugins["Orientation"].link_type == "WCS"):
+
+                        # Default shape for WCS-only layers is 10x10, but it doesn't really matter
+                        # since we only need the angles.
+                        old_angle, _, old_flip = get_compass_info(old_parent.coords, (10, 10))[-3:]
+                        new_angle, _, new_flip = get_compass_info(new_parent.coords, (10, 10))[-3:]
+                        if old_flip != new_flip:
+                            # Note that this won't work for an irregular/assymetric region if we
+                            # ever implement those.
+                            relative_angle = 180 - new_angle - old_angle
+                        else:
+                            relative_angle = new_angle - old_angle
+
                         # Get the correct link to use for translation
                         roi = subset_state.roi
-                        if type(roi) in (CircularROI, CircularAnnulusROI,
-                                         EllipticalROI, TrueCircularROI):
-                            old_xc, old_yc = subset_state.center()
+                        old_xc, old_yc = subset_state.center()
+                        if isinstance(roi, (CircularROI, CircularAnnulusROI, EllipticalROI)):
                             # Convert center
                             x, y = pixel_to_pixel(old_parent.coords, new_parent.coords,
                                                   roi.xc, roi.yc)
@@ -1816,20 +1827,39 @@ class Application(VuetifyTemplate, HubListener):
                                     dummy_x = old_xc + r
                                     x2, y2 = pixel_to_pixel(old_parent.coords, new_parent.coords,
                                                             dummy_x, old_yc)
-                                    new_radius = np.abs(x2 - x)
+                                    # Need to use x and y in this radius calculation because the
+                                    # new orientation is likely rotated compared to the original.
+                                    new_radius = np.sqrt((x2 - x)**2 + (y2 - y)**2)
                                     setattr(roi, att, new_radius)
 
-                        elif type(roi) is RectangularROI:
-                            x_min, y_min = pixel_to_pixel(old_parent.coords, new_parent.coords,
-                                                          roi.xmin, roi.ymin)
-                            x_max, y_max = pixel_to_pixel(old_parent.coords, new_parent.coords,
-                                                          roi.xmax, roi.ymax)
-                            roi.xmin = x_min
-                            roi.xmax = x_max
-                            roi.ymin = y_min
-                            roi.ymax = y_max
+                        elif isinstance(roi, RectangularROI):
+                            x1, y1 = pixel_to_pixel(old_parent.coords, new_parent.coords,
+                                                    roi.xmin, roi.ymin)
+                            x2, y2 = pixel_to_pixel(old_parent.coords, new_parent.coords,
+                                                    roi.xmin, roi.ymax)
+                            x3, y3 = pixel_to_pixel(old_parent.coords, new_parent.coords,
+                                                    roi.xmax, roi.ymin)
 
-                    elif type(subset_group.subset_state) is RangeSubsetState:
+                            # Calculate new width and height from possibly rotated result
+                            new_half_width = np.sqrt((x3-x1)**2 + (y3-y1)**2) * 0.5
+                            new_half_height = np.sqrt((x2-x1)**2 + (y2-y1)**2) * 0.5
+
+                            # Convert center
+                            new_center = pixel_to_pixel(old_parent.coords, new_parent.coords,
+                                                        old_xc, old_yc)
+
+                            # New min/max before applying theta
+                            roi.xmin = new_center[0] - new_half_width
+                            roi.xmax = new_center[0] + new_half_width
+                            roi.ymin = new_center[1] - new_half_height
+                            roi.ymax = new_center[1] + new_half_height
+
+                        # Account for rotation between orientations
+                        if hasattr(roi, "theta"):
+                            fac = 1.0 if (old_flip != new_flip) else -1.0
+                            roi.theta = (fac * (np.deg2rad(relative_angle) - roi.theta)) % (2 * np.pi)  # noqa: E501
+
+                    elif isinstance(subset_group.subset_state, RangeSubsetState):
                         range_state = subset_group.subset_state
                         cur_unit = old_parent.coords.spectral_axis.unit
                         new_unit = new_parent.coords.spectral_axis.unit
@@ -1995,33 +2025,31 @@ class Application(VuetifyTemplate, HubListener):
 
         data_label = event['item_name']
         data = self.data_collection[data_label]
-        self._reparent_subsets(data)
+        orientation_plugin = self._jdaviz_helper.plugins.get("Orientation")
+        if orientation_plugin is not None:
+            orient = orientation_plugin.orientation.selected
+            self._reparent_subsets(data, new_parent=orient)
+        else:
+            self._reparent_subsets(data)
 
         # Make sure the data isn't loaded in any viewers
         for viewer_id in self._viewer_store:
             self.remove_data_from_viewer(viewer_id, data_label)
 
-        # Imviz has some extra logic below that can be skipped after data removal if we're not
-        # removing the reference data, so we check that here.
-        if self.config == "imviz":
-            imviz_refdata = False
-            ref_data, iref = self._jdaviz_helper.get_ref_data()
-            if data is ref_data:
-                imviz_refdata = True
-
         self.data_collection.remove(self.data_collection[data_label])
 
-        # If there are two or more datasets left we need to link them back together after removing
-        # the reference data (which would leave 0 external_links).
-        if len(self.data_collection) > 1 and len(self.data_collection.external_links) == 0:
-            if self.config == "imviz" and imviz_refdata:
-                link_type = self._jdaviz_helper.plugins["Orientation"].link_type.selected.lower()
-                self._jdaviz_helper.link_data(link_type=link_type)
+        # If there are two or more datasets left we need to link them back together if anything
+        # was linked only through the removed data.
+        if (len(self.data_collection) > 1 and
+                len(self.data_collection.external_links) < len(self.data_collection) - 1):
+            if orientation_plugin is not None:
+                orientation_plugin._obj._link_image_data()
                 # Hack to restore responsiveness to imviz layers
                 for viewer_ref in self.get_viewer_reference_names():
                     viewer = self.get_viewer(viewer_ref)
                     loaded_layers = [layer.layer.label for layer in viewer.layers if
-                                     "Subset" not in layer.layer.label]
+                                     "Subset" not in layer.layer.label and layer.layer.label
+                                     not in orientation_plugin.orientation.labels]
                     if len(loaded_layers):
                         self.remove_data_from_viewer(viewer_ref, loaded_layers[-1])
                         self.add_data_to_viewer(viewer_ref, loaded_layers[-1])