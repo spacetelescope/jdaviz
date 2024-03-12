1b3c5de4300e0c1598808874574dcdb806324a8e
Hatice Karatay <haticekaraty@gmail.com>
2024-03-12 11:39:28 -0400

    Change icons and section headers with the theme color

--- jdaviz/components/plugin_section_header.vue
+++ jdaviz/components/plugin_section_header.vue
@@ -40,9 +40,14 @@ module.exports = {
     background: gray;
 }
 
-.strike-active > span:before,
-.strike-active > span:after {
-    background: #c75d2c;  /* active orange */
+.strike-active-light > span:before,
+.strike-active-light > span:after {
+    background: #c75d2c;  /* light mode accent orange */
+}
+
+.strike-active-dark > span:before,
+.strike-active-dark > span:after {
+    background: #FF9D42;  /* dark mode accent orange */
 }
 
 .strike > span:before {
