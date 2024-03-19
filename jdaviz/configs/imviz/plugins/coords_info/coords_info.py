0eb9cb600cec33c166e8ab0f395f75ffc514066f
Ricky O'Steen <rosteen@stsci.edu>
2024-03-19 15:58:36 -0400

    Generalizing coords_info to handle spectral axis first or last [ci skip]

--- jdaviz/configs/imviz/plugins/coords_info/coords_info.py
+++ jdaviz/configs/imviz/plugins/coords_info/coords_info.py
@@ -56,6 +56,7 @@ class CoordsInfo(TemplateMixin, DatasetSelectMixin):
         super().__init__(*args, **kwargs)
         self._marks = {}
         self._dict = {}  # dictionary representation of current mouseover info
+        self._spectral_axis_index = 2 # Needed for cube data
         self._x, self._y = None, None  # latest known cursor positions
 
         # subscribe/unsubscribe to mouse events across all existing viewers
@@ -231,7 +232,10 @@ class CoordsInfo(TemplateMixin, DatasetSelectMixin):
         # return the indices in image.shape for the x and y dimension, respectively
         if image.ndim == 3:
             # cubeviz case
-            return (0, 1)  # (ix_shape, iy_shape)
+            if self._spectral_axis_index == 0:
+                return (2, 1)
+            else:
+                return (0, 1)  # (ix_shape, iy_shape)
         elif image.ndim == 2:
             return (1, 0)  # (ix_shape, iy_shape)
         else:  # pragma: no cover
@@ -240,7 +244,10 @@ class CoordsInfo(TemplateMixin, DatasetSelectMixin):
     def _get_cube_value(self, image, arr, x, y, viewer):
         if image.ndim == 3:
             # cubeviz case:
-            return arr[int(round(x)), int(round(y)), viewer.state.slices[-1]]
+            if self._spectral_axis_index == 0:
+                return arr[viewer.state.slices[-1], int(round(y)), int(round(x))]
+            else:
+                return arr[int(round(x)), int(round(y)), viewer.state.slices[-1]]
         elif image.ndim == 2:
             return arr[int(round(y)), int(round(x))]
         else:  # pragma: no cover
@@ -334,10 +341,16 @@ class CoordsInfo(TemplateMixin, DatasetSelectMixin):
             else:
                 data_wcs = None
 
+            if "spectral_axis_index" in getattr(coo_data, "meta", {}):
+                self._spectral_axis_index = coo_data.meta["spectral_axis_index"]
+
             if data_wcs:
                 try:
                     if wcs_ndim == 3:
-                        sky = data_wcs.pixel_to_world(viewer.slice, y, x)
+                        if self._spectral_axis_index == 0:
+                            sky = data_wcs.pixel_to_world(y, x, viewer.slice)
+                        else:
+                            sky = data_wcs.pixel_to_world(viewer.slice, y, x)
                         sky = [coord for coord in sky if hasattr(coord, "icrs")][0].icrs
                     else:  # wcs_ndim == 2
                         sky = data_wcs.pixel_to_world(x, y).icrs
