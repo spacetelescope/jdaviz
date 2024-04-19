5add83c40ecf0ec7feb2cb5e37d77be460aa791b
Ricky O'Steen <39831871+rosteen@users.noreply.github.com>
2024-04-19 09:46:00 -0400

    Update jdaviz/configs/specviz/plugins/line_analysis/line_analysis.py
    Co-authored-by: Kyle Conroy <kyleconroy@gmail.com>

--- jdaviz/configs/specviz/plugins/line_analysis/line_analysis.py
+++ jdaviz/configs/specviz/plugins/line_analysis/line_analysis.py
@@ -252,8 +252,7 @@ class LineAnalysis(PluginTemplateMixin, DatasetSelectMixin, SpectralSubsetSelect
     def _on_plotted_lines_changed(self, msg):
         self.line_marks = msg.marks
         self.line_items = msg.names_rest
-        menu_labels = [f"{msg.marks[i].name} {msg.marks[i].rest_value} {msg.marks[i].xunit}" for i in range(len(msg.marks))]  # noqa
-        self.line_menu_items = [{"title": menu_labels[i], "value": msg.names_rest[i]} for i in range(len(msg.names_rest))]  # noqa
+        self.line_menu_items = [{"title": f"{mark.name} {mark.rest_value} {mark.xunit}", "value": name_rest} for mark, name_rest in zip(msg.marks, msg.names_rest)]  # noqa
         if self.selected_line not in self.line_items:
             # default to identified line if available
             self.selected_line = self.identified_line
