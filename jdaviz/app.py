b57355299621b9c6a05b31c29395106bb9900876
Jesse Averbukh <javerbukh@gmail.com>
2024-06-17 07:46:36 -0400

    Remove wcs from NDDataArray in to_unit

--- jdaviz/app.py
+++ jdaviz/app.py
@@ -111,7 +111,7 @@ class UnitConverterWithSpectral:
             except RuntimeError:
                 data = data.get_object(cls=NDDataArray)
                 spec = Spectrum1D(flux=data.data * u.Unit(original_units),
-                                  meta=data.meta, wcs=data.wcs)
+                                  meta=data.meta)
             return self._flux_conversion(spec, values, original_units, target_units)
         else:  # spectral axis
             return self._spectral_axis_conversion(values, original_units, target_units)
