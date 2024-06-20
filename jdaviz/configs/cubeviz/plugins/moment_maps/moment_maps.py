96457d3ad3688fad61c044920a03b680d3fbc5f5
Jesse Averbukh <javerbukh@gmail.com>
2024-06-20 19:49:51 -0400

    Remove other unused import

--- jdaviz/configs/cubeviz/plugins/moment_maps/moment_maps.py
+++ jdaviz/configs/cubeviz/plugins/moment_maps/moment_maps.py
@@ -22,7 +22,7 @@ from jdaviz.core.template_mixin import (PluginTemplateMixin,
                                         with_spinner)
 from jdaviz.core.validunits import check_if_unit_is_per_solid_angle
 from jdaviz.core.user_api import PluginUserApi
-from jdaviz.utils import flux_conversion, spectral_axis_conversion
+from jdaviz.utils import flux_conversion
 
 
 __all__ = ['MomentMap']
