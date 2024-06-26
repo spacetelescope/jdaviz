Correct sum units for per steradian (#2873)
* Correct sum units for per steradian

* Rebase with handle_display_unit fix and update tests

* Fix moment map test

* Fix remote data test

* Change _flux and _spectral conversions to be app level

* Fix style

* Move flux and spectral axis conversions to utils

* Remove unused import

* Remove other unused import

* Add description of flux conversion method

* Update changes file

* Update changes file again

jdaviz/tests/test_app.py
@@ -6,7 +6,7 @@ from astropy.wcs import WCS
 from specutils import Spectrum1D
 from jdaviz import Application, Specviz
 from jdaviz.configs.default.plugins.gaussian_smooth.gaussian_smooth import GaussianSmooth
-from jdaviz.app import UnitConverterWithSpectral as uc
+from jdaviz.utils import flux_conversion
 
 
 # This applies to all viz but testing with Imviz should be enough.
@@ -227,8 +227,8 @@ def test_to_unit(cubeviz_helper):
     original_units = u.MJy / u.sr
     target_units = u.MJy
 
-    value = uc._flux_conversion(uc, data.get_object(cls=Spectrum1D), values,
-                                original_units, target_units)
+    value = flux_conversion(data.get_object(cls=Spectrum1D),
+                            values, original_units, target_units)
 
     # will be a uniform array since not wavelength dependent
     # so test first value in array
@@ -240,8 +240,8 @@ def test_to_unit(cubeviz_helper):
     original_units = u.MJy
     target_units = u.erg / u.cm**2 / u.s / u.AA
 
-    new_values = uc._flux_conversion(uc, data.get_object(cls=Spectrum1D), values,
-                                     original_units, target_units)
+    new_values = flux_conversion(data.get_object(cls=Spectrum1D), values,
+                                 original_units, target_units)
 
     assert np.allclose(new_values,
                        (values * original_units)
@@ -255,8 +255,8 @@ def test_to_unit(cubeviz_helper):
     original_units = u.MJy
     target_units = u.erg / u.cm**2 / u.s / u.AA
 
-    new_values = uc._flux_conversion(uc, data.get_object(cls=Spectrum1D), values,
-                                     original_units, target_units)
+    new_values = flux_conversion(data.get_object(cls=Spectrum1D), values,
+                                 original_units, target_units)
 
     # In this case we do a regular spectral density conversion, but using the
     # first value in the spectral axis for the equivalency