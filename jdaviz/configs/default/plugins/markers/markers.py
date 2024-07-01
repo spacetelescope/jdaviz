c88d3b2a72010eb40cf2b4d451bbe0540e3dd737
kcarver1 <kcarver@stsci.edu>
2024-07-01 08:07:53 -0400

    Update jdaviz/configs/default/plugins/markers/markers.py
    Co-authored-by: Kyle Conroy <kyleconroy@gmail.com>

--- jdaviz/configs/default/plugins/markers/markers.py
+++ jdaviz/configs/default/plugins/markers/markers.py
@@ -213,7 +213,6 @@ class Markers(PluginTemplateMixin, ViewerSelectMixin, TableMixin):
     def _on_viewer_key_event(self, viewer, data):
         if data['event'] == 'keydown' and data['key'] == 'm':
             row_info = self.coords_info.as_dict()
-            print(row_info)
 
             if 'viewer' in self.table.headers_avail:
                 row_info['viewer'] = viewer.reference if viewer.reference is not None else viewer.reference_id  # noqa
