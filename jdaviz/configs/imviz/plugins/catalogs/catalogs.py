e383c6c5b9f00ec870114c5908e2ef3cfb7d72de
Katherine Carver <kcarver@osx-summer2401.local>
2024-07-03 08:11:36 -0400

    fixing code style issues

--- jdaviz/configs/imviz/plugins/catalogs/catalogs.py
+++ jdaviz/configs/imviz/plugins/catalogs/catalogs.py
@@ -16,6 +16,7 @@ from jdaviz.core.user_api import PluginUserApi
 
 __all__ = ['Catalogs']
 
+
 @tray_registry('imviz-catalogs', label="Catalog Search")
 class Catalogs(PluginTemplateMixin, ViewerSelectMixin, HasFileImportSelect, TableMixin):
     """
@@ -38,8 +39,7 @@ class Catalogs(PluginTemplateMixin, ViewerSelectMixin, HasFileImportSelect, Tabl
     _default_table_values = {
             'Right Ascension (degrees)': np.nan,
             'Declination (degrees)': np.nan,
-            'Object ID': np.nan
-        }
+            'Object ID': np.nan}
 
     @property
     def user_api(self):
@@ -56,8 +56,8 @@ class Catalogs(PluginTemplateMixin, ViewerSelectMixin, HasFileImportSelect, Tabl
         self.catalog._file_parser = self._file_parser
         self._marker_name = 'catalog_results'
 
-        #initializing the headers in the table that is displayed in the UI
-        headers = ['Right Ascension (degrees)', 'Declination (degrees)','Object ID']
+        # initializing the headers in the table that is displayed in the UI
+        headers = ['Right Ascension (degrees)', 'Declination (degrees)', 'Object ID']
 
         self.table.headers_avail = headers
         self.table.headers_visible = headers
@@ -198,10 +198,8 @@ class Catalogs(PluginTemplateMixin, ViewerSelectMixin, HasFileImportSelect, Tabl
 
         # QTable stores all the filtered sky coordinate points to be marked
         catalog_results = QTable({'coord': filtered_skycoord_table})
-       
-        self.number_of_results = len(catalog_results)
-        
 
+        self.number_of_results = len(catalog_results)
         # markers are added to the viewer based on the table
         viewer.marker = {'color': 'red', 'alpha': 0.8, 'markersize': 5, 'fill': False}
         viewer.add_markers(table=catalog_results, use_skycoord=True, marker_name=self._marker_name)
@@ -210,8 +208,7 @@ class Catalogs(PluginTemplateMixin, ViewerSelectMixin, HasFileImportSelect, Tabl
         for row in self.app._catalog_source_table:
             row_info = {'Right Ascension (degrees)': row['ra'],
                         'Declination (degrees)': row['dec'],
-                        'Object ID' : row['objid']
-                    }
+                        'Object ID': row['objid']}
             self.table.add_item(row_info)
         
         return skycoord_table
