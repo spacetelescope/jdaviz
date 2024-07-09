d408d42358725523576d7b00a053ec09344b0ffd
P. L. Lim <2090236+pllim@users.noreply.github.com>
2024-07-09 10:46:33 -0400

    Merge pull request #2944 from maartenbreddels/fix_matplotlib_standalone_compat
    fix: make standalone work with matplotlib >= 3.9.0
    (cherry picked from commit 6e2826f9c8bb89b41252329bb9b81a6e0f06b790)

--- standalone/jdaviz-cli-entrypoint.py
+++ standalone/jdaviz-cli-entrypoint.py
@@ -1,4 +1,11 @@
 import sys
+# this avoids:
+# ValueError: Key backend: 'module://matplotlib_inline.backend_inline' is not a valid value for backend; supported values are [...]
+# Although not 100% why, it has two effects:
+#  1. PyInstaller picks it up as a module to include
+#  2. It registers the backend, maybe earlier than it would be otherwise
+import matplotlib_inline
+import matplotlib_inline.backend_inline
 
 def start_as_kernel():
     # similar to https://github.com/astrofrog/voila-qt-app/blob/master/voila_demo.py
