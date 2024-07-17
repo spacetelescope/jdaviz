8d869b8f899a5adc85d8a4f4e6d83a3c4742fe05
Duy Nguyen <duytnguyendtn.open@gmail.com>
2024-07-17 10:50:34 -0400

    Filter resource querying on coverage at source, adjust userflow to select source before resource, add manual coordinate toggle to viewer dropdown

--- jdaviz/configs/default/plugins/virtual_observatory/vo_plugin.vue
+++ jdaviz/configs/default/plugins/virtual_observatory/vo_plugin.vue
@@ -4,13 +4,52 @@
     :link="'https://www.ivoa.net/astronomers/index.html'"
     :popout_button="popout_button">
 
-    <plugin-viewer-select
-       :items="viewer_items"
-       :selected.sync="viewer_selected"
-       label="Viewer"
-       :show_if_single_entry="false"
-       hint="Select a viewer to search."
-    />
+    <j-plugin-section-header>Source Selection</j-plugin-section-header>
+
+      <plugin-viewer-select
+        :items="viewer_items"
+        :selected.sync="viewer_selected"
+        label="Viewer"
+        :show_if_single_entry="true"
+        hint="Select a viewer to autofill center coordinates, or Manual for manual coordinate entry."
+      />
+
+      <v-row>
+        <v-text-field
+          v-model="source"
+          label="Source or Coordinates"
+          hint="Enter a source name or ICRS coordinates in degrees to center your query on"
+          :disabled="viewer_selected !== 'Manual'"
+          persistent-hint>
+        </v-text-field>
+      </v-row>
+
+      <v-row>
+        <v-select
+          v-model="coordframe_selected"
+          :menu-props="{ left: true }"
+          attach
+          :items="coordframes"
+          @change="coordframe_selected"
+          label="Coordinate Frame"
+          hint="Astronomical Coordinate Frame of the provided Coordinates"
+          :disabled="viewer_selected !== 'Manual'"
+          persistent-hint
+        ></v-select>
+      </v-row>
+
+    <j-plugin-section-header>Common Options</j-plugin-section-header>
+
+      <v-row>
+        <v-text-field
+        v-model.number="radius_deg"
+        type="number"
+        label="Radius"
+        hint="Angular radius of the specified field in degrees"
+        persistent-hint>
+        </v-text-field>
+      </v-row>
+
 
     <j-plugin-section-header>Survey Collections</j-plugin-section-header>
       <v-row>
@@ -26,52 +65,27 @@
       </v-row>
 
       <v-row>
-        <v-select
-          :menu-props="{ left: true }"
-          attach
-          :items="resources"
-          :loading="resources_loading"
-          @change="resource_selected"
-          label="Available Resources"
-          hint="Select a SIA resource to query"
-          persistent-hint
-        ></v-select>
-      </v-row>
-
-    <j-plugin-section-header>Source Selection</j-plugin-section-header>
-
-      <v-row>
-          <v-text-field
-          v-model="source"
-          label="Source or Coordinates"
-          hint="Enter a source name or ICRS coordinates in degrees to center your query on"
-          persistent-hint>
-          </v-text-field>
-      </v-row>
-
-      <v-row>
-        <v-select
-          v-model="coordframe_selected"
-          :menu-props="{ left: true }"
-          attach
-          :items="coordframes"
-          @change="coordframe_selected"
-          label="Coordinate Frame"
-          hint="Astronomical Coordinate Frame of the provided Coordinates"
-          persistent-hint
-        ></v-select>
-      </v-row>
-
-    <j-plugin-section-header>Common Options</j-plugin-section-header>
-
-      <v-row>
-        <v-text-field
-        v-model.number="radius_deg"
-        type="number"
-        label="Radius"
-        hint="Angular radius of the specified field in degrees"
-        persistent-hint>
-        </v-text-field>
+        <div class="row-select">
+          <v-select
+            :menu-props="{ left: true }"
+            attach
+            :items="resources"
+            :loading="resources_loading"
+            @change="resource_selected"
+            label="Available Resources"
+            hint="Select a SIA resource to query"
+            persistent-hint
+          ></v-select>
+        </div>
+        <div style="line-height: 64px; width=32px">
+          <v-btn
+            id="querybtn"
+            @click="query_registry_resources"
+            icon
+            :loading="resources_loading">
+            <v-icon>mdi-refresh</v-icon>
+          </v-btn>
+        </div>
       </v-row>
 
     <v-row class="row-no-outside-padding">
