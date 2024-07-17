5bea741a8689b9354d3c338c29f5673abf664dec
Brett M. Morris <bmmorris@stsci.edu>
2024-07-17 12:33:11 -0400

    fix for Imviz parser romandeps test since default update in #2767

--- jdaviz/configs/imviz/tests/test_parser_roman.py
+++ jdaviz/configs/imviz/tests/test_parser_roman.py
@@ -8,7 +8,7 @@ from gwcs import WCS as GWCS
 
 @pytest.mark.parametrize(
     ('ext_list', 'n_dc'),
-    [(None, 5),
+    [(None, 1),
      ('data', 1),
      (['data', 'var_rnoise'], 2)])
 def test_roman_wfi_ext_options(imviz_helper, roman_imagemodel, ext_list, n_dc):
@@ -16,10 +16,12 @@ def test_roman_wfi_ext_options(imviz_helper, roman_imagemodel, ext_list, n_dc):
     dc = imviz_helper.app.data_collection
     assert len(dc) == n_dc
 
-    if ext_list is None:
+    if ext_list == '*':
         ext_list = ('data', 'dq', 'err', 'var_poisson', 'var_rnoise')
     elif isinstance(ext_list, str):
         ext_list = (ext_list, )
+    elif ext_list is None:
+        ext_list = ('data', )
 
     for data, ext in zip(dc, ext_list):
         assert data.label == f'roman_wfi_image_model[{ext.upper()}]'
