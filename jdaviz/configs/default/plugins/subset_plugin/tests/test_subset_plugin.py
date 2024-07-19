642e8b13fc9788a1ae4f021eaf51049a432f9cca
Jesse Averbukh <javerbukh@gmail.com>
2024-07-19 09:34:42 -0400

    Add test for single SpectralRegion

--- jdaviz/configs/default/plugins/subset_plugin/tests/test_subset_plugin.py
+++ jdaviz/configs/default/plugins/subset_plugin/tests/test_subset_plugin.py
@@ -184,7 +184,8 @@ def test_circle_recenter_linking(roi_class, subset_info, imviz_helper, image_2d_
      ((SpectralRegion(5.772486091213352 * u.um, 6.052963676101135 * u.um) +
        SpectralRegion(5.8 * u.um, 5.9 * u.um) +
        SpectralRegion(6.494371022809778 * u.um, 6.724270682553864 * u.um) +
-       SpectralRegion(7 * u.um, 7.2 * u.um)), [AndNotMode, OrMode, OrMode], 1, 4)
+       SpectralRegion(7 * u.um, 7.2 * u.um)), [AndNotMode, OrMode, OrMode], 1, 4),
+     (SpectralRegion(5.8 * u.um, 5.9 * u.um), None, 1, 1)
      ]
 )
 def test_import_spectral_region(cubeviz_helper, spectrum1d_cube, spec_regions, mode, len_subsets,
