d9f79236006343b03f3a6ca84a6d56c83024b567
Ricky O'Steen <rosteen@stsci.edu>
2024-08-02 15:20:11 -0400

    Fix failing test

--- jdaviz/configs/imviz/tests/test_helper.py
+++ jdaviz/configs/imviz/tests/test_helper.py
@@ -1,6 +1,8 @@
 import numpy as np
+import pytest
 
 
+@pytest.mark.filterwarnings(r"ignore:The .* function is deprecated")
 def test_plugin_user_apis(imviz_helper):
     for plugin_name, plugin_api in imviz_helper.plugins.items():
         plugin = plugin_api._obj
