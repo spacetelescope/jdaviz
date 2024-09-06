14c643499529cf8ddf64d313f18ce995b2efc8fc
Ricky O'Steen <rosteen@stsci.edu>
2024-09-06 14:30:56 -0400

    Don't set this if unit conversion doesn't exist

--- jdaviz/configs/specviz/plugins/parsers.py
+++ jdaviz/configs/specviz/plugins/parsers.py
@@ -162,7 +162,7 @@ def specviz_spectrum1d_parser(app, data, data_label=None, format=None, show_in_v
 
             # If this is the first loaded data, we want to set spectral y unit type to Flux or
             # Surface Brightness as appropriate
-            if len(app.data_collection) == 0:
+            if len(app.data_collection) == 0 and "Unit Conversion" in app._jdaviz_helper.plugins:
                 uc = app._jdaviz_helper.plugins["Unit Conversion"]
                 if check_if_unit_is_per_solid_angle(flux_units):
                     uc._obj.spectral_y_type = "Surface Brightness"
