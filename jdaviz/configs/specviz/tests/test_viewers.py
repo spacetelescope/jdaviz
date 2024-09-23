029955c75820beec7b2d8ad6ba68b520bf5b7c92
Kyle Conroy <kyleconroy@gmail.com>
2024-09-23 07:38:24 -0400

    unskip test

--- jdaviz/configs/specviz/tests/test_viewers.py
+++ jdaviz/configs/specviz/tests/test_viewers.py
@@ -4,7 +4,6 @@ import pytest
 from specutils import Spectrum1D
 
 
-@pytest.mark.skip(reason="unskip after 3192 merged")
 @pytest.mark.parametrize(
     ('input_unit', 'y_axis_label'),
     [(u.MJy, 'Flux'),
