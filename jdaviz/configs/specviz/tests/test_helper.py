8680144e6a1f5c59061d3faf4ecb06bb053f36d5
Ricky O'Steen <rosteen@stsci.edu>
2024-10-24 12:34:32 -0400

    Add clarity to test and changelog

--- jdaviz/configs/specviz/tests/test_helper.py
+++ jdaviz/configs/specviz/tests/test_helper.py
@@ -406,6 +406,8 @@ def test_load_spectrum_list_directory_concat(tmpdir, specviz_helper):
 
 
 def test_load_2d_flux(specviz_helper):
+    # Test loading a spectrum with a 2D flux, which should be split into separate
+    # 1D Spectrum1D objects to load in Specviz.
     spec = Spectrum1D(spectral_axis=np.linspace(4000, 6000, 10)*u.Angstrom,
                       flux=np.ones((4, 10))*u.Unit("1e-17 erg / (Angstrom cm2 s)"))
     specviz_helper.load_data(spec, data_label="test")
@@ -416,6 +418,7 @@ def test_load_2d_flux(specviz_helper):
     spec2 = Spectrum1D(spectral_axis=np.linspace(4000, 6000, 10)*u.Angstrom,
                        flux=np.ones((2, 10))*u.Unit("1e-17 erg / (Angstrom cm2 s)"))
 
+    # Make sure 2D spectra in a SpectrumList also get split properly.
     spec_list = SpectrumList([spec, spec2])
     specviz_helper.load_data(spec_list, data_label="second test")
 
