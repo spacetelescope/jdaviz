37d714a34bcedc55a3f54777c1d7aa18aa809086
Brett M. Morris <morrisbrettm@gmail.com>
2024-11-21 11:00:17 -0500

    Backport PR #3301: Adding links to ramp fitting docs for roman and jwst in rampviz

--- jdaviz/components/external_link.vue
+++ jdaviz/components/external_link.vue
@@ -1,10 +1,8 @@
 <template>
-  <div style="margin-top: 4px">
-    <a :href="link" target="__blank" style="color: #A75000">
-      <b>{{ linktext || "Learn More" }}</b>
-      <v-icon x-small color="#A75000">mdi-open-in-new</v-icon>
-    </a>
-  </div>
+  <a :href="link" target="__blank" style="color: #A75000">
+    <b>{{ linktext || "Learn More" }}</b>
+    <v-icon x-small color="#A75000">mdi-open-in-new</v-icon>
+  </a>
 </template>
 
 <script>
