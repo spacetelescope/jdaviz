0a3a17133870a4800f11c40ccfe4ce7588b02e15
gibsongreen <gibsongreen99@outlook.com>
2024-12-17 14:40:44 -0500

    fix syntax error

--- jdaviz/configs/default/plugins/model_fitting/model_fitting.py
+++ jdaviz/configs/default/plugins/model_fitting/model_fitting.py
@@ -1113,7 +1113,7 @@ class ModelFitting(PluginTemplateMixin, DatasetSelectMixin,
             return
         models_to_fit = self._reinitialize_with_fixed()
 
-        masked_spectrum = self._apply_subset_masks(self.dataset.selected_spectrum(),
+        masked_spectrum = self._apply_subset_masks(self.dataset.selected_spectrum,
                                                    self.spectral_subset)
         if masked_spectrum.flux.unit.to_string != self.app._get_display_unit('flux'):
             equivalencies = u.spectral_density(masked_spectrum.spectral_axis)
