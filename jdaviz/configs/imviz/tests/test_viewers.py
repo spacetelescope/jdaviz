5445a76bffb658dcf7fe97f5b20f61e26199259f
Clare Shanahan <cshanahan@stsci.edu>
2024-12-23 10:41:02 -0500

    remove test because it requires image to be rendered

--- jdaviz/configs/imviz/tests/test_viewers.py
+++ jdaviz/configs/imviz/tests/test_viewers.py
@@ -118,18 +118,3 @@ class TestDeleteData(BaseImviz_WCS_NoWCS):
         po.stretch_function = "Square Root"
         self.imviz.destroy_viewer("imviz-1")
         assert len(po.layer.choices) == 2
-
-
-def test_viewer_limits(imviz_helper):
-    """
-    Test that sequential calls to set_limits and get_limits return the same
-    viewer limits.
-    """
-    arr = np.ones((10, 10))
-    imviz_helper.load_data(arr, data_label='my_array')
-    viewer = imviz_helper.default_viewer._obj
-
-    # set limits then get limits and make sure they are the same
-    viewer.set_limits(x_min=0, x_max=5, y_min=0, y_max=5)
-    limits = viewer.get_limits()
-    assert limits == (0, 5, 0, 5)
