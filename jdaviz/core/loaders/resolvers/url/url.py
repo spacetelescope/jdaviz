346efadfdf3e9beaa7c2de333c316a44588f6bb6
Kyle Conroy <kyleconroy@gmail.com>
2025-02-14 16:17:59 -0500

    basic URL resolver implementation

--- jdaviz/core/loaders/resolvers/url/url.py
+++ jdaviz/core/loaders/resolvers/url/url.py
@@ -1,24 +1,29 @@
-import os
-from traitlets import Unicode
+from traitlets import Bool, Unicode, observe
+from urllib.parse import urlparse
 
 from jdaviz.core.registries import loader_resolver_registry
-from jdaviz.core.template_mixin import PluginTemplateMixin
+from jdaviz.core.loaders.resolvers import BaseResolver
 from jdaviz.utils import download_uri_to_path
 
 
 @loader_resolver_registry('url')
-class URLResolver(PluginTemplateMixin):
+class URLResolver(BaseResolver):
     template_file = __file__, "url.vue"
 
     url = Unicode("").tag(sync=True)
+    cache = Bool(True).tag(sync=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     @property
     def is_valid(self):
-        return not os.path.exists(self.url)
+        return urlparse(self.url).scheme in ['http', 'https']
 
-    def __call__(self, cache=True, local_path=None, timeout=60):
-        return download_uri_to_path(self.url, cache=cache,
+    @observe('url', 'cache')
+    def _on_url_changed(self, change):
+        self.format._update_items()
+
+    def __call__(self, local_path=None, timeout=60):
+        return download_uri_to_path(self.url, cache=self.cache,
                                     local_path=local_path, timeout=timeout)
