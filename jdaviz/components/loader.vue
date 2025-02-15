2ad4764bf09066d4ca84621fd4ca5dec9828fb9e
Kyle Conroy <kyleconroy@gmail.com>
2025-02-15 14:37:34 -0500

    re-usable j-loader component with slot for content

---
+++ jdaviz/components/loader.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-card flat>
+    <v-card-title class="headline" color="primary" primary-title>{{title}}</v-card-title>
+    <v-card-text>
+        <v-container>
+            <slot/>
+        </v-container>
+        <div style="display: grid"> <!-- overlay container -->
+            <div style="grid-area: 1/1">
+                <plugin-select
+                    :show_if_single_entry="true"
+                    :items="format_items.map(i => i.label)"
+                    :selected.sync="format_selected"
+                    label="Format"
+                    api_hint="loader.format ="
+                    :api_hints_enabled="false"
+                    hint="Choose input format"
+                />
+                <v-row v-if="format_selected.length">
+                    <jupyter-widget :widget="importer_widget"></jupyter-widget>
+                </v-row>
+            </div>
+            <div v-if="format_items_spinner"
+                class="text-center"
+                style="grid-area: 1/1;
+                        z-index:2;
+                        margin-left: -24px;
+                        margin-right: -24px;
+                        padding-top: 24px;
+                        background-color: rgb(0 0 0 / 20%)">
+                <v-progress-circular
+                    indeterminate
+                    color="spinner"
+                    size="50"
+                    width="6"
+                />
+            </div>
+        </div> <!-- overlay container -->
+    </v-card-text>
+    <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+            color="primary"
+            :disabled="!format_selected.length"
+            @click="alert('IMPORT')"
+        >
+            Import
+        </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+module.exports = {
+  props: ['title', 'format_items_spinner', 'format_items', 'format_selected', 'importer_widget'],
+}
+</script>
--- jdaviz/core/loaders/resolvers/filepath/filepath.vue
+++ jdaviz/core/loaders/resolvers/filepath/filepath.vue
@@ -1,58 +1,19 @@
 <template>
-    <v-card>
-        <v-card-title class="headline" color="primary" primary-title>Import Data</v-card-title>
-
-        <v-card-text>
+    <j-loader
+        title="Import Data"
+        :format_items_spinner="format_items_spinner"
+        :format_items="format_items"
+        :format_selected.sync="format_selected"
+        :importer_widget="importer_widget"
+    >
+    
+        <v-row>
             Select a file with data you want to load into this instance of Jdaviz
             and click "IMPORT".
             Note that single clicks navigate into directories.
-            <v-container>
-                <v-row>
-                <v-col>
-                    <g-file-import id="file-uploader"></g-file-import>
-                </v-col>
-                </v-row>
-
-
-
-                <div style="display: grid"> <!-- overlay container -->
-                    <div style="grid-area: 1/1">
-                        <plugin-select
-                            :show_if_single_entry="true"
-                            :items="format_items.map(i => i.label)"
-                            :selected.sync="format_selected"
-                            label="Format"
-                            api_hint="loader.format ="
-                            :api_hints_enabled="false"
-                            hint="Choose input format"
-                        />
-                        <div v-if="format_selected.length">
-                            <p>Importer Options:</p>
-                            <jupyter-widget :widget="importer_widget"></jupyter-widget>
-                        </div>
-                    </div>
-                    <div v-if="format_items_spinner"
-                        class="text-center"
-                        style="grid-area: 1/1;
-                                z-index:2;
-                                margin-left: -24px;
-                                margin-right: -24px;
-                                padding-top: 24px;
-                                background-color: rgb(0 0 0 / 20%)">
-                        <v-progress-circular
-                            indeterminate
-                            color="spinner"
-                            size="50"
-                            width="6"
-                        />
-                    </div>
-                </div> <!-- overlay container -->
-
-
-
-            </v-container>
-
-        </v-card-text>
-    </v-card>
+        </v-row>
+        <v-row>
+          <g-file-import id="file-uploader"></g-file-import>
+        </v-row>
+    </j-loader>
 </template>
-  
