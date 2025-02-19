c0f13b13f6793a2b1612d6da5748b1549424dbd8
Kyle Conroy <kyleconroy@gmail.com>
2025-02-19 12:30:38 -0500

    simplify logic when changing link_type
    to avoid iterating through internal viewer

--- jdaviz/configs/imviz/plugins/orientation/orientation.py
+++ jdaviz/configs/imviz/plugins/orientation/orientation.py
@@ -417,11 +417,8 @@ class Orientation(PluginTemplateMixin, ViewerSelectMixin):
         viewers_to_update = kwargs.get(
             'viewers_to_update', self.app._viewer_store.keys()
         )
-        # this is an ugly way to do this - let's see if we can avoid setting internal traitlets
-        # and instead modify the viewer selection from the new data menu
         for viewer_ref in viewers_to_update:
-            self.viewer.selected = viewer_ref
-            self.orientation.update_wcs_only_filter(wcs_only=self.align_by_selected == 'WCS')
+            viewer_dm = self.app._jdaviz_helper.viewers.get(viewer_ref).data_menu
             for wcs_layer in wcs_only_layers:
                 if wcs_layer not in self.viewer.selected_obj.layers:
                     self.app.add_data_to_viewer(viewer_ref, wcs_layer)
@@ -430,7 +427,7 @@ class Orientation(PluginTemplateMixin, ViewerSelectMixin):
                     wcs_only_layers and
                     self.align_by_selected == 'WCS'
             ):
-                self.orientation.selected = base_wcs_layer_label
+                viewer_dm.orientation.selected = base_wcs_layer_label
 
     def _on_data_add_to_viewer(self, msg):
         all_wcs_only_layers = all(
