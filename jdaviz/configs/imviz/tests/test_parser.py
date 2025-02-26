031a28aadeeb0281538a18e9977c3fc177f72475
Jesse Averbukh <javerbukh@gmail.com>
2025-02-26 14:15:11 -0500

    Update HST test with new values

--- jdaviz/configs/imviz/tests/test_parser.py
+++ jdaviz/configs/imviz/tests/test_parser.py
@@ -393,11 +393,11 @@ class TestParseImage:
         comp = data.get_component('SCI,1')
         expected_label = os.path.splitext(os.path.basename(url))[0] + '[SCI,1]'
         assert data.label == expected_label
-        assert data.shape == (4299, 4219)
+        assert data.shape == (4299, 4220)
         assert_allclose(data.meta['PHOTFLAM'], 7.8711728E-20)
         assert isinstance(data.coords, WCS)
         assert comp.units == 'electron/s'
-        assert comp.data.shape == (4299, 4219)
+        assert comp.data.shape == (4299, 4220)
 
         # --- Since download is expensive, we attach FITS WCS-specific tests here. ---
 
@@ -420,7 +420,7 @@ class TestParseImage:
         assert_allclose(sky.dec.deg, 10.802045612042956, rtol=1e-3)
         data_unit = u.electron / u.s
         assert_quantity_allclose(tbl[0]['background'], 0.0014 * data_unit)
-        assert_quantity_allclose(tbl[0]['sum'], 112.712406 * data_unit, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['sum'], 126.636048 * data_unit, rtol=1e-3)
         assert_quantity_allclose(tbl[0]['sum_aper_area'], 2583.959958 * PIX2, rtol=1e-3)
         assert_array_equal(tbl[0]['pixarea_tot'], None)
         assert_array_equal(tbl[0]['aperture_sum_counts'], None)
@@ -428,21 +428,21 @@ class TestParseImage:
         assert_array_equal(tbl[0]['counts_fac'], None)
         assert_array_equal(tbl[0]['aperture_sum_mag'], None)
         assert_array_equal(tbl[0]['flux_scaling'], None)
-        assert_quantity_allclose(tbl[0]['min'], -0.02422 * data_unit, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['max'], 1.577081 * data_unit, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['mean'], 0.043684 * data_unit, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['median'], 0.02129 * data_unit, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['mode'], -0.023497 * data_unit, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['std'], 0.09908 * data_unit, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['mad_std'], 0.023219 * data_unit, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['var'], 0.009817 * (data_unit * data_unit), rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['biweight_location'], 0.020978 * data_unit, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['biweight_midvariance'], 0.000606 * (data_unit * data_unit), rtol=1e-3)  # noqa
-        assert_quantity_allclose(tbl[0]['fwhm'], 21.230628 * u.pix, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['semimajor_sigma'], 9.712415 * u.pix, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['semiminor_sigma'], 8.260686 * u.pix, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['orientation'], 80.054019 * u.deg, rtol=1e-3)
-        assert_quantity_allclose(tbl[0]['eccentricity'], 0.525929, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['min'], -0.028581 * data_unit, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['max'], 3.942783 * data_unit, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['mean'], 0.0491 * data_unit, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['median'], 0.021689 * data_unit, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['mode'], -0.033133 * data_unit, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['std'], 0.144362 * data_unit, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['mad_std'], 0.023314 * data_unit, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['var'], 0.02084 * (data_unit * data_unit), rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['biweight_location'], 0.02098 * data_unit, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['biweight_midvariance'], 0.000622 * (data_unit * data_unit), rtol=1e-3)  # noqa
+        assert_quantity_allclose(tbl[0]['fwhm'], 25.22627 * u.pix, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['semimajor_sigma'], 12.95812 * u.pix, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['semiminor_sigma'], 7.84902 * u.pix, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['orientation'], 88.73361 * u.deg, rtol=1e-3)
+        assert_quantity_allclose(tbl[0]['eccentricity'], 0.79568, rtol=1e-3)
 
         # Request specific extension (name only), use given label
         imviz_helper.load_data(filename, ext='CTX', data_label='jclj01010_drz',
