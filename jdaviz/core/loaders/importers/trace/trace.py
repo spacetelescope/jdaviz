cf84478ab50f27be2f32929a41a3d0b26fa45916
Kyle Conroy <kyleconroy@gmail.com>
2025-02-27 12:24:56 -0500

    Trace parser to data-collection instead of plugin

--- jdaviz/core/loaders/importers/trace/trace.py
+++ jdaviz/core/loaders/importers/trace/trace.py
@@ -2,22 +2,20 @@ from specreduce.tracing import Trace
 
 
 from jdaviz.core.registries import loader_importer_registry
-from jdaviz.core.loaders.importers import BaseImporterToPlugin
+from jdaviz.core.loaders.importers import BaseImporterToDataCollection
 
 
 @loader_importer_registry('Trace')
-class TraceImporter(BaseImporterToPlugin):
-    template_file = __file__, "trace.vue"
+class TraceImporter(BaseImporterToDataCollection):
+    template_file = __file__, "../to_dc_with_label.vue"
 
     @property
     def is_valid(self):
-        return isinstance(self.input, Trace) and self.has_default_plugin
+        return (isinstance(self.input, Trace)
+                and 'Spectral Extraction' in self.app._jdaviz_helper.plugins)
 
     @property
-    def default_plugin(self):
-        # NOTE: eventually we'll need to differentiate between
-        # cubeviz and specviz2d's spectral extraction
-        return 'Spectral Extraction'
-
-    def __call__(self, subset_label=None):
-        self.app._jdaviz_helper.plugins['Spectral Extraction'].import_trace(self.input)
+    def default_viewer(self):
+        # returns the registry name of the default viewer
+        # only used if `show_in_viewer=True` and no existing viewers can accept the data
+        return 'mosviz-profile-2d-viewer'
