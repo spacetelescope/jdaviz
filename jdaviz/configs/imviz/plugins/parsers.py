7680aadc6cca997eaf88f6d28857f0a158585a14
Hatice Karatay <haticekaraty@gmail.com>
2025-03-12 13:24:20 -0400

    Update parser

--- jdaviz/configs/imviz/plugins/parsers.py
+++ jdaviz/configs/imviz/plugins/parsers.py
@@ -81,13 +81,13 @@ def parse_data(app, file_obj, ext=None, data_label=None,
         `~astropy.utils.data.download_file` or
         `~astroquery.mast.Conf.timeout`).
     """
-    file_obj = get_cloud_fits(file_obj)
 
     if not isinstance(file_obj, str):
         _parse_image(app, file_obj, data_label, ext=ext, parent=parent)
         return
 
     if isinstance(file_obj, str):
+        file_obj = get_cloud_fits(file_obj)
         if data_label is None:
             data_label = os.path.splitext(os.path.basename(file_obj))[0]
 
