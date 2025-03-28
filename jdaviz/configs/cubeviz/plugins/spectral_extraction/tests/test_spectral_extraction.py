855fa7bd8c2ee2b0558f5eb43e2bc0f1ea0fdcfc
Kyle Conroy <kyleconroy@gmail.com>
2025-03-28 08:05:39 -0400

    unskip previously failing test

--- jdaviz/configs/cubeviz/plugins/spectral_extraction/tests/test_spectral_extraction.py
+++ jdaviz/configs/cubeviz/plugins/spectral_extraction/tests/test_spectral_extraction.py
@@ -579,7 +579,6 @@ def test_spectral_extraction_unit_conv_one_spec(
     assert spectrum_viewer.state.y_display_unit == "MJy"
 
 
-@pytest.mark.skip(reason='MAST filename changed')
 @pytest.mark.usefixtures('_jail')
 @pytest.mark.remote_data
 @pytest.mark.parametrize(
