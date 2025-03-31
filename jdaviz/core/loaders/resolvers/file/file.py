f50ff3f5fc1981d783e71c204dabab5216170f2f
Kyle Conroy <kyleconroy@gmail.com>
2025-03-31 13:06:01 -0400

    file resolver: assume current directory if filename passed

--- jdaviz/core/loaders/resolvers/file/file.py
+++ jdaviz/core/loaders/resolvers/file/file.py
@@ -35,6 +35,8 @@ class FileResolver(BaseResolver):
     def _on_filepath_changed(self, change):
         if self._file_upload.file_path != change['new']:
             path, file = os.path.split(change['new'])
+            if path == '':
+                path = './'
             self._file_upload._set_form_values(path, file)
         self._update_format_items()
 
