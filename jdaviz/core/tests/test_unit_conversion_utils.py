7add5b3c06e012d91146feb41bd9cd5e5b0c7375
Ricky O'Steen <rosteen@stsci.edu>
2025-04-04 15:17:34 -0400

    Fix bugs revealed by CI and update tests with new values where needed
    More debugging
    
    Debugging cube model fitting
    
    Fixing more tests
    
    Debugging spectral extraction and collapse
    
    Keep metadata in return_spectrum_with_correct_units
    
    Update gaussian smooth tests
    
    Take correct slice index in coords info
    
    Look for SpectralGWCS for linking as well as SpectralCoordinate
    
    More debugging
    
    Fix spectrum-at-spaxel for spectral-first cubes, linking for plugin generated cubes
    
    Fix model initialization and subset application for non-last spectral axis
    
    Link moment maps to correct cube components
    
    Update moment map test
    
    Remove debugging prints
    
    Update test value
    
    Remove debugging prints

--- jdaviz/core/tests/test_unit_conversion_utils.py
+++ jdaviz/core/tests/test_unit_conversion_utils.py
@@ -217,7 +217,7 @@ def test_viewer_flux_conversion():
     # use viewer_flux_conversion_equivalencies to generate list of equivalencies,
     # which requires a Spectrum
     spec = Spectrum(flux=[1, 2, 3]*u.Jy, spectral_axis=[1, 2, 3]*u.um,
-                      meta={'_pixel_scale_factor': [0.1, 0.2, 0.3]})
+                    meta={'_pixel_scale_factor': [0.1, 0.2, 0.3]})
     viewer_equivs = viewer_flux_conversion_equivalencies([10, 20], spec)
 
     # test for when scale factor array len > 2 but there are 2 values to be
