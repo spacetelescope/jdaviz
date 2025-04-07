104abac030da5e9f044b7f8c839159c660bad97c
Jesse Averbukh <javerbukh@gmail.com>
2025-04-07 14:40:48 -0400

    If edit_subset is set but not combination_mode, update subset

--- jdaviz/configs/default/plugins/subset_tools/subset_tools.py
+++ jdaviz/configs/default/plugins/subset_tools/subset_tools.py
@@ -1129,8 +1129,13 @@ class SubsetTools(PluginTemplateMixin, LoadersMixin):
         with self.app._jdaviz_helper.batch_load():
             # This method can edit a particular subset or create a new subset
             # and apply the combination modes depending on this argument
-            if edit_subset:
+            if edit_subset and combination_mode is not None:
                 self.subset_selected = edit_subset
+            elif edit_subset and combination_mode is None:
+                # If combination_mode is not set, assume the user
+                # wants to update the subset in edit_subset
+                self.subset_selected = edit_subset
+                combination_mode = 'replace'
             else:
                 self.app.session.edit_subset_mode.edit_subset = None
 
