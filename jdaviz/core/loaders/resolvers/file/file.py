2d7698d463f57a71397ea7c7b11fd502cefdde35
Kyle Conroy <kyleconroy@gmail.com>
2025-04-30 11:10:20 -0400

    file loader: migrate to use solara component

--- jdaviz/core/loaders/resolvers/file/file.py
+++ jdaviz/core/loaders/resolvers/file/file.py
@@ -1,7 +1,9 @@
 import os
-from traitlets import Unicode, observe
+from traitlets import Any, Unicode, observe
+from ipywidgets import widget_serialization
+from solara import FileBrowser
+import reacton
 
-from jdaviz.configs.default.plugins.data_tools.file_chooser import FileChooser
 from jdaviz.core.registries import loader_resolver_registry
 from jdaviz.core.loaders.resolvers import BaseResolver
 from jdaviz.core.user_api import LoaderUserApi
@@ -15,36 +17,38 @@ class FileResolver(BaseResolver):
     template_file = __file__, "file.vue"
     default_input = 'filepath'
 
+    file_chooser_widget = Any().tag(sync=True, **widget_serialization)
     filepath = Unicode().tag(sync=True)
 
     def __init__(self, *args, **kwargs):
         start_path = os.environ.get('JDAVIZ_START_DIR', os.path.curdir)
-        self._file_upload = FileChooser(start_path)
-        self._file_upload.observe(self._on_file_upload_path_changed, names='file_path')
-        self.components = {'g-file-import': self._file_upload}
+
+        self.file_chooser_widget_el = FileBrowser(directory=start_path,
+                                                  on_path_select=self._on_file_chooser_path_changed,
+                                                  can_select=True)
+        self.file_chooser_widget, rc = reacton.render(self.file_chooser_widget_el)
         super().__init__(*args, **kwargs)
 
     @property
     def user_api(self):
         return LoaderUserApi(self, expose=['filepath'])
 
-    def _on_file_upload_path_changed(self, event):
-        self.filepath = self._file_upload.file_path
+    def _on_file_chooser_path_changed(self, path):
+        self.filepath = str(path)
 
     @observe('filepath')
     def _on_filepath_changed(self, change):
-        if self._file_upload.file_path != change['new']:
-            path, file = os.path.split(change['new'])
-            if path == '':
-                path = './'
-            self._file_upload._set_form_values(path, file)
+        # when the filepath traitlet is changed, need to update the file_chooser_widget to match the corresponding path
+#        if self._file_upload.file_path != change['new']:
+#            path, file = os.path.split(change['new'])
+#            if path == '':
+#                path = './'
+#            self._file_upload._set_form_values(path, file)
         self._update_format_items()
 
     @property
     def is_valid(self):
-        if self._file_upload.file_path is None:
-            return False
-        return os.path.exists(self._file_upload.file_path) and os.path.isfile(self._file_upload.file_path)  # noqa
+        return os.path.exists(self.filepath) and os.path.isfile(self.filepath)  # noqa
 
     def __call__(self):
-        return self._file_upload.file_path
+        return self.filepath
--- jdaviz/core/loaders/resolvers/file/file.vue
+++ jdaviz/core/loaders/resolvers/file/file.vue
@@ -23,8 +23,6 @@
                 ldr.filepath = '{{ filepath }}'
             </span>
         </v-row>
-        <v-row>
-          <g-file-import id="file-uploader"></g-file-import>
-        </v-row>
+        <jupyter-widget :widget="file_chooser_widget"></jupyter-widget>
     </j-loader>
 </template>
