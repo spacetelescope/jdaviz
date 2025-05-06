ffeb1fce2767026f15990f86264b4da4121587be
Kyle Conroy <kyleconroy@gmail.com>
2025-05-06 14:07:25 -0400

    implement timeout support

--- jdaviz/core/loaders/resolvers/url/url.py
+++ jdaviz/core/loaders/resolvers/url/url.py
@@ -1,6 +1,7 @@
 from traitlets import Bool, Unicode, observe
 from urllib.parse import urlparse
 
+from jdaviz.core.custom_traitlets import FloatHandleEmpty
 from jdaviz.core.registries import loader_resolver_registry
 from jdaviz.core.loaders.resolvers import BaseResolver
 from jdaviz.core.user_api import LoaderUserApi
@@ -17,22 +18,23 @@ class URLResolver(BaseResolver):
 
     url = Unicode("").tag(sync=True)
     cache = Bool(True).tag(sync=True)
+    timeout = FloatHandleEmpty(10).tag(sync=True)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     @property
     def user_api(self):
-        return LoaderUserApi(self, expose=['url', 'cache'])
+        return LoaderUserApi(self, expose=['url', 'cache', 'timeout'])
 
     @property
     def is_valid(self):
         return urlparse(self.url).scheme in ['http', 'https', 'mast', 'ftp']
 
-    @observe('url', 'cache')
+    @observe('url', 'cache', 'timeout')
     def _on_url_changed(self, change):
         self._update_format_items()
 
-    def __call__(self, local_path=None, timeout=60):
-        return download_uri_to_path(self.url, cache=self.cache,
-                                    local_path=local_path, timeout=timeout)
+    def __call__(self, local_path=None):
+        return download_uri_to_path(self.url.strip(), cache=self.cache,
+                                    local_path=local_path, timeout=self.timeout)
