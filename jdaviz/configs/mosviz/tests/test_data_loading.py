0da48f5c41347c7b9515d3ef1e64aaa64373793a
Ricky O'Steen <rosteen@stsci.edu>
2025-05-08 16:50:50 -0400

    Trying to fix tests related to this fixture

--- jdaviz/configs/mosviz/tests/test_data_loading.py
+++ jdaviz/configs/mosviz/tests/test_data_loading.py
@@ -117,7 +117,7 @@ def test_load_mos_spectrum2d(mosviz_helper, mos_spectrum2d):
                                         ).data()
 
     assert len(data) == 1
-    assert data[0].shape == (1024, 15)
+    assert data[0].shape == (15, 1024)
 
 
 @pytest.mark.parametrize('label', [None, "Test Label"])
@@ -221,11 +221,11 @@ def test_load_single_image_multi_spec(mosviz_helper, mos_image, spectrum1d, mos_
     assert label_mouseover.icon == ''
 
     label_mouseover._viewer_mouse_event(spec2d_viewer,
-                                        {'event': 'mousemove', 'domain': {'x': 10, 'y': 100}})
+                                        {'event': 'mousemove', 'domain': {'x': 100, 'y': 10}})
 
     # Note: spectra2d Wave loaded in meters, but we respect one spectral unit, so the meters in
     # converted to Angstrom (the spectra1d spectral unit).
-    assert label_mouseover.as_text() == ('Pixel x=00010.0 y=00100.0 Value +8.12986e-01 Jy',
+    assert label_mouseover.as_text() == ('Pixel x=00100.0 y=00010.0 Value +8.12986e-01 Jy',
                                          'Wave 1.10000e+05 Angstrom', '')
     assert label_mouseover.icon == 'c'
 
