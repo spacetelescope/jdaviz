spacetelescope/jdaviz

commit 0989145ea913fab2fbc245ee3c356af2d17cd491

Disable rename in multiselect editable select  (#3623)

jdaviz/components/plugin_editable_select.vue
@@ -29,10 +29,10 @@
       </template>
       <template v-slot:append>
         <v-icon style="cursor: pointer">mdi-menu-down</v-icon>
-        <j-tooltip tooltipcontent="rename">
+        <j-tooltip tooltipcontent="rename" v-if="!multiselect">
           <v-icon style="cursor: pointer" @click="modeRename">mdi-pencil</v-icon>
         </j-tooltip>
-        <j-tooltip tooltipcontent="remove">
+        <j-tooltip tooltipcontent="remove" v-if="!multiselect">
           <v-icon style="cursor: pointer" @click="modeRemove">mdi-delete</v-icon>
         </j-tooltip>
         <j-tooltip tooltipcontent="create new">

jdaviz/components/plugin_subset_select.vue
@@ -43,7 +43,7 @@
           </span>
         </div>
       </template>
-      <template v-slot:append v-if="selected !== 'Create New'">
+      <template v-slot:append v-if="selected !== 'Create New' && !multiselect">
         <v-icon style="cursor: pointer">mdi-menu-down</v-icon>
         <j-tooltip tooltipcontent="rename" v-if="api_hint_rename">
           <v-icon style="cursor: pointer" @click="() => {rename_new_label = selected; rename_mode = true}">mdi-pencil</v-icon>

commit 004dd9b8a6a659df47969cbe2a271b30a9e2e569

fix bug hiding editable select UI
introduced in #3623

jdaviz/components/plugin_editable_select.vue
@@ -92,7 +92,7 @@
 
 <script>
 module.exports = {
-  props: ['mode', 'edit_value', 'items', 'selected', 'label', 'hint', 'rules',
+  props: ['mode', 'edit_value', 'items', 'selected', 'multiselect', 'label', 'hint', 'rules',
           'api_hint', 'api_hint_add', 'api_hint_rename', 'api_hint_remove', 'api_hints_enabled'
   ],
   computed: {