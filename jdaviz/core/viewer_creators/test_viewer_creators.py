29718897ce91072140afe7fa9749289e7eeace03
Jesse Averbukh <javerbukh@gmail.com>
2025-06-17 13:52:22 -0400

    Fix failing test

--- jdaviz/core/viewer_creators/test_viewer_creators.py
+++ jdaviz/core/viewer_creators/test_viewer_creators.py
@@ -2,14 +2,14 @@
 
 def test_viewer_creator_relevance(deconfigged_helper, spectrum1d, spectrum2d):
     assert len(deconfigged_helper.new_viewers) == 0
-    deconfigged_helper.load(spectrum1d)
+    deconfigged_helper.load(spectrum1d, format='1D Spectrum')
     assert len(deconfigged_helper.new_viewers) == 1
     deconfigged_helper.load(spectrum2d, format='2D Spectrum')
     assert len(deconfigged_helper.new_viewers) == 2
 
 
 def test_viewer_creation(deconfigged_helper, spectrum1d):
-    deconfigged_helper.load(spectrum1d)
+    deconfigged_helper.load(spectrum1d, format='1D Spectrum')
     assert len(deconfigged_helper.viewers) == 1
     deconfigged_helper.new_viewers['1D Spectrum']()
     assert len(deconfigged_helper.viewers) == 2
