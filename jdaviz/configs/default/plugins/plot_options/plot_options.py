63125f458a1d0c6824c3056030854d10acf9de79
gibsongreen <gibsongreen99@outlook.com>
2025-06-26 12:24:20 -0400

    remove comment

--- jdaviz/configs/default/plugins/plot_options/plot_options.py
+++ jdaviz/configs/default/plugins/plot_options/plot_options.py
@@ -605,7 +605,6 @@ class PlotOptions(PluginTemplateMixin, ViewerSelectMixin):
                                                  'volume_value', 'volume_sync',
                                                  state_filter=is_sonified)
 
-        # updated to use visible so syncing occurs between plot options and data menu
         self.sonified_audible = PlotOptionsSyncState(self, self.viewer, self.layer, 'audible',
                                                      'sonified_audible_value',
                                                      'sonified_audible_sync',
