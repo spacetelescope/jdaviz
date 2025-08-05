70118d4a71ebbdf0d8a3c86bc2f7002c7a7945fd
Matthew Portman <mportman@stsci.edu>
2025-08-05 15:57:30 -0400

    WIP include search bar * It's a bit rudimentary but it'll work for now. May attempt to clean it up later.

--- jdaviz/core/loaders/importers/spectrum_list/spectrum_list.vue
+++ jdaviz/core/loaders/importers/spectrum_list/spectrum_list.vue
@@ -1,15 +1,24 @@
 <template>
   <v-container>
-  <plugin-select
-      :items="spectra_items.map(i => i.label)"
+    <div style="margin-bottom: 8px;">
+      <input
+        type="text"
+        v-model="spectra_search_input"
+        placeholder="Search Source IDs..."
+        style="width: 200px;"
+      />
+    </div>
+    <plugin-select
+      :items="filtered_spectra_items"
       :selected.sync="spectra_selected"
       :show_if_single_entry="true"
       :multiselect="spectra_multiselect"
       label="Source IDs"
       api_hint="ldr.source_ids ="
       :api_hints_enabled="api_hints_enabled"
-      hint="Source IDs to select from the BINtable."
+      hint="Source IDs to select from the list of spectra."
     ></plugin-select>
+
     <v-row>
       <plugin-auto-label
         :value.sync="data_label_value"
@@ -24,3 +33,35 @@
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      spectra_search_input: ''
+    };
+  },
+  computed: {
+    filtered_spectra_items() {
+      if (!this.spectra_search_input) {
+        return this.spectra_items.map(i => i.label);
+      }
+      return this.spectra_items
+        .filter(i =>
+          (i.label || '').toLowerCase().includes(this.spectra_search_input.toLowerCase())
+        )
+        .map(i => i.label);
+    }
+  },
+  props: {
+    spectra_items: Array,
+    spectra_selected: Array,
+    spectra_multiselect: Boolean,
+    api_hints_enabled: Boolean,
+    data_label_value: String,
+    data_label_default: String,
+    data_label_auto: Boolean,
+    data_label_invalid_msg: String
+  }
+};
+</script>
