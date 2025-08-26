0ba22227287a48d65f2bbefab4f505845af4b043
Kyle Conroy <kyleconroy@gmail.com>
2025-08-26 13:34:36 -0400

    fix padding issues in loaders

--- jdaviz/core/loaders/importers/image/image.vue
+++ jdaviz/core/loaders/importers/image/image.vue
@@ -20,18 +20,16 @@
       :api_hints_enabled="api_hints_enabled"
       hint="Advanced: manually select a dataset to associate as the parent of the new data entry, 'Auto' will automatically associate non-science extensions with the science extension."
     ></plugin-dataset-select>
-    <v-row>
-      <plugin-auto-label
-        :value.sync="data_label_value"
-        :default="data_label_default"
-        :auto.sync="data_label_auto"
-        :invalid_msg="data_label_invalid_msg"
-        label="Data Label"
-        api_hint="ldr.importer.data_label ="
-        :api_hints_enabled="api_hints_enabled"
-        :hint="data_label_is_prefix ? 'Prefix to assign to the new data entry.' : 'Label to assign to the new data entry.'"
-      ></plugin-auto-label>
-    </v-row>
+    <plugin-auto-label
+      :value.sync="data_label_value"
+      :default="data_label_default"
+      :auto.sync="data_label_auto"
+      :invalid_msg="data_label_invalid_msg"
+      label="Data Label"
+      api_hint="ldr.importer.data_label ="
+      :api_hints_enabled="api_hints_enabled"
+      :hint="data_label_is_prefix ? 'Prefix to assign to the new data entry.' : 'Label to assign to the new data entry.'"
+    ></plugin-auto-label>
     <v-row>
       <plugin-switch
         :value.sync="gwcs_to_fits_sip"
