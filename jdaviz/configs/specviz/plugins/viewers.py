edaa107ccbc4a580a4c02087c8cc90139bda08fe
Ricky O'Steen <rosteen@stsci.edu>
2025-09-05 10:21:43 -0400

    Debug fix

--- jdaviz/configs/specviz/plugins/viewers.py
+++ jdaviz/configs/specviz/plugins/viewers.py
@@ -54,12 +54,12 @@ class Spectrum1DViewer(JdavizProfileView):
             # If we load, e.g.,  one spectrum in Frequency and one in Wavelength,
             # the viewer state x_att won't match the component of the second spectrum since
             # they're no longer both the non-specific "World 0"
-            if str(self.state.x_att) in data.components:
+            if str(self.state.x_att) in data.component_ids():
                 data_xunit = data.get_component(str(self.state.x_att)).units
             else:
                 for comp in ('Wavelength', 'Frequency', 'Energy',
                              'Velocity', 'Wavenumber', 'World 0'):
-                    if comp in data.components:
+                    if comp in data.component_ids():
                         data_xunit = data.get_component(comp).units
                         break
             data_yunit = data.get_component('flux').units
