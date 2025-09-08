46962afd2c3fcb934854ecda0a489690a7fba51e
Matthew Portman <29168957+MatthewPortman@users.noreply.github.com>
2025-09-08 13:02:33 -0400

    Backport PR #3762: Fix wildcard bug and update test

--- jdaviz/tests/test_utils.py
+++ jdaviz/tests/test_utils.py
@@ -3,8 +3,12 @@ import warnings
 
 import pytest
 
-from jdaviz.utils import alpha_index, download_uri_to_path, get_cloud_fits, cached_uri
+from jdaviz.utils import (alpha_index, download_uri_to_path,
+                          get_cloud_fits, cached_uri, escape_brackets,
+                          has_wildcard, wildcard_match)
 from astropy.io import fits
+
+from jdaviz.conftest import FakeSpectrumListImporter
 
 
 @pytest.mark.parametrize("test_input,expected", [(0, 'a'), (1, 'b'), (25, 'z'), (26, 'aa'),
@@ -86,3 +90,105 @@ def test_get_cloud_fits_ext():
 
     hdul = get_cloud_fits(s3_uri, ext=["SCI"])
     assert isinstance(hdul, fits.HDUList)
+
+
+class FakeObject:
+    def __init__(self):
+        pass
+
+
+def test_escape_brackets():
+    # Test that wildcard_match escapes brackets properly
+    assert escape_brackets("") == ""
+    assert escape_brackets("no brackets") == "no brackets"
+    assert escape_brackets("some [brackets]") == "some [[]brackets[]]"
+    assert escape_brackets("s[o]me [br]a[]ckets]") == "s[[]o[]]me [[]br[]]a[[][]]ckets[]]"
+
+
+def test_has_wildcard():
+    assert has_wildcard("") is False
+    assert has_wildcard("no wildcards") is False
+    assert has_wildcard("some * wildcard") is True
+    assert has_wildcard("some ? wildcard") is True
+    # Don't check for brackets anymore, just * and ?
+    assert has_wildcard("some [brackets] wildcard") is False
+    assert has_wildcard("some [brackets] and * wildcard") is True
+    assert has_wildcard("some [brackets] and ? wildcard") is True
+    assert has_wildcard("*") is True
+    assert has_wildcard("?") is True
+
+
+def test_wildcard_match_basic(deconfigged_helper, premade_spectrum_list):
+    default_choices = ['some choice', 'some good choice', 'good choice', 'maybe a bad choice']
+    test_obj = FakeObject()
+
+    # No choices in obj or provided
+    match_result = wildcard_match(test_obj, '*')
+    assert match_result == '*'
+
+    # Multiselect is not an attribute yet
+    match_result = wildcard_match(test_obj, '*', choices=default_choices[0])
+    assert match_result == '*'
+
+    # Add choices attribute, no matches
+    test_obj.choices = default_choices
+    match_result = wildcard_match(test_obj, 'not good*')
+    assert match_result == 'not good*'
+
+    # Test that the function kwarg `choices` overrides the object's choices attribute
+    match_result = wildcard_match(test_obj, 'bad*', choices=default_choices[:-1])
+    assert match_result == 'bad*'
+
+    test_selections = {
+        # Test all
+        '*': default_choices,
+        # Test repeats
+        ('*', '* good *'): default_choices,
+        # Test single selection
+        'some*': default_choices[:2],
+        # Test multi-wildcard
+        ('*', 'good*'): default_choices,
+        # Test multi-selection
+        ('some*', 'good*'): default_choices[:-1]}
+
+    for selection, expected in test_selections.items():
+        # Reset
+        test_obj.multiselect = False
+        match_result = wildcard_match(test_obj, selection)
+        assert test_obj.multiselect is True
+        assert match_result == expected
+
+    # Making sure a stand-in for a SelectPluginComponent object with an attribute
+    # that has `choices` works as expected
+    fake_importer = FakeSpectrumListImporter(app=deconfigged_helper.app,
+                                             resolver=deconfigged_helper.loaders['object']._obj,
+                                             input=premade_spectrum_list)
+    test_obj = fake_importer.sources
+
+    """
+    Left here for reference, premade_spectrum_list has 5 spectra:
+    default_choices = ['1D Spectrum at index: 0',
+                       '1D Spectrum at index: 1',
+                       'Exposure 0, Source ID: 0000',
+                       'Exposure 0, Source ID: 1111',
+                       'Exposure 1, Source ID: 1111']
+    """
+
+    test_selections = {
+        # Test all
+        '*': test_obj.choices,
+        # Test repeats
+        ('*', '*:*'): test_obj.choices,
+        # Test single selection
+        '1D Spectrum at index: ?': test_obj.choices[:2],
+        # Test multi-wildcard
+        '*Exposure*': test_obj.choices[2:],
+        # Test multi-selection
+        ('*at index: 1', 'Exposure 0*'): test_obj.choices[1:-1]}
+
+    for selection, expected in test_selections.items():
+        # Reset
+        test_obj.multiselect = False
+        match_result = wildcard_match(test_obj, selection)
+        assert test_obj.multiselect is True
+        assert match_result == expected
