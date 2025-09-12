93cdc1b36c85efa505a7c33f22db5a287670d871
Kyle Conroy <kyleconroy@gmail.com>
2025-09-12 08:53:05 -0400

    update tests

--- jdaviz/core/viewer_creators/test_viewer_creators.py
+++ jdaviz/core/viewer_creators/test_viewer_creators.py
@@ -3,11 +3,11 @@
 def test_viewer_creator_relevance(deconfigged_helper, spectrum1d, spectrum2d):
     assert len(deconfigged_helper.new_viewers) == 0
     deconfigged_helper.load(spectrum1d, format='1D Spectrum')
-    # 1D Spectrum
-    assert len(deconfigged_helper.new_viewers) == 1
+    # 1D Spectrum, Scatter
+    assert len(deconfigged_helper.new_viewers) == 2
     deconfigged_helper.load(spectrum2d, format='2D Spectrum')
-    # 1D Spectrum, 2D Spectrum Image
-    assert len(deconfigged_helper.new_viewers) == 3
+    # 1D Spectrum, 2D Spectrum, Image, Scatter
+    assert len(deconfigged_helper.new_viewers) == 4
 
 
 def test_viewer_creation(deconfigged_helper, spectrum1d):
