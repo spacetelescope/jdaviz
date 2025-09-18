9c27adb53c1ce80e390dfe147b8f4ba205008a0c
Kyle Conroy <kyleconroy@gmail.com>
2025-09-18 14:54:26 -0400

    more fixes to import_region

--- jdaviz/configs/default/plugins/subset_tools/subset_tools.py
+++ jdaviz/configs/default/plugins/subset_tools/subset_tools.py
@@ -1230,8 +1230,12 @@ class SubsetTools(PluginTemplateMixin, LoadersMixin):
         # Subset is global but reference data is viewer-dependent.
         if refdata_label is None and hasattr(viewer.state, 'reference_data'):
             data = viewer.state.reference_data
-        else:
+        elif refdata_label is not None:
             data = self.app.data_collection[refdata_label]
+        elif len(viewer.layers):
+            data = viewer.layers[0].layer
+        else:
+            raise ValueError('No reference data found in viewer.')
 
         has_wcs = data_has_valid_wcs(data, ndim=2) or data_has_valid_wcs(data, ndim=3)
 
