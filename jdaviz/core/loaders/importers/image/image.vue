363900f71d2c0b087f78df18c343bd87edb0dba0
Matthew Portman <mportman@stsci.edu>
2025-10-03 18:15:36 -0400

    Big updates * Now *correctly* uses the hash to determine which labels are in the data collection. The hashes are checked in the backend via `data_hashes` attribute while a list of matches are sent to the frontend. This list resets on file selection. * `exists_map` was previously a dictionary of booleans. Now it is a list of labels. If a label is in the list, it is a duplicate. * variables have been renamed for clarity.

--- jdaviz/core/loaders/importers/image/image.vue
+++ jdaviz/core/loaders/importers/image/image.vue
@@ -2,7 +2,7 @@
   <v-container>
     <plugin-select
       :items="extension_items.map(i => i.label)"
-      :exists_map="data_in_data_collection"
+      :exists_in_dc="existing_data_in_dc"
       :selected.sync="extension_selected"
       :show_if_single_entry="true"
       :multiselect="extension_multiselect"
