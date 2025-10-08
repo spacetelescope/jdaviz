f057c2082218918ab9415b565ac4c92aea7b7418
Kyle Conroy <kyleconroy@gmail.com>
2025-10-08 08:44:44 -0400

    histogram viewer/viewer-creator

---
+++ jdaviz/core/viewer_creators/histogram/histogram.py
@@ -0,0 +1,55 @@
+from traitlets import List, Unicode, observe
+
+from jdaviz.configs.default.plugins.viewers import HistogramViewer
+from jdaviz.core.user_api import ViewerCreatorUserApi
+from jdaviz.core.viewer_creators import BaseViewerCreator
+from jdaviz.core.registries import viewer_creator_registry
+from jdaviz.core.template_mixin import SelectPluginComponent
+
+
+__all__ = ['HistogramViewerCreator']
+
+@viewer_creator_registry('Histogram', overwrite=True)
+class HistogramViewerCreator(BaseViewerCreator):
+    template_file = __file__, "histogram.vue"
+    xatt_items = List().tag(sync=True)
+    xatt_selected = Unicode().tag(sync=True)
+
+    def __init__(self, app, **kwargs):
+        super().__init__(app, **kwargs)
+        self.viewer_label_default = 'Histogram'
+
+        self.xatt = SelectPluginComponent(self,
+                                          items='xatt_items',
+                                          selected='xatt_selected',
+                                          default_mode='first')
+
+    @property
+    def user_api(self):
+        return ViewerCreatorUserApi(self, expose=('xatt',))
+    @property
+    def viewer_class(self):
+        return HistogramViewer
+
+    @observe('dataset_selected')
+    def _on_dataset_selected_change(self, event):
+        if not hasattr(self, 'dataset') or not hasattr(self, 'xatt'):
+            return
+        dc_items = self.dataset.selected_dc_item
+        if not self.dataset.is_multiselect:
+            dc_items = [dc_items] if dc_items else []
+        valid_atts = []
+        for dc_item in dc_items:
+            valid_atts += [c.label for c in dc_item.components
+                           if 'Pixel Axis' not in c.label]
+        valid_atts = sorted(set(valid_atts))
+        self.xatt._manual_options = valid_atts
+        self.xatt._update_items()
+
+    def __call__(self):
+        nv = super().__call__()
+        if self.xatt.selected != '':
+            nv._obj.state.x_att = self.xatt.selected
+            if hasattr(nv._obj, 'data'):
+                nv._obj.reset_limits()
+        return nv
