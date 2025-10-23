4430aee8582867a247baf1af1b29aa7bba106457
Ricky O'Steen <rosteen@stsci.edu>
2025-10-23 11:57:27 -0400

    Codestyle

--- jdaviz/configs/default/plugins/subset_tools/tests/test_subset_tools.py
+++ jdaviz/configs/default/plugins/subset_tools/tests/test_subset_tools.py
@@ -585,6 +585,7 @@ def test_delete_subset(cubeviz_helper, spectrum1d_cube):
     assert plg.subset.choices == ['Create New', 'Subset 2']
     assert len(cubeviz_helper.app.data_collection) == 2
 
+
 def test_update_subset(cubeviz_helper, spectrum1d_cube):
     cubeviz_helper.load_data(spectrum1d_cube)
     plg = cubeviz_helper.plugins['Subset Tools']
