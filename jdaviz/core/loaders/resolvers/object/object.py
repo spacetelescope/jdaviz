e9f438152743e9c7f6f5eb566985384d02797086
Hatice Karatay <haticekaraty@gmail.com>
2025-11-08 17:26:36 -0500

    Add CustomToolbarToggleMixin support to object loader

--- jdaviz/core/loaders/resolvers/object/object.py
+++ jdaviz/core/loaders/resolvers/object/object.py
@@ -1,22 +1,42 @@
+import os
 from pathlib import Path
 from traitlets import Unicode
+
+from glue_jupyter.common.toolbar_vuetify import read_icon
 
 from jdaviz.core.registries import loader_resolver_registry
 from jdaviz.core.loaders.resolvers import BaseResolver
 from jdaviz.core.user_api import LoaderUserApi
+from jdaviz.core.template_mixin import CustomToolbarToggleMixin
+from jdaviz.core.tools import ICON_DIR
 
 
 @loader_resolver_registry('object')
-class ObjectResolver(BaseResolver):
+class ObjectResolver(BaseResolver, CustomToolbarToggleMixin):
     template_file = __file__, "object.vue"
     default_input = 'object'
     requires_api_support = True
 
     object_repr = Unicode("").tag(sync=True)
+    footprint_select_icon = Unicode(read_icon(os.path.join(
+        ICON_DIR, 'footprint_select.svg'), 'svg+xml')).tag(sync=True)
 
     def __init__(self, *args, **kwargs):
         self._object = None
         super().__init__(*args, **kwargs)
+
+        def custom_toolbar(viewer):
+            if hasattr(self, 'observation_table') and self.observation_table is not None:
+                if 's_region' in self.observation_table.headers_avail:
+                    return viewer.toolbar._original_tools_nested[:3] + [['jdaviz:selectregion']], 'jdaviz:selectregion'
+            return None, None
+
+        self.custom_toolbar.callable = custom_toolbar
+        self.custom_toolbar.name = "Footprint Selection"
+
+    def toggle_custom_toolbar(self):
+        """Override to control footprint display when toolbar is toggled."""
+        super().toggle_custom_toolbar()
 
     @property
     def user_api(self):
