f54ff9db3787416a72163eef756b6b615ea6a956
Kyle Conroy <kyleconroy@gmail.com>
2025-11-24 10:22:49 -0500

    viewer creators

--- jdaviz/core/viewer_creators/__init__.py
+++ jdaviz/core/viewer_creators/__init__.py
@@ -6,3 +6,6 @@ from .image.image import *  # noqa
 from .histogram.histogram import *  # noqa
 from .scatter.scatter import *  # noqa
 from .table.table import *  # noqa
+from .ramp3d.ramp3d import *  # noqa
+from .rampdiff3d.rampdiff3d import *  # noqa
+from .ramp_integration.ramp_integration import *  # noqa
