384aa410cf3e7698ab0fe12d6272b950add5d245
Hatice Karatay <haticekaraty@gmail.com>
2025-12-22 14:52:39 -0500

    Select skewer vs nearest dynamically

--- jdaviz/core/events.py
+++ jdaviz/core/events.py
@@ -375,10 +375,11 @@ class FootprintSelectClickEventMessage(Message):
     Message emitted when a user clicks on a viewer to select an overlay.
     """
 
-    def __init__(self, data, *args, **kwargs):
+    def __init__(self, data, mode="nearest", *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.x = data["domain"]["x"]
         self.y = data["domain"]["y"]
+        self.mode = mode
 
 
 class FootprintOverlayClickMessage(Message):
@@ -386,10 +387,11 @@ class FootprintOverlayClickMessage(Message):
     Message emitted when a user clicks on a viewer to select a footprint/region overlay.
     """
 
-    def __init__(self, data, *args, **kwargs):
+    def __init__(self, data, mode="nearest", *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.x = data["domain"]["x"]
         self.y = data["domain"]["y"]
+        self.mode = mode
 
 
 class RedshiftMessage(Message):
