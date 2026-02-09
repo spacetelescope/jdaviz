9a55b53391243e5310806900f0acfd11fc00ade3
Matthew Portman <mportman@stsci.edu>
2026-02-09 10:15:52 -0500

    Attempt to ignore some network related warnings

--- jdaviz/core/loaders/tests/test_load_catalogs.py
+++ jdaviz/core/loaders/tests/test_load_catalogs.py
@@ -342,6 +342,8 @@ def test_load_catalog_skycoord(imviz_helper, tmp_path, from_file):
 
 
 @pytest.mark.remote_data
+@pytest.mark.filterwarnings('ignore::pytest.PytestUnraisableExceptionWarning')
+@pytest.mark.filterwarnings('ignore:.*unraisable exception warnings.*')
 def test_astroquery_load_catalog_source(deconfigged_helper):
     ldr = deconfigged_helper.loaders['astroquery']
     ldr.source = 'M4'
