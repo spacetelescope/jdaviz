1fda6d8aced15c25eba7449ba3bd2c3a7eef7b77
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2026-02-10 21:27:33 +0100

    Backport PR #4016: Testing solutions to 143 Errors (#4022)
    Co-authored-by: Matthew Portman <29168957+MatthewPortman@users.noreply.github.com>

--- jdaviz/core/loaders/tests/test_load_catalogs.py
+++ jdaviz/core/loaders/tests/test_load_catalogs.py
@@ -356,6 +356,7 @@ def test_load_catalog_skycoord(imviz_helper, tmp_path, from_file):
 
 
 @pytest.mark.remote_data
+@pytest.mark.filterwarnings('ignore::pytest.PytestUnraisableExceptionWarning')
 def test_astroquery_load_catalog_source(deconfigged_helper):
     deconfigged_helper.app.state.catalogs_in_dc = True
 
