632f561229ec9095c6531718b36563557ffc6248
Matthew Portman <mportman@stsci.edu>
2026-02-11 15:12:16 -0500

    Add `slow` mark to unit conversions check code

--- jdaviz/configs/cubeviz/plugins/spectral_extraction/tests/test_spectral_extraction.py
+++ jdaviz/configs/cubeviz/plugins/spectral_extraction/tests/test_spectral_extraction.py
@@ -648,6 +648,7 @@ def test_spectral_extraction_scientific_validation(
     assert median_abs_relative_dev < expected_rtol
 
 
+@pytest.mark.slow
 @pytest.mark.parametrize("flux_angle_unit", [(u.Unit(x), u.sr) for x in SPEC_PHOTON_FLUX_DENSITY_UNITS]  # noqa
                                               + [(u.Unit(x), PIX2) for x in SPEC_PHOTON_FLUX_DENSITY_UNITS])  # noqa
 def test_spectral_extraction_flux_unit_conversions(cubeviz_helper,
