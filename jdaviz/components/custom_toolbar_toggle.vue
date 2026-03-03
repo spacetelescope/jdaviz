dd347116b83515a854cb7c0ae94b518bd58f0988
Hatice Karatay <haticekaraty@gmail.com>
2026-03-03 06:23:56 -0500

    Use api-hint-invert-color class

--- jdaviz/components/custom_toolbar_toggle.vue
+++ jdaviz/components/custom_toolbar_toggle.vue
@@ -7,7 +7,7 @@
         @click="$emit('toggle-custom-toolbar')"
       >
         <slot></slot>
-        <span v-if="api_hints_enabled && api_hint_enable" class="api-hint" :style="enabled ? 'color: white !important' : ''">
+        <span v-if="api_hints_enabled && api_hint_enable" :class="enabled ? 'api-hint api-hint-invert-color' : 'api-hint'">
           {{ enabled ? api_hint_disable : api_hint_enable }}
         </span>
         <span v-else>
