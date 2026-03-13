99918feef6678de1cf1df177295a2236277b3b36
Ricky O'Steen <rosteen@stsci.edu>
2026-03-13 15:27:10 -0400

    Initial pass at adding deconfigged to launcher

--- jdaviz/cli.py
+++ jdaviz/cli.py
@@ -105,7 +105,7 @@ def _main(config=None):
     filepaths_nargs = '*'
     if config is None:
         parser.add_argument('--layout', default='', choices=ALL_JDAVIZ_CONFIGS,
-                            help='Configuration to use.')
+                            help='Configuration to use.', deprecated=True)
     if (config == "mosviz") or ("mosviz" in sys.argv):
         filepaths_nargs = 1
     parser.add_argument('filepaths', type=str, nargs=filepaths_nargs, default=None,
