0e348ebfce5302e8bb61d3b1fc7ece0b23dbb9e5
Clare Shanahan <cshanahan@stsci.edu>
2026-03-17 12:47:05 -0400

    increase tolerence

--- jdaviz/configs/imviz/tests/test_orientation.py
+++ jdaviz/configs/imviz/tests/test_orientation.py
@@ -252,8 +252,8 @@ class TestDeleteOrientation(BaseImviz_WCS_WCS):
         out_reg = self.imviz.app.get_subsets(include_sky_region=True)["Subset 1"][0]["sky_region"]
         assert_allclose(out_reg.center.ra.deg, reg.center.ra.deg)
         assert_allclose(out_reg.center.dec.deg, reg.center.dec.deg)
-        assert_quantity_allclose(out_reg.width, reg.width)
-        assert_quantity_allclose(out_reg.height, reg.height)
+        assert_quantity_allclose(out_reg.width, reg.width, rtol=1e-5)
+        assert_quantity_allclose(out_reg.height, reg.height, rtol=1e-5)
         # FIXME: However, sky angle has to stay the same as per regions convention.
         with pytest.raises(AssertionError, match="Not equal to tolerance"):
             assert_quantity_allclose(out_reg.angle, reg.angle)
