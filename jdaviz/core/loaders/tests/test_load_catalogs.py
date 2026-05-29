81696baeea400317bf8ec5e16dedf51a1ac31ee9
Jesse Averbukh <javerbukh@gmail.com>
2026-05-29 11:27:49 -0400

    Remove changes pulled in by rebase

--- jdaviz/core/loaders/tests/test_load_catalogs.py
+++ jdaviz/core/loaders/tests/test_load_catalogs.py
@@ -1,5 +1,4 @@
 import os
-import requests
 
 from astropy.coordinates import SkyCoord
 from astropy.table import Table, QTable
@@ -352,15 +351,7 @@ def test_astroquery_load_catalog_source(deconfigged_helper):
     ldr.source = 'M4'
     ldr.telescope = 'Gaia'
     ldr.max_results = 10
-    try:
-        ldr.query_archive()
-    except requests.exceptions.HTTPError as exc:
-        msg = str(exc)
-        if '408' in msg or 'timeout' in msg.lower() or 'aborted' in msg.lower():
-            pytest.skip(f"Remote archive query timed out: {exc}")
-        raise
-    except requests.exceptions.RequestException as exc:
-        pytest.skip(f"Transient remote archive failure: {exc}")
+    ldr.query_archive()
     assert 'Catalog' in ldr.format.choices
     ldr.format = 'Catalog'
 
