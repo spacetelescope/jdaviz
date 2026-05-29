fa0d03afff6f6e5a716c3a6347ebcf517384194a
jbchampagne <jackie.champagne.astro@gmail.com>
2026-05-29 08:07:33 -0700

    updated ra/dec variation list

--- jdaviz/core/loaders/importers/catalog/test_catalog.py
+++ jdaviz/core/loaders/importers/catalog/test_catalog.py
@@ -13,7 +13,7 @@ def test_coord_column_detection(deconfigged_helper):
     """
 
     # Variations of 'ra' and 'dec' that should be correctly identified as coordinate columns
-    # testing a mix of case/delimeter variations, but not an exhaustive list of all possible variations
+    # testing a mix of case/delimeter variations
     ra_variations = ['Right_Ascension', 'ra', 'radeg',
                      'radegrees', 'right-ascension-Degrees', 'rightascension_deg',
                      'raobj', 'objra', 'sourcera', 'Rasource', 'raj2000', 'ra2000',
@@ -22,14 +22,6 @@ def test_coord_column_detection(deconfigged_helper):
                       'dec-degrees', 'declination_degrees', 'declinationdeg',
                       'decobj', 'objdec', 'DECsource', 'sourcedec', 'decJ2000',
                       'dec2000', 'world_dec', 'targdec', 'SCI_dec']
-    ra_variations = ['rightascension', 'ra', 'radeg',
-                     'radegrees', 'rightascensiondegrees', 'rightascensiondeg',
-                     'raobj', 'objra', 'sourcera', 'rasource', 'raj2000', 'ra2000',
-                     'worldra', 'targra', 'scira']
-    dec_variations = ['declination', 'dec', 'decdeg',
-                      'decdegrees', 'declinationdegrees', 'declinationdeg',
-                      'decobj', 'objdec', 'decsource', 'sourcedec', 'decj2000',
-                      'dec2000', 'worlddec', 'targdec', 'scidec']
 
     variations_to_pass = list(zip(ra_variations, dec_variations))
 
