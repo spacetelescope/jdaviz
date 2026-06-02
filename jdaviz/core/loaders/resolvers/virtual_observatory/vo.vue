80dbae39029b23270ce730c5c57603ae7eb49a9b
Duy Nguyen <duytnguyendtn.open@gmail.com>
2026-06-02 13:58:10 -0400

    Drop SIA terminology in API Hint

--- jdaviz/core/loaders/resolvers/virtual_observatory/vo.vue
+++ jdaviz/core/loaders/resolvers/virtual_observatory/vo.vue
@@ -152,7 +152,7 @@
         :search="true"
         api_hint="ldr.resource ="
         :api_hints_enabled="api_hints_enabled"
-        hint="Select a SIA resource to query"
+        hint="Select a resource to query"
       ></plugin-select>
     </v-form>
 
