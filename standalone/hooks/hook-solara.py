5f8298945f20f1bcd0c9300ca468fa8cc5750214
Sarah Betti <sbetti@stsci.edu>
2026-06-26 15:15:25 -0400

    updates to hooks for building standalone app

--- standalone/hooks/hook-solara.py
+++ standalone/hooks/hook-solara.py
@@ -1,5 +1,8 @@
-from PyInstaller.utils.hooks import collect_data_files, copy_metadata, collect_submodules
+from PyInstaller.utils.hooks import collect_data_files, copy_metadata, collect_submodules, collect_all
 
-hiddenimports = collect_submodules("solara")
-datas = collect_data_files('solara')
+# hiddenimports = collect_submodules("solara")
+
+datas, binaries, hiddenimports = collect_all('solara')
+
+# datas = collect_data_files('solara')
 datas += collect_data_files('solara-ui')
