0e4a64539eab853d8863c625cccdaf1d7d0eea17
Sarah Betti <sbetti@stsci.edu>
2026-06-26 15:42:40 -0400

    update hook-scipy due to scipy update to 1.18

--- standalone/hooks/hook-scipy.py
+++ standalone/hooks/hook-scipy.py
@@ -1,5 +1,10 @@
-from PyInstaller.utils.hooks import collect_data_files, copy_metadata, collect_submodules
+from PyInstaller.utils.hooks import check_requirement, collect_data_files, copy_metadata, collect_submodules
 
 hiddenimports = collect_submodules("scipy")
 datas = collect_data_files('scipy')
 datas += copy_metadata('scipy')
+
+if check_requirement("scipy >= 1.14.0") and check_requirement("scipy < 1.18.0"): 
+    hiddenimports += ['scipy._lib.array_api_compat.numpy.fft'] 
+elif check_requirement("scipy >= 1.18.0"): 
+    hiddenimports += ['scipy._external.array_api_compat.numpy.fft'] 
