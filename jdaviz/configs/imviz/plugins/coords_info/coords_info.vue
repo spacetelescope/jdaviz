7f4a8fc62c50b25a8c82813e097f3fe0ec92df81
Mario Buikhuizen <mariobuikhuizen@widgetti.io>
2026-07-10 18:21:37 +0200

    fix: warning about table structure after update to Vue 3.5.39

--- jdaviz/configs/imviz/plugins/coords_info/coords_info.vue
+++ jdaviz/configs/imviz/plugins/coords_info/coords_info.vue
@@ -28,20 +28,22 @@
         </v-btn>
         <span style="display: inline-block; white-space: nowrap; line-height: 14pt; margin: 0; position: absolute; margin-left: 48px">
           <table>
-            <tr>
-              <td colspan="4" :style="row1_unreliable ? 'color: #B8B8B8' : ''">
-                <b>{{ row1a_title }} </b>{{ row1a_text }}&nbsp;&nbsp;
-                <b>{{ row1b_title }} </b>{{ row1b_text }}
-              </td>
-            </tr>
-            <tr :style="row2_unreliable ? 'color: #B8B8B8' : ''">
-              <td width="42"><b>{{ row2_title }}</b></td>
-              <td>{{ row2_text }}</td>
-            </tr>
-            <tr :style="row3_unreliable ? 'color: #B8B8B8' : ''">
-              <td width="42"><b>{{ row3_title }}</b></td>
-              <td>{{ row3_text }}</td>
-            </tr>
+            <tbody>
+              <tr>
+                <td colspan="4" :style="row1_unreliable ? 'color: #B8B8B8' : ''">
+                  <b>{{ row1a_title }} </b>{{ row1a_text }}&nbsp;&nbsp;
+                  <b>{{ row1b_title }} </b>{{ row1b_text }}
+                </td>
+              </tr>
+              <tr :style="row2_unreliable ? 'color: #B8B8B8' : ''">
+                <td width="42"><b>{{ row2_title }}</b></td>
+                <td>{{ row2_text }}</td>
+              </tr>
+              <tr :style="row3_unreliable ? 'color: #B8B8B8' : ''">
+                <td width="42"><b>{{ row3_title }}</b></td>
+                <td>{{ row3_text }}</td>
+              </tr>
+            </tbody>
           </table>
         </span>
       </v-toolbar-items>
