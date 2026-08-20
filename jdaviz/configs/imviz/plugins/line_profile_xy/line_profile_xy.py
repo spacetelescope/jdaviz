e3545f9e31e850b14bad52df645ce9aff2d08b5c
Jesse Averbukh <javerbukh@gmail.com>
2026-08-20 09:42:20 -0400

    Support L and l in line profile

--- jdaviz/configs/imviz/plugins/line_profile_xy/line_profile_xy.py
+++ jdaviz/configs/imviz/plugins/line_profile_xy/line_profile_xy.py
@@ -47,9 +47,9 @@ class LineProfileXY(PluginTemplateMixin, ViewerSelectMixin):
         self.plot_across_x_widget = 'IPY_MODEL_'+self.plot_across_x.model_id
         self.plot_across_y_widget = 'IPY_MODEL_'+self.plot_across_y.model_id
 
-        self.docs_description = "Press lowercase 'L' to plot line profiles across\
-                                 X and Y under cursor. You can also manually enter\
-                                 X, Y and then click PLOT."
+        self.docs_description = "Press 'L' to plot line profiles across X and Y\
+                                 under cursor. You can also manually enter X, Y\
+                                 and then click PLOT."
 
         self.hub.subscribe(self, ViewerAddedMessage, handler=self._on_viewer_added)
 
@@ -90,7 +90,7 @@ class LineProfileXY(PluginTemplateMixin, ViewerSelectMixin):
         self.vue_draw_plot(msg)
 
     def _on_viewer_key_event(self, viewer, data):
-        if data['key'] == 'l':
+        if data['key'] in ('l', 'L'):
             image = viewer.active_image_layer.layer
             x = data['domain']['x']
             y = data['domain']['y']
