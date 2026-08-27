c7bf5dc41fccbff5e5f26b8fea3cb2654bde5137
Matthew Portman <mportman@stsci.edu>
2026-08-27 14:09:03 -0400

    Adjust for new reporting behavior

--- jdaviz/core/loaders/tests/test_loaders.py
+++ jdaviz/core/loaders/tests/test_loaders.py
@@ -632,8 +632,8 @@ def test_failed_astroquery(deconfigged_helper):
     ldr = deconfigged_helper.loaders['astroquery']
     ldr.source = "Bad Object"
     ldr.query_archive()
-    snackbar_msg = "Unable to resolve source coordinates: Bad Object"
-    assert deconfigged_helper.plugins['Logger'].history[-1]['text'] == snackbar_msg
+    snackbar_msg = "Unable to resolve source as name: Bad Object"
+    assert snackbar_msg in [d['text'] for d in deconfigged_helper.plugins['Logger'].history]
 
 
 def test_invoke_from_plugin(specviz_helper, spectrum1d, tmp_path):
