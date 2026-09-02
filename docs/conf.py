69ed2a6bbcf8115afd452f2e9482ac44e7e51c9d
Sarah Betti <sbetti@stsci.edu>
2026-09-02 15:38:42 -0400

    testing favicon

--- docs/conf.py
+++ docs/conf.py
@@ -241,12 +241,12 @@ html_context = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'logos/jdaviz_wordmark.svg'
+html_logo = '_static/logos/jdaviz_wordmark.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'logos/specviz2d.ico'
+html_favicon = '_static/logos/specviz2d.ico'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
