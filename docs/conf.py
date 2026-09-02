9d99b14800ce96665f88c844d86fa9f5a3c3d459
Sarah Betti <sbetti@stsci.edu>
2026-09-02 16:29:24 -0400

    trying favicon with .png

--- docs/conf.py
+++ docs/conf.py
@@ -246,7 +246,7 @@ html_logo = '_static/logos/jdaviz_wordmark.svg'
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = '_static/logos/specviz2d.ico'
+html_favicon = '_static/logos/jdaviz_favicon.png'
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
