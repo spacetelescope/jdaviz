13d8c4341570acb0d614631b557742eddb20ab25
Sarah Betti <sbetti@stsci.edu>
2026-09-04 12:22:15 -0400

    removed html_favicon

--- docs/conf.py
+++ docs/conf.py
@@ -243,11 +243,7 @@ html_context = {
 # of the sidebar.
 html_logo = 'logos/jdaviz_wordmark.svg'
 
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-# html_favicon = 'logos/jdaviz_favicon.png'
-
+# The name of an image file (within the static path) to use as favicons
 favicons = [
     {
         "rel": "icon",
